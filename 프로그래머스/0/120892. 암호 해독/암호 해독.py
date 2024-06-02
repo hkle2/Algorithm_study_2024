58e5e18b8aa628e1ef63a017af70de63de11fde2
hkle2 <minnie110598@gmail.com>
2024-06-02 15:46:50 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.py
@@ -1,7 +1,3 @@
 def solution(cipher, code):
-    answer = ''
-    print(len(cipher))
-    for i in range(len(cipher)):
-        if (i + 1) % code == 0:
-            answer += cipher[i]
+    answer = cipher[code-1::code]
     return answer
