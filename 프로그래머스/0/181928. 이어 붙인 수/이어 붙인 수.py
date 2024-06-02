4b3c9406b64c0e010d68b274f6134eed26650a71
hkle2 <minnie110598@gmail.com>
2024-06-02 19:47:13 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -1,10 +1,10 @@
 def solution(num_list):
-    answer1 = ''
-    answer2 = ''
+    odd = ''
+    even = ''
     for i in num_list:
         if i % 2 != 0:
-            answer1 += str(i)
+            odd += str(i)
         else:
-            answer2 += str(i)
-    answer = int(answer1) + int(answer2)
+            even += str(i)
+    answer = int(odd) + int(even)
     return answer
