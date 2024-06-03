cc26abfdb65eaec495565e81da7d3acbb83e2181
hkle2 <minnie110598@gmail.com>
2024-06-03 12:39:18 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,11 +1,5 @@
 def solution(myString, pat):
     answer = 0
-    new_string = ''
-    for i in myString:
-        if i == "A":
-            new_string += "B"
-        else:
-            new_string += "A"
-    if pat in new_string:
+    if pat in myString.replace("A", "C").replace("B", "A").replace("C", "B"):
         answer = 1
     return answer
