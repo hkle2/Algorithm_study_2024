4d613a7001d6df07a90900c298356abb2f280e84
hkle2 <minnie110598@gmail.com>
2024-06-03 18:04:31 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,6 +1,5 @@
 def solution(my_string, is_prefix):
     answer = 0
-    for i in range(len(my_string)):
-        if my_string[:i] == is_prefix:
-            answer = 1
+    if my_string.startswith(is_prefix):
+        answer = 1
     return answer
