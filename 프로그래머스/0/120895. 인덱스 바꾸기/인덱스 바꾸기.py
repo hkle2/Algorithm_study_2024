5be8287e993aeea6b94a5746360fe283fc47ca89
hkle2 <minnie110598@gmail.com>
2024-06-04 19:19:22 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,10 +1,5 @@
 def solution(my_string, num1, num2):
-    answer = ''
-    for i in range(len(my_string)):
-        if i == num1:
-            answer += my_string[num2]
-        elif i == num2:
-            answer += my_string[num1]
-        else:
-            answer += my_string[i]
+    s = list(my_string)
+    s[num1], s[num2] = s[num2], s[num1]
+    answer = "".join(s)
     return answer
