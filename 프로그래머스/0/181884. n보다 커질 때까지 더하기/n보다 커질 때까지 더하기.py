a337c1ededef85f250deee0935a0c59acef14432
hkle2 <minnie110598@gmail.com>
2024-06-04 15:47:22 +0900

    Update n보다 커질 때까지 더하기.py

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.py
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.py
@@ -1,10 +1,5 @@
 def solution(numbers, n):
     answer = 0
-    # for i in numbers:
-    #     if answer <= n:
-    #         answer += i
-    #     else:
-    #         break
     i = 0
     while answer <= n:
         answer += numbers[i]
