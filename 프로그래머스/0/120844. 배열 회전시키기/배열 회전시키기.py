cf96298c3869e1426ade0284094294e1362773de
hkle2 <minnie110598@gmail.com>
2024-06-04 22:41:38 +0900

    Update 배열 회전시키기.py

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,11 +1,18 @@
 def solution(numbers, direction):
-    answer = [0 for i in range(len(numbers))]
     if direction == "right":
-        for i in range(len(numbers)-1):
-            answer[i + 1] = numbers[i]
-        answer[0] = numbers[-1]
+        answer = [numbers[-1]] + numbers[:-1]
     else:
-        for i in range(len(numbers)-1):
-            answer[i] = numbers[i + 1]
-        answer[-1] = numbers[0]
+        answer = numbers[1:] + [numbers[0]]
+    return answer
+
+
+from collections import deque
+
+def solution(numbers, direction):
+    numbers = deque(numbers)
+    if direction == "right":
+        numbers.rotate(1)
+    else:
+        numbers.rotate(-1)
+    answer = list(numbers)
     return answer
