590cae3541989ce870ce507d6619fa10505eec1a
hkle2 <minnie110598@gmail.com>
2024-06-04 21:10:49 +0900

    [level 0] Title: A 강조하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -1,10 +1,3 @@
 def solution(myString):
-    answer = ''
-    for i in myString:
-        if i == "a":
-            answer += "A"
-        elif i.isupper() and i != "A":
-            answer += i.lower()
-        else:
-            answer += i
+    answer = myString.lower().replace("a", "A")
     return answer
