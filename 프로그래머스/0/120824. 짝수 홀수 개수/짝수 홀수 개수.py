e7190cbe12d535fcbf3e576e36e867df89801766
hkle2 <minnie110598@gmail.com>
2024-06-04 16:07:24 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,8 +1,5 @@
 def solution(num_list):
     answer = [0, 0]
     for i in num_list:
-        if i % 2 == 0:
-            answer[0] += 1
-        else:
-            answer[1] += 1
+        answer[i % 2] += 1
     return answer
