af4e6aacfc19f8f6195aabd80afdf8bb0a292c2c
hkle2 <minnie110598@gmail.com>
2024-06-07 21:47:28 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/Python3/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -0,0 +1,17 @@
+def solution(numbers, direction):
+    if direction == "right":
+        answer = [numbers[-1]] + numbers[:-1]
+    else:
+        answer = numbers[1:] + [numbers[0]]
+    return answer
+
+from collections import deque
+
+def solution(numbers, direction):
+    numbers = deque(numbers)
+    if direction == "right":
+        numbers.rotate(1)
+    else:
+        numbers.rotate(-1)
+    answer = list(numbers)
+    return answer
