e278024715fe95d0495d3dd84eba2843454a9c76
hkle2 <minnie110598@gmail.com>
2024-06-08 00:43:51 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/Python3/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,12 @@
+def solution(a, b, c):
+    answer = 0
+    e1 = a + b + c
+    e2 = a**2 + b**2 + c**2
+    e3 = a**3 + b**3 + c**3
+    if a != b and b != c and a != c:
+        answer = e1
+    elif a == b == c:
+        answer = e1 * e2 * e3
+    else:
+        answer = e1 * e2
+    return answer
