3c88a60d488c1073aae1fbb48daa9d6dc932ea22
hkle2 <minnie110598@gmail.com>
2024-06-08 00:53:30 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/Python3/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/Python3/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -10,3 +10,17 @@ def solution(a, b, c):
     else:
         answer = e1 * e2
     return answer
+
+def solution(a, b, c):
+    answer = 0
+    n = set([a, b, c])
+    e1 = a + b + c
+    e2 = a**2 + b**2 + c**2
+    e3 = a**3 + b**3 + c**3
+    if len(n) == 3:
+        answer = e1
+    elif len(n) == 2:
+        answer = e1 * e2
+    else:
+        answer = e1 * e2 * e3
+    return answer
