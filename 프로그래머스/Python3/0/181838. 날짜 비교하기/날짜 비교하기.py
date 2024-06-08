3027e408acfbbc5d723be0b38483933891315189
hkle2 <minnie110598@gmail.com>
2024-06-08 16:34:54 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Python3/0/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/Python3/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -3,6 +3,7 @@ def solution(date1, date2):
     for i in range(len(date1)):
         if date1[i] < date2[i]:
             answer = 1
+            break
         elif date1[i] == date2[i]:
             answer = 0
         else:
