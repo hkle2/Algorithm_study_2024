d362cd63d11eceb315288bfa07a62b3a0435ad3b
hkle2 <minnie110598@gmail.com>
2024-06-08 16:16:14 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/Python3/0/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/Python3/0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -4,3 +4,7 @@ def solution(my_string):
         if i not in answer:
             answer += i
     return answer
+
+def solution(my_string):
+    answer = "".join(dict.fromkeys(my_string))
+    return answer
