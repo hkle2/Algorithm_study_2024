914c82dd6d39d6a7aec3b8ec328154bf7456e08d
hkle2 <minnie110598@gmail.com>
2024-06-09 19:36:50 +0900

    [level 0] Title: 2의 영역, Time: 3.94 ms, Memory: 15.5 MB -BaekjoonHub

---
+++ 프로그래머스/Python3/0/181894. 2의 영역/2의 영역.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = []
+    idx = []
+    for i in range(len(arr)):
+        if arr[i] == 2:
+            idx.append(i)
+    if idx:
+        answer = arr[idx[0]:idx[-1]+1]
+    else:
+        answer = [-1]
+    return answer
