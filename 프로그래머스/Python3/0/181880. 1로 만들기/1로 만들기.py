e01fbfca81a9a484a87277989b88f28213faa222
hkle2 <minnie110598@gmail.com>
2024-06-09 12:52:31 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Python3/0/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/Python3/0/181880. 1로 만들기/1로 만들기.py
@@ -1,13 +1,13 @@
 def solution(num_list):
     answer = 0
     for i in num_list:
-        while True:
-            if i == 1:
-                break
-            elif i % 2 == 0:
-                i = i / 2
-                answer += 1
-            else:
-                i = (i - 1) / 2
-                answer += 1
+        while i > 1:
+            i //= 2
+            answer += 1
+    return answer
+
+def solution(num_list):
+    answer = 0
+    for i in num_list:
+        answer += len(bin(i)) - 3
     return answer
