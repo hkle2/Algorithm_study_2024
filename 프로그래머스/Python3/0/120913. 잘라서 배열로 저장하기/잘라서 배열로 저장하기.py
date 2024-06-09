dcf0224672f385ea96c65b7d8682d99d29298339
hkle2 <minnie110598@gmail.com>
2024-06-09 23:38:19 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Python3/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
+++ 프로그래머스/Python3/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
@@ -9,3 +9,9 @@ def solution(my_str, n):
     if s:
         answer.append(s)
     return answer
+
+def solution(my_str, n):
+    answer = []
+    for i in range(0, len(my_str), n):
+        answer.append(my_str[i:i+n])
+    return answer
