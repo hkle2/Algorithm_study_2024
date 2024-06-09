576f031bab7cb85ed143fd9a0cf83f09844a99d6
hkle2 <minnie110598@gmail.com>
2024-06-09 21:49:08 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/Python3/0/120885. 이진수 더하기/이진수 더하기.py
@@ -0,0 +1,9 @@
+def solution(bin1, bin2):
+    answer1 = 0
+    answer2 = 0
+    for i, j in enumerate(bin1[::-1]):
+        answer1 += 2**int(i) * int(j)
+    for i, j in enumerate(bin2[::-1]):
+        answer2 += 2**int(i) * int(j)
+    answer = bin(answer1 + answer2)[2:]
+    return answer
