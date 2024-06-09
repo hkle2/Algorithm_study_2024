4953a129d1340a0feaaa19f8fbebd0f24b4bc674
hkle2 <minnie110598@gmail.com>
2024-06-09 19:59:12 +0900

    [level 0] Title: 2의 영역, Time: 0.82 ms, Memory: 15.8 MB -BaekjoonHub

--- 프로그래머스/Python3/0/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/Python3/0/181894. 2의 영역/2의 영역.py
@@ -9,3 +9,10 @@ def solution(arr):
     else:
         answer = [-1]
     return answer
+
+def solution(arr):
+    if 2 not in arr:
+        answer = [-1]
+    else:
+        answer = arr[arr.index(2):len(arr)-arr[::-1].index(2)]
+    return answer
