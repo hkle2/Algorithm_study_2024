2ffc30a9e1f4784fb730c07957b338020ed391a9
hkle2 <minnie110598@gmail.com>
2024-06-10 00:49:46 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/Python3/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    answer = [0] * 52
+    for i in my_string:
+        print(ord(i))
+        if i.isupper():
+            answer[ord(i) - 65] += 1
+        else:
+            answer[ord(i) - 97 + 26] += 1
+    return answer
