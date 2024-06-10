d4f4ab2c5540019139fb029b13b1eb2921225c85
hkle2 <minnie110598@gmail.com>
2024-06-10 10:42:34 +0900

    Time: 84 ms (80.31%), Space: 21 MB (70.16%) - LeetHub

--- LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,4 +1,25 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        answer = []
+        nums_dict = {}
+        for num in nums:
+            if num not in nums_dict:
+                nums_dict[num] = 0
+            nums_dict[num] += 1
+        sorted_keys = sorted(nums_dict, key=lambda x: nums_dict[x], reverse=True)
+        for i in range(k):
+            answer.append(sorted_keys[i])
+        return answer
+
 from collections import Counter
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        nums_counter = Counter(nums)
+        return [x[0] for x in nums_counter.most_common()[:k]]
+
+from collections import Counter
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         answer = []
