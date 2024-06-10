53f808edca1a4776765f122e26682849532a3229
hkle2 <minnie110598@gmail.com>
2024-06-11 01:42:26 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/Python3/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -0,0 +1,9 @@
+def solution(sides):
+    answer = 0
+    sides.sort()
+    a, b = sides
+    for i in range(b-a+1, b+1):
+        answer += 1
+    for i in range(b+1, a+b):
+        answer += 1
+    return answer
