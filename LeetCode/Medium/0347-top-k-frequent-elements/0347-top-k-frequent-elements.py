c810b64951c4e35ed8bacc9913f3ad97f56ac2fb
hkle2 <minnie110598@gmail.com>
2024-06-10 09:41:17 +0900

    Time: 91 ms (44.25%), Space: 21 MB (70.16%) - LeetHub

--- LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,9 +3,7 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         answer = []
         num_dict = Counter(nums)
-        nums_dict = dict(sorted(num_dict.items(), key=lambda x: x[1], reverse=True))
-        for i, (key, value) in enumerate(nums_dict.items()):
-            if i < k:
-                answer.append(key)
+        count = sorted(num_dict.items(), key=lambda x: x[1], reverse=True)
+        for i in range(k):
+            answer.append(count[i][0])
         return answer
-        
