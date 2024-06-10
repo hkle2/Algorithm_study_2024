b9f2f86c82d38701e2e0f7538013a59feda275f4
hkle2 <minnie110598@gmail.com>
2024-06-10 14:44:53 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/Python3/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/Python3/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,11 +1,22 @@
+# def solution(my_string):
+#     l = my_string.split()
+#     answer = int(l[0])
+#     for i in range(1, len(l)):
+#         if l[i] == "+":
+#             answer += int(l[i + 1])
+#         elif l[i] == "-":
+#             answer -= int(l[i + 1])
+#         else:
+#             continue
+#     return answer
+
+# def solution(my_string):
+#     answer = eval(my_string)
+#     return answer
+
 def solution(my_string):
-    l = my_string.split()
-    answer = int(l[0])
-    for i in range(1, len(l)):
-        if l[i] == "+":
-            answer += int(l[i + 1])
-        elif l[i] == "-":
-            answer -= int(l[i + 1])
-        else:
-            continue
+    answer = 0
+    l = my_string.replace("- ", "+ -").split(" + ")
+    for i in l:
+        answer += int(i)
     return answer
