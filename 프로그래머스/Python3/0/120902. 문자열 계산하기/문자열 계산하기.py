a0eee2b663b7111b24ef028e999e50878208b794
hkle2 <minnie110598@gmail.com>
2024-06-10 14:46:01 +0900

    Update 문자열 계산하기.py

--- 프로그래머스/Python3/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/Python3/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,18 +1,18 @@
-# def solution(my_string):
-#     l = my_string.split()
-#     answer = int(l[0])
-#     for i in range(1, len(l)):
-#         if l[i] == "+":
-#             answer += int(l[i + 1])
-#         elif l[i] == "-":
-#             answer -= int(l[i + 1])
-#         else:
-#             continue
-#     return answer
+def solution(my_string):
+    l = my_string.split()
+    answer = int(l[0])
+    for i in range(1, len(l)):
+        if l[i] == "+":
+            answer += int(l[i + 1])
+        elif l[i] == "-":
+            answer -= int(l[i + 1])
+        else:
+            continue
+    return answer
 
-# def solution(my_string):
-#     answer = eval(my_string)
-#     return answer
+def solution(my_string):
+    answer = eval(my_string)
+    return answer
 
 def solution(my_string):
     answer = 0
