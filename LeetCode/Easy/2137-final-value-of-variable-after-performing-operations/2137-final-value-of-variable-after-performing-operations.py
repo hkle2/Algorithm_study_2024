841d45c4eaf413817611e3f756cd340e2812fc9a
hkle2 <minnie110598@gmail.com>
2024-06-10 10:18:24 +0900

    Time: 50 ms (78.99%), Space: 16.6 MB (9.29%) - LeetHub

--- LeetCode/Easy/2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.py
+++ LeetCode/Easy/2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.py
@@ -1,6 +1,18 @@
 class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
         answer = 0
+        for operation in operations:
+            # X++ 혹은 ++X일 때는 +1
+            # 아니면 -1
+            if operation == "++X" or operation == "X++":
+                answer += 1
+            else:
+                answer -= 1
+        return answer
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        answer = 0
         for i in operations:
             if "+" in i:
                 answer += 1
