9578419a732638f37f1d2c39f357e098255cc357
hkle2 <minnie110598@gmail.com>
2024-06-11 23:52:05 +0900

    [level 0] Title: 그림 확대, Time: 0.01 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/Python3/0/181836. 그림 확대/그림 확대.py
+++ 프로그래머스/Python3/0/181836. 그림 확대/그림 확대.py
@@ -7,3 +7,10 @@ def solution(picture, k):
         for j in range(k):
             answer.append(s)
     return answer
+
+def solution(picture, k):
+    answer = []
+    for i in range(len(picture)):
+        for j in range(k):
+            answer.append(picture[i].replace(".", "." * k).replace("x", "x" * k))
+    return answer
