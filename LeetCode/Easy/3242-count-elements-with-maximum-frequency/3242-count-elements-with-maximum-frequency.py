fb795810fd69a957830927399d16eb17ee712ba3
hkle2 <minnie110598@gmail.com>
2024-06-11 14:43:58 +0900

    Time: 44 ms (49.78%), Space: 16.5 MB (82.98%) - LeetHub

--- LeetCode/Easy/3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.py
+++ LeetCode/Easy/3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.py
@@ -2,6 +2,18 @@ from collections import Counter
 
 class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
+        nums_counter = Counter(nums)
+        answer = 0
+        m = max(nums_counter.values())
+        for k, v in nums_counter.items():
+            if v == m:
+                answer += v
+        return answer
+
+from collections import Counter
+
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
         answer = 0
         nums_dict = Counter(nums)
         for i, j in nums_dict.items():
