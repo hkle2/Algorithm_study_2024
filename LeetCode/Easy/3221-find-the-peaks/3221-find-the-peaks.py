94a3368a4937c21946e51404a24ee2f8e4cc5d94
hkle2 <minnie110598@gmail.com>
2024-06-11 10:14:35 +0900

    Time: 46 ms (72.96%), Space: 16.4 MB (79.69%) - LeetHub

--- LeetCode/Easy/3221-find-the-peaks/3221-find-the-peaks.py
+++ LeetCode/Easy/3221-find-the-peaks/3221-find-the-peaks.py
@@ -1,6 +1,16 @@
 class Solution:
     def findPeaks(self, mountain: List[int]) -> List[int]:
         answer = []
+        for i in range(len(mountain)):
+            if i == 0 or i == len(mountain) - 1:
+                continue
+            if mountain[i] < mountain[i-1] and mountain[i] > mountain[i+1]:
+                answer.append(i)
+        return answer
+
+class Solution:
+    def findPeaks(self, mountain: List[int]) -> List[int]:
+        answer = []
         for i in range(1, len(mountain)-1):
             if mountain[i-1] < mountain[i] and mountain[i] > mountain[i+1]:
                 answer.append(i)
