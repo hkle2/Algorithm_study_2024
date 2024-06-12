f01fa51d209ca0e8d5968ca7445c613029da5048
hkle2 <minnie110598@gmail.com>
2024-06-12 09:57:20 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/Python3/0/181934. 조건 문자열/조건 문자열.py
+++ 프로그래머스/Python3/0/181934. 조건 문자열/조건 문자열.py
@@ -1,12 +1,9 @@
 def solution(ineq, eq, n, m):
     answer = 0
-    if n > m:
-        if (ineq == ">" and eq == "=") or (ineq == ">" and eq == "!"):
-            answer = 1
-    elif n < m:
-        if (ineq == "<" and eq == "=") or (ineq == "<" and eq == "!"):
-            answer = 1
-    else:
-        if (ineq == ">" and eq == "=") or (ineq == "<" and eq == "="):
-            answer = 1
+    if n > m and ineq == ">":
+        answer = 1
+    elif n < m and ineq == "<":
+        answer = 1
+    elif n == m and eq == "=":
+        answer = 1
     return answer
