ad9f83f9611d5edd7cac13dae0cdb994009cecf2
hkle2 <minnie110598@gmail.com>
2024-06-12 10:03:32 +0900

    Update 2639-separate-the-digits-in-an-array.py

--- LeetCode/Easy/2639-separate-the-digits-in-an-array/2639-separate-the-digits-in-an-array.py
+++ LeetCode/Easy/2639-separate-the-digits-in-an-array/2639-separate-the-digits-in-an-array.py
@@ -1,14 +1,11 @@
-# class Solution:
-#     def separateDigits(self, nums: List[int]) -> List[int]:
-#         answer = []
-#         for num in nums:
-#             num_str = str(num)
-#             for c in num_str:
-#                 print(c, type(c))
-#                 # 숫자를 문자로 바꿔서 한글자씩 읽어오는데 성공!
-#                 # 이를 다시 숫자로 바꿔서 answer에 append
-#                 answer.append(int(s[j]))
-#         return answer
+class Solution:
+    def separateDigits(self, nums: List[int]) -> List[int]:
+        answer = []
+        for num in nums:
+            num_str = str(num)
+            for c in num_str:
+                answer.append(int(s[j]))
+        return answer
 
 class Solution:
     def separateDigits(self, nums: List[int]) -> List[int]:
