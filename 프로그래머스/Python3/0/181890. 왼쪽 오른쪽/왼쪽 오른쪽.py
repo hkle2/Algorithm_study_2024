19dab0d96682b408773fd9811e535146d02d4b34
hkle2 <minnie110598@gmail.com>
2024-06-12 09:42:51 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/Python3/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/Python3/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -1,11 +1,7 @@
 def solution(str_list):
     for i in range(len(str_list)):
         if str_list[i] == "l":
-            answer = str_list[:i]
-            break
+            return str_list[:i]
         elif str_list[i] == "r":
-            answer = str_list[i+1:]
-            break
-        else:
-            answer = []
-    return answer
+            return str_list[i+1:]
+    return []
