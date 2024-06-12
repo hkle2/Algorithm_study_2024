ce041443ba1ed20ddd41c7d1d9723c78f7a3711d
hkle2 <minnie110598@gmail.com>
2024-06-12 10:06:04 +0900

    Time: 75 ms (79.17%), Space: 16.6 MB (83.55%) - LeetHub

--- LeetCode/Easy/2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
+++ LeetCode/Easy/2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
@@ -2,6 +2,18 @@ from collections import Counter
 
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
+        nums_counter = Counter(nums)
+        answer = True
+        for key, value in nums_counter.items():
+            if value % 2 != 0:
+                answer = False
+                break
+        return answer
+
+from collections import Counter
+
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
         answer = True
         nums_dict = Counter(nums)
         for i in nums_dict.values():
