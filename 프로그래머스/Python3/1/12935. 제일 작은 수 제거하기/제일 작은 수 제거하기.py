1953c8b2a0de9e34fb76895a9a44462778761eaf
hkle2 <minnie110598@gmail.com>
2024-06-13 15:33:58 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/Python3/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
+++ 프로그래머스/Python3/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -3,3 +3,10 @@ def solution(arr):
     if not arr:
         arr.append(-1)
     return arr
+
+def solution(arr):
+    answer = sorted(arr)
+    arr.remove(answer[0])
+    if not arr:
+        arr.append(-1)
+    return arr
