074acdea4dd83a574991d1013b8af0f37cd9cd17
hkle2 <minnie110598@gmail.com>
2024-06-13 17:44:41 +0900

    [level 0] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Python3/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/Python3/0/181932. 코드 처리하기/코드 처리하기.py
@@ -15,3 +15,22 @@ def solution(code):
     if ret == "":
         ret = "EMPTY"
     return ret
+
+def solution(code):
+    ret = ""
+    mode = 0
+    for idx in range(len(code)):
+        if code[idx] == "1":
+            mode ^= 1
+        else:
+            if idx % 2 == mode:
+                ret += code[idx]
+    if ret == "":
+        ret = "EMPTY"
+    return ret
+
+def solution(code):
+    answer = "".join(code.split("1"))[::2]
+    if not answer:
+        answer = "EMPTY"
+    return answer
