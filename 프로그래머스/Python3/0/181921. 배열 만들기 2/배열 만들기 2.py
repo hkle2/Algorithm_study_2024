437547be90f94e29a2f6ba84e2172b9c56b165cb
hkle2 <minnie110598@gmail.com>
2024-06-13 16:49:09 +0900

    [level 0] Title: 배열 만들기 2, Time: 29.46 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Python3/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/Python3/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,12 +1,21 @@
 def solution(l, r):
     answer = []
     for i in range(l, r+1):
-        a = True
         s = str(i)
+        a = True
         for j in s:
             if int(j) % 5 != 0:
                 a = False
         if a == True:
+            answer.append(i)
+    if not answer:
+        answer.append(-1)
+    return answer
+
+def solution(l, r):
+    answer = []
+    for i in range(l, r+1):
+        if not set(str(i)) - set(["0", "5"]):
             answer.append(i)
     if not answer:
         answer.append(-1)
