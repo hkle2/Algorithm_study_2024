3ebc9924644bfcc652281ad79a8a22e3dd2aa453
hkle2 <minnie110598@gmail.com>
2024-06-14 18:32:52 +0900

    [level 2] Title: 피보나치 수, Time: 585.06 ms, Memory: 439 MB -BaekjoonHub

--- 프로그래머스/Python3/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/Python3/2/12945. 피보나치 수/피보나치 수.py
@@ -1,6 +1,15 @@
 def solution(n):
     a, b = 0, 1
-    for i in range(n):
-        a, b = b, a+b
+    for i in range(1, n+1):
+        a, b = b, a + b
     answer = a % 1234567
+    return answer
+
+def solution(n):
+    dp = [0] * (n + 1)
+    dp[0] = 0
+    dp[1] = 1
+    for i in range(2, n+1):
+        dp[i] = dp[i - 2] + dp[i - 1]
+    answer = dp[n] % 1234567
     return answer
