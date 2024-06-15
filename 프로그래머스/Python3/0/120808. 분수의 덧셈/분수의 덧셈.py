edba5554dc748a40f9a5deb0967e55c4e017d9c5
hkle2 <minnie110598@gmail.com>
2024-06-15 23:46:07 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Python3/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/Python3/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,11 +1,37 @@
+# def solution(numer1, denom1, numer2, denom2):
+#     answer = []
+#     m = numer1 * denom2 + numer2 * denom1
+#     n = denom1 * denom2
+#     for i in range(m, 1, -1):
+#         if m % i == 0 and n % i == 0:
+#             m //= i
+#             n //= i
+#     answer.append(m)
+#     answer.append(n)
+#     return answer
+
+import math
+
 def solution(numer1, denom1, numer2, denom2):
     answer = []
     m = numer1 * denom2 + numer2 * denom1
     n = denom1 * denom2
-    for i in range(m, 1, -1):
-        if m % i == 0 and n % i == 0:
-            m //= i
-            n //= i
+    gcd = math.gcd(m, n)
+    m //= gcd
+    n //= gcd
     answer.append(m)
     answer.append(n)
     return answer
+
+# import math
+
+# def solution(numer1, denom1, numer2, denom2):
+#     answer = []
+#     m = numer1 * denom2 + numer2 * denom1
+#     n = denom1 * denom2
+#     gcd = math.gcd(m, n)
+#     m //= gcd
+#     n //= gcd
+#     answer.append(m)
+#     answer.append(n)
+#     return answer
