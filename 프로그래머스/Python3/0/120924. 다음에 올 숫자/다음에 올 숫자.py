7b171deb533a20aae7aa68682b54c76ca97a34be
hkle2 <minnie110598@gmail.com>
2024-06-15 21:17:10 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/Python3/0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/Python3/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,7 +1,14 @@
 def solution(common):
-    answer = 0
     if common[2] - common[1] == common[1] - common[0]:
         answer = common[-1] + common[2] - common[1]
     else:
         answer = common[-1] * common[2] / common[1]
+    return answer
+
+def solution(common):
+    a, b, c = common[:3]
+    if b - a == c - b:
+        answer = common[-1] + (b - a)
+    else:
+        answer = common[-1] * (b / a)
     return answer
