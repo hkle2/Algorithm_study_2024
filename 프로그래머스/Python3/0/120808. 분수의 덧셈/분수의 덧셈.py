1a5c5991dd9d24de2acd7af51ec5ca5848166e51
hkle2 <minnie110598@gmail.com>
2024-06-15 23:49:49 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/Python3/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/Python3/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,14 +1,14 @@
-# def solution(numer1, denom1, numer2, denom2):
-#     answer = []
-#     m = numer1 * denom2 + numer2 * denom1
-#     n = denom1 * denom2
-#     for i in range(m, 1, -1):
-#         if m % i == 0 and n % i == 0:
-#             m //= i
-#             n //= i
-#     answer.append(m)
-#     answer.append(n)
-#     return answer
+def solution(numer1, denom1, numer2, denom2):
+    answer = []
+    m = numer1 * denom2 + numer2 * denom1
+    n = denom1 * denom2
+    for i in range(m, 1, -1):
+        if m % i == 0 and n % i == 0:
+            m //= i
+            n //= i
+    answer.append(m)
+    answer.append(n)
+    return answer
 
 import math
 
@@ -23,15 +23,11 @@ def solution(numer1, denom1, numer2, denom2):
     answer.append(n)
     return answer
 
-# import math
+from fractions import Fraction
 
-# def solution(numer1, denom1, numer2, denom2):
-#     answer = []
-#     m = numer1 * denom2 + numer2 * denom1
-#     n = denom1 * denom2
-#     gcd = math.gcd(m, n)
-#     m //= gcd
-#     n //= gcd
-#     answer.append(m)
-#     answer.append(n)
-#     return answer
+def solution(numer1, denom1, numer2, denom2):
+    answer = []
+    f = Fraction(numer1, denom1) + Fraction(numer2, denom2)
+    answer.append(f.numerator)
+    answer.append(f.denominator)
+    return answer
