ab9d20f5e7167604f0b581ad0b59350ac02ffdf6
hkle2 <minnie110598@gmail.com>
2024-06-15 19:37:11 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 2.69 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/Python3/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/Python3/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,11 +1,11 @@
 def solution(friends, gifts):
     answer = []
     record = {}
-    for g in gifts:
-        id1, id2 = g.split()
+    score = {}
+    for gift in gifts:
+        id1, id2 = gift.split()
         record[id1] = record.get(id1, {})
         record[id1][id2] = record[id1].get(id2, 0) + 1
-    score = {}
     for f in friends:
         if f in record:
             score[f] = sum(record.get(f).values())
@@ -19,7 +19,34 @@ def solution(friends, gifts):
                 score[s1] -= record.get(s2).get(s1, 0)
     for f1 in friends:
         gift = 0
-        tmp = 0
+        for f2 in friends:
+            if f1 == f2:
+                continue
+            tmp = record.get(f1, {}).get(f2, 0) - record.get(f2, {}).get(f1, 0)
+            if tmp > 0:
+                gift += 1
+            elif tmp == 0 and score[f1] > score[f2]:
+                gift += 1
+        answer.append(gift)
+    return max(answer)
+
+def solution(friends, gifts):
+    answer = []
+    record = {}
+    score = {}
+    for gift in gifts:
+        id1, id2 = gift.split()
+        record[id1] = record.get(id1, {})
+        record[id1][id2] = record[id1].get(id2, 0) + 1
+        record[id2] = record.get(id2, {})
+        record[id2][id1] = record[id2].get(id1, 0) - 1
+    for f in friends:
+        if f in record:
+            score[f] = sum(record.get(f).values())
+        else:
+            score[f] = 0
+    for f1 in friends:
+        gift = 0
         for f2 in friends:
             if f1 == f2:
                 continue
