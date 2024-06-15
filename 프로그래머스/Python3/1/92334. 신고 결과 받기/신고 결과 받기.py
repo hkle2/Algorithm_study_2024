4dfde58aecfd71748f1875732061125e63c66bdd
hkle2 <minnie110598@gmail.com>
2024-06-15 17:52:45 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Python3/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/Python3/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -7,10 +7,10 @@ def solution(id_list, report, k):
         d_user[i] = []
         d_spam[i] = 0
     for r in report:
-        u, s = r.split()
-        if s not in d_user[u]:
-            d_user[u] += [s]
-            d_spam[s] += 1
+        id1, id2 = r.split()
+        if id2 not in d_user[id1]:
+            d_user[id1] += [id2]
+            d_spam[id2] += 1
     for key, value in d_spam.items():
         if value >= k:
             spam.append(key)
@@ -20,4 +20,21 @@ def solution(id_list, report, k):
             if value[i] in spam:
                 a += 1
         answer.append(a)
+    return answer
+
+def solution(id_list, report, k):
+    answer = []
+    spam = {}
+    mail = {}
+    for r in report:
+        id1, id2 = r.split()
+        tmp = spam.get(id2, set())
+        tmp.add(id1)
+        spam[id2] = tmp
+    for v in spam.values():
+        if len(v) >= k:
+            for id in v:
+                mail[id] = mail.get(id, 0) + 1
+    for id in id_list:
+        answer.append(mail.get(id, 0))
     return answer
