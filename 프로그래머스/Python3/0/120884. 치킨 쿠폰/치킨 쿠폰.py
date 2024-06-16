035e1562733cbf11e8967b71d5f942b7e2837a37
hkle2 <minnie110598@gmail.com>
2024-06-16 21:45:17 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/Python3/0/120884. 치킨 쿠폰/치킨 쿠폰.py
+++ 프로그래머스/Python3/0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -1,21 +1,20 @@
-# def solution(chicken):
-#     answer = 0
-#     while chicken >= 10:
-#         answer += chicken // 10
-#         chicken = chicken // 10 + chicken % 10
-#     return answer
-
-# def solution(chicken):
-#     answer = 0
-#     while chicken >= 10:
-#         a, b = divmod(chicken, 10)
-#         answer += a
-#         chicken = a + b
-#     return answer
-
 def solution(chicken):
-    answer = (max(chicken, 1) - 1) // 9
+    answer = 0
+    while chicken >= 10:
+        answer += chicken // 10
+        chicken = chicken // 10 + chicken % 10
     return answer
 
-# def solution(chicken):
-#     return int(chicken * 0.11111111111)
+def solution(chicken):
+    answer = 0
+    while chicken >= 10:
+        a, b = divmod(chicken, 10)
+        answer += a
+        chicken = a + b
+    return answer
+
+def solution(chicken):
+    return (max(chicken, 1) - 1) // 9
+
+def solution(chicken):
+    return int(chicken * 0.111111)
