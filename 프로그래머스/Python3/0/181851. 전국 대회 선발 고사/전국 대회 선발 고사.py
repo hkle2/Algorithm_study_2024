c3126d3395274cbf284e4c2dbf02737ad3ed3c7d
hkle2 <minnie110598@gmail.com>
2024-06-16 17:07:37 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Python3/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/Python3/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,7 +1,11 @@
 def solution(rank, attendance):
     answer = []
-    for i, r in enumerate(attendance):
-        if r:
+    for i, a in enumerate(attendance):
+        if a:
             answer.append([rank[i], i])
     answer = sorted(answer)
+    return 10000 * answer[0][1] + 100 * answer[1][1] + answer[2][1]
+
+def solution(rank, attendance):
+    answer = sorted([[rank[i], i] for i, a in enumerate(attendance) if a])
     return 10000 * answer[0][1] + 100 * answer[1][1] + answer[2][1]
