1864d3ea07d757baba0646a4b249382b9d799501
hkle2 <minnie110598@gmail.com>
2024-06-16 15:57:58 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/Python3/0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/Python3/0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,13 +1,8 @@
 def solution(id_pw, db):
-    answer = ''
     id, pw = id_pw[0], id_pw[1]
     for ids, pws in db:
         if id == ids and pw == pws:
-            answer = "login"
+            return "login"
         elif id == ids and pw != pws:
-            answer = "wrong pw"
-        else:
-            continue
-    if not answer:
-        answer = "fail"
-    return answer
+            return "wrong pw"
+    return "fail"
