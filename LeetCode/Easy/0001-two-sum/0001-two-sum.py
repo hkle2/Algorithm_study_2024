b19e9d278b8d3c638c948e604174efb4c8fe85e3
hkle2 <minnie110598@gmail.com>
2024-06-17 23:10:44 +0900

    Time: 1766 ms (15.03%), Space: 17.2 MB (94.34%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.py
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.py
@@ -7,3 +7,11 @@ class Solution:
                     continue
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        length = len(nums)
+        for i, num1 in enumerate(nums):
+            for j in range(i+1, length):
+                if num1 + nums[j] == target:
+                    return [i, j] 
