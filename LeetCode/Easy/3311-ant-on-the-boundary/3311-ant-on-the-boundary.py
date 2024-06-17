73e0c38ca415dffb7b4baabdd9ddb9af63651c7c
hkle2 <minnie110598@gmail.com>
2024-06-17 10:07:05 +0900

    Time: 29 ms (99.8%), Space: 16.4 MB (79.41%) - LeetHub

--- LeetCode/Easy/3311-ant-on-the-boundary/3311-ant-on-the-boundary.py
+++ LeetCode/Easy/3311-ant-on-the-boundary/3311-ant-on-the-boundary.py
@@ -1,6 +1,16 @@
 class Solution:
     def returnToBoundaryCount(self, nums: List[int]) -> int:
         answer = 0
+        location = 0
+        for num in nums:
+            location += num
+            if location == 0:
+                answer += 1
+        return answer
+
+class Solution:
+    def returnToBoundaryCount(self, nums: List[int]) -> int:
+        answer = 0
         boundary = 0
         for i in nums:
             boundary += i
