1a4c7277d2651ff8b53056d9cb3709c052fa0440
hkle2 <minnie110598@gmail.com>
2024-06-17 09:13:34 +0900

    Time: 217 ms (5.03%), Space: 16.9 MB (70.05%) - LeetHub

--- LeetCode/Easy/2190-count-common-words-with-one-occurrence/2190-count-common-words-with-one-occurrence.py
+++ LeetCode/Easy/2190-count-common-words-with-one-occurrence/2190-count-common-words-with-one-occurrence.py
@@ -3,10 +3,8 @@ from collections import Counter
 class Solution:
     def countWords(self, words1: List[str], words2: List[str]) -> int:
         answer = 0
-        cnt1 = Counter(words1)
-        cnt2 = Counter(words2)
-        for w1, i in cnt1.items():
+        for w, i in Counter(words1).items():
             if i == 1:
-                if cnt2.get(w1, 0) == 1:
+                if Counter(words2).get(w, 0) == 1:
                     answer += 1
         return answer
