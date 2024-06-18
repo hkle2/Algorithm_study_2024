cef572cbd00a560e74d5642b79bc2438de23c1d1
hkle2 <minnie110598@gmail.com>
2024-06-18 09:16:28 +0900

    Time: 48 ms (50.18%), Space: 16.7 MB (29.96%) - LeetHub

--- LeetCode/Easy/1293-three-consecutive-odds/1293-three-consecutive-odds.py
+++ LeetCode/Easy/1293-three-consecutive-odds/1293-three-consecutive-odds.py
@@ -3,7 +3,7 @@ class Solution:
         for i in range(len(arr)-2):
             cnt = 0
             for j in range(3):
-                if arr[i+j] % 2 != 0:
+                if arr[i + j] % 2 != 0:
                     cnt += 1
             if cnt == 3:
                 return True
