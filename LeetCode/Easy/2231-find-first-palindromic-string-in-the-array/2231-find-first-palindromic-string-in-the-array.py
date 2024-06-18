16fee7c039261370cc1c3a773164909d7c974c96
hkle2 <minnie110598@gmail.com>
2024-06-18 09:30:44 +0900

    Time: 96 ms (8.52%), Space: 16.5 MB (89.32%) - LeetHub

--- LeetCode/Easy/2231-find-first-palindromic-string-in-the-array/2231-find-first-palindromic-string-in-the-array.py
+++ LeetCode/Easy/2231-find-first-palindromic-string-in-the-array/2231-find-first-palindromic-string-in-the-array.py
@@ -1,6 +1,17 @@
 class Solution:
     def firstPalindrome(self, words: List[str]) -> str:
-        for i in words:
-            if i == i[::-1]:
-                return i
-        return ""
+        answer = ""
+        for word in words:
+            # 만약 word가 palindrome이라면 answer에 word를 지정하고 break!
+            # word[::-1]
+            if word == word[::-1]:
+                answer = word
+                break
+        return answer
+
+# class Solution:
+#     def firstPalindrome(self, words: List[str]) -> str:
+#         for i in words:
+#             if i == i[::-1]:
+#                 return i
+#         return ""
