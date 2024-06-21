d2ba7928cd43b041859708f948af5c2b3d976767
hkle2 <minnie110598@gmail.com>
2024-06-21 10:11:25 +0900

    Time: 667 ms (81.47%), Space: 0B (100%) - LeetHub

--- LeetCode/Easy/2041-the-latest-login-in-2020/2041-the-latest-login-in-2020.sql
+++ LeetCode/Easy/2041-the-latest-login-in-2020/2041-the-latest-login-in-2020.sql
@@ -3,8 +3,3 @@ SELECT user_id, MAX(time_stamp) AS last_stamp
 FROM Logins
 WHERE time_stamp LIKE '2020-%'
 GROUP BY user_id;
-
--- SELECT user_id, MAX(time_stamp) AS last_stamp
--- FROM Logins
--- WHERE EXTRACT(YEAR FROM time_stamp) = 2020
--- GROUP BY user_id;
