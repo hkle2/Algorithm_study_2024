76e3ce8ca4632f39e7ef644980a7d4519f7ae66a
hkle2 <minnie110598@gmail.com>
2024-06-24 09:26:50 +0900

    Time: 526 ms (88.86%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
+++ LeetCode/Medium/1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
@@ -1,8 +1,4 @@
 # Write your MySQL query statement below
--- SELECT A.customer_id, A.product_key
--- FROM Customer AS A INNER JOIN Product AS B ON A.product_key = B.product_key
--- GROUP BY customer_id, product_key
-
 SELECT customer_id
 FROM Customer
 GROUP BY customer_id
