3066ce2d58dde5de12dc77830ebee7a76c9d4c9d
hkle2 <minnie110598@gmail.com>
2024-06-24 10:20:16 +0900

    Time: 328 ms (87.66%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ LeetCode/Medium/0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,4 +1,14 @@
 # Write your MySQL query statement below
+-- WITH report AS
+-- (SELECT managerId, COUNT(*) AS report_cnt
+-- FROM Employee
+-- GROUP BY managerId
+-- HAVING report_cnt >= 5)
+-- SELECT Employee.name
+-- FROM report
+-- INNER JOIN Employee
+-- ON report.managerId = Employee.id
+
 SELECT name
 FROM Employee
 WHERE id IN
