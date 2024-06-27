edff654fd7e2168aa15b4a520c21c41fbb554dd8
hkle2 <minnie110598@gmail.com>
2024-06-27 10:05:45 +0900

    Time: 39 ms (25.83%), Space: 16.3 MB (99.92%) - LeetHub

--- LeetCode/Easy/2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.py
+++ LeetCode/Easy/2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.py
@@ -1,5 +1,10 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
+        loc = word.find(ch)
+        return word[:loc+1][::-1] + word[loc+1:]
+
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
         n = word.find(ch)
         if n != -1:
             return word[n::-1] + word[n+1:]
