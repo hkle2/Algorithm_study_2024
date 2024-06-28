2cabbf155d99a404eb781925d41d4e0e17218a28
hkle2 <minnie110598@gmail.com>
2024-06-28 09:31:51 +0900

    Time: 57 ms (97.41%), Space: 16.5 MB (79.31%) - LeetHub

--- LeetCode/Easy/2785-semi-ordered-permutation/2785-semi-ordered-permutation.py
+++ LeetCode/Easy/2785-semi-ordered-permutation/2785-semi-ordered-permutation.py
@@ -1,10 +1,8 @@
 class Solution:
     def semiOrderedPermutation(self, nums: List[int]) -> int:
-        answer = 0
         n = len(nums)
         s, e = nums.index(1), nums.index(n)
         if s < e:
-            answer = s + (n - 1) - e
+            return s + (n - 1) - e
         else:
-            answer = s + (n - 2) - e
-        return answer
+            return s + (n - 2) - e
