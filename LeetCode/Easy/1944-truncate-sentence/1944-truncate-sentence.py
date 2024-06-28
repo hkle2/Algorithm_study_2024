900356e9ed57f12dd5b656383ec9418adf2481e5
hkle2 <minnie110598@gmail.com>
2024-06-28 10:01:51 +0900

    Time: 38 ms (34.31%), Space: 16.4 MB (81.47%) - LeetHub

--- LeetCode/Easy/1944-truncate-sentence/1944-truncate-sentence.py
+++ LeetCode/Easy/1944-truncate-sentence/1944-truncate-sentence.py
@@ -1,4 +1,9 @@
 class Solution:
     def truncateSentence(self, s: str, k: int) -> str:
+        word_list = s.split(" ")
+        return " ".join(word_list[:k])
+
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
         l = s.split()[:k]
         return " ".join(l)
