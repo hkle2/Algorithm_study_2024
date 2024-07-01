90f7c03babc039076d231088cf08e2b1d51b5800
hkle2 <minnie110598@gmail.com>
2024-07-01 10:03:10 +0900

    Time: 52 ms (42.32%), Space: 16.5 MB (75.44%) - LeetHub

--- LeetCode/Easy/1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.py
+++ LeetCode/Easy/1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.py
@@ -2,16 +2,14 @@ from collections import Counter
 
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
-        # sorted, counter
         c = Counter(nums)
-        # 큰 숫자가 앞으로, 작은 숫자가 뒤로
         return sorted(nums, key=lambda x: (c[x], -x))
 
-# from collections import Counter
+from collections import Counter
 
-# class Solution:
-#     def frequencySort(self, nums: List[int]) -> List[int]:
-#         answer = []
-#         count_num = Counter(nums)
-#         answer = sorted(nums, key=lambda x: (count_num[x], -x))
-#         return answer
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        answer = []
+        count_num = Counter(nums)
+        answer = sorted(nums, key=lambda x: (count_num[x], -x))
+        return answer
