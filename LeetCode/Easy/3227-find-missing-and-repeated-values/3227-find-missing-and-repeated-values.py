dbea7626bff00d9750ea4b076e0d4a55f5ae79b6
hkle2 <minnie110598@gmail.com>
2024-07-01 09:36:24 +0900

    Time: 133 ms (32.54%), Space: 16.9 MB (49.08%) - LeetHub

--- LeetCode/Easy/3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.py
+++ LeetCode/Easy/3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.py
@@ -1,14 +1,33 @@
+from collections import Counter
+
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
-        answer = []
-        l = []
+        # 2중 for문을 사용할 것
+        c = Counter()
         for i in range(len(grid)):
-            for j in range(len(grid)):
-                if grid[i][j] not in l:
-                    l.append(grid[i][j])
-                else:
-                    answer.append(grid[i][j])
+            for j in range(len(grid[0])):
+                c[grid[i][j]] += 1
+        answer = [-1, -1]
+        # 1부터 n^2까지 반복을 진행하면서
+        # 2번 등장한 값과 0번 등장한 값을 찾아서 answer에 지정하고 리턴
         for i in range(1, len(grid)**2+1):
-            if l.count(i) == 0:
-                answer.append(i)
+            if c[i] == 2:
+                answer[0] = i
+            if c[i] == 0:
+                answer[1] = i
         return answer
+
+# class Solution:
+#     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+#         answer = []
+#         l = []
+#         for i in range(len(grid)):
+#             for j in range(len(grid)):
+#                 if grid[i][j] not in l:
+#                     l.append(grid[i][j])
+#                 else:
+#                     answer.append(grid[i][j])
+#         for i in range(1, len(grid)**2+1):
+#             if l.count(i) == 0:
+#                 answer.append(i)
+#         return answer
