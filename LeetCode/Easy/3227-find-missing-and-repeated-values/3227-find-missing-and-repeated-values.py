183b5fef68721528149b81b8d4275389bbb6ae92
hkle2 <minnie110598@gmail.com>
2024-07-01 10:04:47 +0900

    Time: 1584 ms (5.01%), Space: 16.9 MB (78.15%) - LeetHub

--- LeetCode/Easy/3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.py
+++ LeetCode/Easy/3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.py
@@ -1,21 +1,19 @@
-# from collections import Counter
+from collections import Counter
 
-# class Solution:
-#     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
-#         # 2중 for문을 사용할 것
-#         c = Counter()
-#         for i in range(len(grid)):
-#             for j in range(len(grid[0])):
-#                 c[grid[i][j]] += 1
-#         answer = [-1, -1]
-#         # 1부터 n^2까지 반복을 진행하면서
-#         # 2번 등장한 값과 0번 등장한 값을 찾아서 answer에 지정하고 리턴
-#         for i in range(1, len(grid)**2+1):
-#             if c[i] == 2:
-#                 answer[0] = i
-#             if c[i] == 0:
-#                 answer[1] = i
-#         return answer
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        c = Counter()
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                c[grid[i][j]] += 1
+        answer = [-1, -1]
+        n = len(grid)
+        for i in range(1, n*n+1):
+            if c[i] == 2:
+                answer[0] = i
+            if c[i] == 0:
+                answer[1] = i
+        return answer
 
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
