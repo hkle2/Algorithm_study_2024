a4275f30248d6668344514f185dc59376fd58c21
hkle2 <minnie110598@gmail.com>
2024-07-02 10:02:22 +0900

    Time: 44 ms (7.86%), Space: 16.5 MB (83.13%) - LeetHub

--- LeetCode/Easy/2401-count-asterisks/2401-count-asterisks.py
+++ LeetCode/Easy/2401-count-asterisks/2401-count-asterisks.py
@@ -1,7 +1,18 @@
 class Solution:
     def countAsterisks(self, s: str) -> int:
+        # split, count, for, if
         answer = 0
-        l = s.split("|")
-        for i in range(0, len(l)+1, 2):
-            answer += l[i].count("*")
+        word_list = s.split("|")
+        for i in range(0, len(word_list), 2):
+            # word_list[i] 안에 포함되어 있는 *의 개수 세어서
+            # answer에다가 추가하기
+            answer += word_list[i].count("*")
         return answer
+
+# class Solution:
+#     def countAsterisks(self, s: str) -> int:
+#         answer = 0
+#         l = s.split("|")
+#         for i in range(0, len(l)+1, 2):
+#             answer += l[i].count("*")
+#         return answer
