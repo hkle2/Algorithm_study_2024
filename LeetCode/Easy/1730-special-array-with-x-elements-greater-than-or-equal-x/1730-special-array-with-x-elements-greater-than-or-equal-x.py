606ad12126406398731ea4440a6268a0cb574dc6
hkle2 <minnie110598@gmail.com>
2024-07-02 10:08:01 +0900

    Time: 48 ms (13.26%), Space: 16.5 MB (18.72%) - LeetHub

--- LeetCode/Easy/1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.py
+++ LeetCode/Easy/1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,5 +1,19 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
+        nums = sorted(nums, reverse=True)
+        answer = -1
+        for i in range(len(nums)):
+            x = i + 1
+            if i == len(nums) - 1:
+                if nums[i] >= x:
+                    answer = x
+            elif nums[i] >= x and nums[i+1] < x:
+                answer = x
+                break
+        return answer
+
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
         nums.sort()
         n = len(nums)
         for i in range(1, n+1):
