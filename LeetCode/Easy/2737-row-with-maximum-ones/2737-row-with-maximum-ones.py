fd6c1720476886c3438b0ee2efe24aca6b5e3c22
hkle2 <minnie110598@gmail.com>
2024-07-04 10:10:02 +0900

    Time: 737 ms (63.44%), Space: 17.1 MB (44.09%) - LeetHub

--- LeetCode/Easy/2737-row-with-maximum-ones/2737-row-with-maximum-ones.py
+++ LeetCode/Easy/2737-row-with-maximum-ones/2737-row-with-maximum-ones.py
@@ -1,5 +1,24 @@
 class Solution:
     def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
+        data = []
+        for i, row in enumerate(mat):
+            one_cnt = row.count(1)
+            data.append([i, one_cnt])
+        return sorted(data, key=lambda x: (-x[1], x[0]))[0]
+
+class Solution:
+    def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
+        answer = [0, 0]
+        prev_cnt = 0
+        for i, row in enumerate(mat):
+            one_cnt = row.count(1)
+            if one_cnt > prev_cnt:
+                answer = [i, one_cnt]
+                prev_cnt = one_cnt
+        return answer
+
+class Solution:
+    def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
         answer = []
         for i in mat:
             cnt = 0
