c76bbb1867594f74ae195101bfde434bc8148ee6
hkle2 <minnie110598@gmail.com>
2024-07-05 09:46:15 +0900

    Time: 278 ms (40.63%), Space: 16.5 MB (73.68%) - LeetHub

--- LeetCode/Easy/2566-number-of-unequal-triplets-in-array/2566-number-of-unequal-triplets-in-array.py
+++ LeetCode/Easy/2566-number-of-unequal-triplets-in-array/2566-number-of-unequal-triplets-in-array.py
@@ -1,14 +1,14 @@
-# class Solution:
-#     def unequalTriplets(self, nums: List[int]) -> int:
-#        # 3중 for문
-#        answer = 0
-#        for i in range(len(nums)):
-#             for j in range(i+1, len(nums)):
-#                 for k in range(j+1, len(nums)):
-#                     # 세 숫자가 모두 다른 경우에만 answer에 1 더해주기
-#                     if nums[i] != nums[j] and nums[i] != nums[k] and nums[j] != nums[k]:
-#                         answer += 1
-#        return answer
+class Solution:
+    def unequalTriplets(self, nums: List[int]) -> int:
+       # 3중 for문
+       answer = 0
+       for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                for k in range(j+1, len(nums)):
+                    # 세 숫자가 모두 다른 경우에만 answer에 1 더해주기
+                    if nums[i] != nums[j] and nums[i] != nums[k] and nums[j] != nums[k]:
+                        answer += 1
+       return answer
 
 # class Solution:
 #     def unequalTriplets(self, nums: List[int]) -> int:
@@ -22,12 +22,12 @@
 #                             answer += 1
 #         return answer
 
-from itertools import combinations
+# from itertools import combinations
 
-class Solution:
-    def unequalTriplets(self, nums: List[int]) -> int:
-        answer = 0
-        for num in combinations(nums, 3):
-            if len(set(num)) == 3:
-                answer += 1
-        return answer
+# class Solution:
+#     def unequalTriplets(self, nums: List[int]) -> int:
+#         answer = 0
+#         for num in combinations(nums, 3):
+#             if len(set(num)) == 3:
+#                 answer += 1
+#         return answer
