655dc22ebbaacd4c87ff231b038dbfa5364b49ea
hkle2 <minnie110598@gmail.com>
2024-07-05 09:36:43 +0900

    Time: 275 ms (46.53%), Space: 16.4 MB (97.47%) - LeetHub

--- LeetCode/Easy/2566-number-of-unequal-triplets-in-array/2566-number-of-unequal-triplets-in-array.py
+++ LeetCode/Easy/2566-number-of-unequal-triplets-in-array/2566-number-of-unequal-triplets-in-array.py
@@ -1,14 +1,22 @@
 class Solution:
     def unequalTriplets(self, nums: List[int]) -> int:
-        answer = 0
-        # s = set(nums)
-        # print(len(s))
-        n = len(nums)
-        for i in range(n):
-            for j in range(1, n):
-                for k in range(2, n):
-                    if i < j and j < k:
-                        if nums[i] != nums[j] and nums[i] != nums[k] and nums[j] != nums[k]:
-                            answer += 1
+       # 3중 for문
+       answer = 0
+       for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                for k in range(j+1, len(nums)):
+                    if nums[i] != nums[j] and nums[i] != nums[k] and nums[j] != nums[k]:
+                        answer += 1
+       return answer
 
-        return answer
+# class Solution:
+#     def unequalTriplets(self, nums: List[int]) -> int:
+#         answer = 0
+#         n = len(nums)
+#         for i in range(n):
+#             for j in range(1, n):
+#                 for k in range(2, n):
+#                     if i < j and j < k:
+#                         if nums[i] != nums[j] and nums[i] != nums[k] and nums[j] != nums[k]:
+#                             answer += 1
+#         return answer
