c70382629cac657d5f548bc95facc2b4ad5e2172
hkle2 <minnie110598@gmail.com>
2024-07-16 10:14:31 +0900

    Time: 39 ms (50.65%), Space: 16.6 MB (53.83%) - LeetHub

--- LeetCode/Easy/3484-lexicographically-smallest-string-after-a-swap/3484-lexicographically-smallest-string-after-a-swap.py
+++ LeetCode/Easy/3484-lexicographically-smallest-string-after-a-swap/3484-lexicographically-smallest-string-after-a-swap.py
@@ -1,25 +1,17 @@
-# class Solution:
-#     def getSmallestString(self, s: str) -> str:
-#         answer = [s]
-#         for i in range(len(s) - 1):
-#             if int(s[i]) % 2 == int(s[i+1]) % 2:
-#                 swaped = s[:i] + s[i+1] + s[i] + s[i+2:]
-#                 answer.append(swaped)
-#         return sorted(answer)[0]
+class Solution:
+    def getSmallestString(self, s: str) -> str:
+        answer = [s]
+        for i in range(len(s) - 1):
+            if int(s[i]) % 2 == int(s[i+1]) % 2:
+                swaped = s[:i] + s[i+1] + s[i] + s[i+2:]
+                answer.append(swaped)
+        return sorted(answer)[0]
 
 class Solution:
     def getSmallestString(self, s: str) -> str:
-        l = [s]
+        answer = [s]
         for i in range(len(s)-1):
             if int(s[i]) % 2 == int(s[i+1]) % 2:
-                l.append(s[:i] + s[i+1] + s[i] + s[i+2:])
-        l.sort()
-        return l[0]
-
-# class Solution:
-#     def getSmallestString(self, s: str) -> str:
-#         for i in range(len(s)-1):
-#             a, b = int(s[i]), int(s[i+1])
-#             if a % 2 == b % 2 and a < b:
-#                 return s[:i] + s[i+1] + s[i] + s[i+2:]
-#         return s
+                answer.append(s[:i] + s[i+1] + s[i] + s[i+2:])
+        answer.sort()
+        return answer[0]
