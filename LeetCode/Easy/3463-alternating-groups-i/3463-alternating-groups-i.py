ef45d21abcb9a3b906fc2bd6aa29c301c00d1f75
hkle2 <minnie110598@gmail.com>
2024-07-17 10:09:54 +0900

    Time: 38 ms (99.36%), Space: 16.7 MB (10.47%) - LeetHub

--- LeetCode/Easy/3463-alternating-groups-i/3463-alternating-groups-i.py
+++ LeetCode/Easy/3463-alternating-groups-i/3463-alternating-groups-i.py
@@ -1,15 +1,22 @@
-# class Solution:
-#     def numberOfAlternatingGroups(self, colors: List[int]) -> int:
-#         answer = 0
-#         # for문, modulo 사용해줄 것
-#         for i in range(len(colors)):
-#             color_one = colors[i]
-#             color_two = colors[(i+1) % len(colors)]
-#             color_three = colors[(i+2) % len(colors)]
-#             # color_one과 color_two가 서로 다르고
-#             # color_two와 color_three가 서로 다른 경우
-#             # alternating group 조건 성립! answer에 1 더해주기
-#         return answer
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int]) -> int:
+        answer = 0
+        for i in range(len(colors)):
+            color_one = colors[i]
+            color_two = colors[(i+1) % len(colors)]
+            color_three = colors[(i+2) % len(colors)]
+            if color_one != color_two and color_two != color_three:
+                answer += 1
+        return answer
+
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int]) -> int:
+        answer = 0
+        n = len(colors)
+        for i in range(n):
+            if colors[i-1] != colors[i-2] and colors[i-1] != colors[i]:
+                answer += 1
+        return answer
 
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int]) -> int:
@@ -20,19 +27,3 @@ class Solution:
             if a != b and b != c:
                 answer += 1
         return answer
-
-# class Solution:
-#     def numberOfAlternatingGroups(self, colors: List[int]) -> int:
-#         answer = 0
-#         n = len(colors)
-#         for i in range(n):
-#             if i > 0 and i < n - 1:
-#                 if (colors[i] != colors[i+1]) and (colors[i] != colors[i-1]):
-#                     answer += 1
-#             elif i == 0:
-#                 if colors[i] != colors[i+1] and colors[i] != colors[-1]:
-#                     answer += 1
-#             else:
-#                 if colors[i] != colors[0] and colors[i] != colors[i-1]:
-#                     answer += 1
-#         return answer
