47491d63320c68999c945463ca50862168e5eff1
hkle2 <minnie110598@gmail.com>
2024-07-17 09:04:54 +0900

    Time: 51 ms (68.01%), Space: 16.5 MB (66.8%) - LeetHub

---
+++ LeetCode/Easy/3429-special-array-i/3429-special-array-i.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        answer = True
+        for i in range(len(nums)-1):
+            if nums[i] % 2 == nums[i+1] % 2:
+                answer = False
+        return answer
