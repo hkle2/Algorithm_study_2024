2ba6cd69fd03ac3d993d394194bb19cdf3db2ca5
hkle2 <minnie110598@gmail.com>
2024-07-17 09:17:43 +0900

    Time: 51 ms (62.83%), Space: 16.5 MB (50.21%) - LeetHub

--- LeetCode/Easy/3463-alternating-groups-i/3463-alternating-groups-i.py
+++ LeetCode/Easy/3463-alternating-groups-i/3463-alternating-groups-i.py
@@ -3,13 +3,23 @@ class Solution:
         answer = 0
         n = len(colors)
         for i in range(n):
-            if i > 0 and i < n - 1:
-                if (colors[i] != colors[i+1]) and (colors[i] != colors[i-1]):
-                    answer += 1
-            elif i == 0:
-                if colors[i] != colors[i+1] and colors[i] != colors[-1]:
-                    answer += 1
-            else:
-                if colors[i] != colors[0] and colors[i] != colors[i-1]:
-                    answer += 1
+            a, b, c = colors[i], colors[(i+1) % n], colors[(i+2) % n]
+            if a != b and b != c:
+                answer += 1
         return answer
+
+# class Solution:
+#     def numberOfAlternatingGroups(self, colors: List[int]) -> int:
+#         answer = 0
+#         n = len(colors)
+#         for i in range(n):
+#             if i > 0 and i < n - 1:
+#                 if (colors[i] != colors[i+1]) and (colors[i] != colors[i-1]):
+#                     answer += 1
+#             elif i == 0:
+#                 if colors[i] != colors[i+1] and colors[i] != colors[-1]:
+#                     answer += 1
+#             else:
+#                 if colors[i] != colors[0] and colors[i] != colors[i-1]:
+#                     answer += 1
+#         return answer
