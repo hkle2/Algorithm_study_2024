9911e3ee2d6bbdd0dcb123513e8c5e742b5006a1
hkle2 <minnie110598@gmail.com>
2024-07-18 11:06:36 +0900

    Time: 39 ms (35.25%), Space: 16.6 MB (25.76%) - LeetHub

--- LeetCode/Easy/2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.py
+++ LeetCode/Easy/2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.py
@@ -1,6 +1,18 @@
 class Solution:
     def minTimeToType(self, word: str) -> int:
         answer = 0
+        loc = 0
+        for c in word:
+            target_loc = ord(c) - 97
+            move = abs(target_loc - loc)
+            move_reverse = 26 - abs(target_loc - loc)
+            answer += min(move, move_reverse) + 1
+            loc = target_loc
+        return answer
+
+class Solution:
+    def minTimeToType(self, word: str) -> int:
+        answer = 0
         cur = 0
         for i in word:
             if abs(cur - (ord(i) - ord("a"))) <= 13:
