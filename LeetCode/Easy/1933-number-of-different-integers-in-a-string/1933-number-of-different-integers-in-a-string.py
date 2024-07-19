a8c0f0eafd0913938b350de78cc5f763df95a006
hkle2 <minnie110598@gmail.com>
2024-07-19 09:39:38 +0900

    Time: 33 ms (77.8%), Space: 16.4 MB (94.09%) - LeetHub

--- LeetCode/Easy/1933-number-of-different-integers-in-a-string/1933-number-of-different-integers-in-a-string.py
+++ LeetCode/Easy/1933-number-of-different-integers-in-a-string/1933-number-of-different-integers-in-a-string.py
@@ -1,27 +1,26 @@
-# class Solution:
-#     def numDifferentIntegers(self, word: str) -> int:
-#         answer = []
-#         tmp = ""
-#         for w in word:
-#             if w.isnumeric():
-#                 tmp += w
-#             elif tmp:
-#                 answer.append(tmp)
-#                 tmp = ""
-#         if tmp:
-#             answer.append(tmp)
-#         for i in range(len(answer)):
-#             answer[i] = int(answer[i])
-#         return len(set(answer))
-
 class Solution:
     def numDifferentIntegers(self, word: str) -> int:
-        answer = 0
+        answer = []
         tmp = ""
         for w in word:
-            if not w.isnumeric():
-                word = word.replace(w, " ")
-        l = word.split()
-        for i in range(len(l)):
-            l[i] = int(l[i])
-        return len(set(l))
+            if w.isnumeric():
+                tmp += w
+            elif tmp:
+                answer.append(tmp)
+                tmp = ""
+        if tmp:
+            answer.append(tmp)
+        for i in range(len(answer)):
+            answer[i] = int(answer[i])
+        return len(set(answer))
+
+# class Solution:
+#     def numDifferentIntegers(self, word: str) -> int:
+#         tmp = ""
+#         for w in word:
+#             if not w.isnumeric():
+#                 word = word.replace(w, " ")
+#         l = word.split()
+#         for i in range(len(l)):
+#             l[i] = int(l[i])
+#         return len(set(l))
