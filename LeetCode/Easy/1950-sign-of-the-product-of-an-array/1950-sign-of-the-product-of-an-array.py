f0acd149cff6a640bdea45884f744b9957cd1fe7
hkle2 <minnie110598@gmail.com>
2024-07-19 09:20:54 +0900

    Time: 133 ms (5.39%), Space: 36.7 MB (7.32%) - LeetHub

--- LeetCode/Easy/1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.py
+++ LeetCode/Easy/1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.py
@@ -1,12 +1,24 @@
+import numpy as np
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
-        answer = 1
-        for i in nums:
-            if i > 0:
-                answer *= 1
-            elif i < 0:
-                answer *= -1
-            else:
-                answer *= 0
-        return answer
-                
+        p = math.prod(nums)
+        print(p)
+        if p > 0:
+            answer = 1
+        elif p < 0:
+            answer = -1
+        else:
+            answer = 0
+        return answer 
+
+# class Solution:
+#     def arraySign(self, nums: List[int]) -> int:
+#         answer = 1
+#         for i in nums:
+#             if i > 0:
+#                 answer *= 1
+#             elif i < 0:
+#                 answer *= -1
+#             else:
+#                 answer *= 0
+#         return answer 
