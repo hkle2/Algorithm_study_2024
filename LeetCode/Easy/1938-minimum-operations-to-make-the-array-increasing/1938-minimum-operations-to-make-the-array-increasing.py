5175c1f54043b8b027ff841b289d88552db65be0
hkle2 <minnie110598@gmail.com>
2024-07-22 09:21:07 +0900

    Time: 109 ms (25.06%), Space: 17.2 MB (86.18%) - LeetHub

--- LeetCode/Easy/1938-minimum-operations-to-make-the-array-increasing/1938-minimum-operations-to-make-the-array-increasing.py
+++ LeetCode/Easy/1938-minimum-operations-to-make-the-array-increasing/1938-minimum-operations-to-make-the-array-increasing.py
@@ -1,11 +1,20 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
         answer = 0
-        prev = 0
-        for num in nums:
-            if prev >= num:
-                answer += prev - num + 1
-                prev = prev + 1
-            else:
-                prev = num
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i-1]:
+                answer += nums[i-1] - nums[i] + 1
+                nums[i] = nums[i-1] + 1
         return answer
+
+# class Solution:
+#     def minOperations(self, nums: List[int]) -> int:
+#         answer = 0
+#         prev = 0
+#         for num in nums:
+#             if prev >= num:
+#                 answer += prev - num + 1
+#                 prev = prev + 1
+#             else:
+#                 prev = num
+#         return answer
