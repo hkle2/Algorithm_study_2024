7bc6e490eb37af75a922ef98d866ddbb5870f9c6
hkle2 <minnie110598@gmail.com>
2024-07-22 09:23:33 +0900

    Time: 26 ms (96.87%), Space: 16.4 MB (91.2%) - LeetHub

--- LeetCode/Easy/1954-replace-all-digits-with-characters/1954-replace-all-digits-with-characters.py
+++ LeetCode/Easy/1954-replace-all-digits-with-characters/1954-replace-all-digits-with-characters.py
@@ -1,11 +1,8 @@
 class Solution:
     def replaceDigits(self, s: str) -> str:
         answer = ""
-        if len(s) % 2 == 0:
-            for i in range(0, len(s), 2):
-                answer += s[i] + chr(ord(s[i]) + int(s[i+1]))
-        else:
-            for i in range(0, len(s)-1, 2):
-                answer += s[i] + chr(ord(s[i]) + int(s[i+1]))
+        for i in range(0, len(s)-1, 2):
+            answer += s[i] + chr(ord(s[i]) + int(s[i+1]))
+        if len(s) % 2 != 0:
             answer += s[-1]
         return answer
