1d78f3d63704234656b285950bc46276b6ea50de
hkle2 <minnie110598@gmail.com>
2024-07-23 10:04:14 +0900

    Time: 36 ms (54.08%), Space: 16.6 MB (10.35%) - LeetHub

--- LeetCode/Easy/3511-find-the-winning-player-in-coin-game/3511-find-the-winning-player-in-coin-game.py
+++ LeetCode/Easy/3511-find-the-winning-player-in-coin-game/3511-find-the-winning-player-in-coin-game.py
@@ -1,5 +1,16 @@
 class Solution:
     def losingPlayer(self, x: int, y: int) -> str:
+        is_alice = True
+        while True:
+            if x < 1 or y < 4:
+                break
+            x -= 1
+            y -= 4
+            is_alice = not is_alice
+        return "Bob" if is_alice else "Alice"
+
+class Solution:
+    def losingPlayer(self, x: int, y: int) -> str:
         i = 1
         while x > 0 and y > 0:
             x -= 1
