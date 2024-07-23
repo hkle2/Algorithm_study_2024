78a000aea3de9e43eec21744e6b7cfd5a7ce409f
hkle2 <minnie110598@gmail.com>
2024-07-23 09:38:20 +0900

    Time: 65 ms (88.25%), Space: 16.6 MB (70.5%) - LeetHub

--- LeetCode/Easy/1382-calculate-amount-paid-in-taxes/1382-calculate-amount-paid-in-taxes.py
+++ LeetCode/Easy/1382-calculate-amount-paid-in-taxes/1382-calculate-amount-paid-in-taxes.py
@@ -10,18 +10,3 @@ class Solution:
                 answer += (income - prev) * p / 100
                 break
         return answer
-
-# class Solution:
-#     def calculateTax(self, brackets: List[List[int]], income: int) -> float:
-#         answer = 0
-#         for i in range(len(brackets)):
-#             inc = income
-#             if i == 0:
-#                 answer += brackets[i][0] * brackets[i][1] / 100
-#             else:
-#                 if brackets[i][0] <= income:
-#                     answer += (brackets[i][0] - brackets[i-1][0]) * brackets[i][1] / 100
-#                 else:
-#                     answer += (brackets[i][0] - brackets[i-1][0]) * brackets[i][1] / 100
-#                     break
-#         return answer
