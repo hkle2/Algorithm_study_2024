7df9116c6e16d1a31c3540f0d24107537d8e0cd3
hkle2 <minnie110598@gmail.com>
2024-07-24 10:03:21 +0900

    Time: 103 ms (72.14%), Space: 16.8 MB (44.53%) - LeetHub

--- LeetCode/Easy/2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
+++ LeetCode/Easy/2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,13 +1,27 @@
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
+        # for문 돌면서 양수 개수, 음수 개수 세주기
         pos = 0
         neg = 0
-        for i in range(len(nums)):
-            if nums[i] > 0:
+        for num in nums:
+            # 양수면 pos에 1 더해주기
+            # 음수면 neg에 1 더해주기
+            if num > 0:
                 pos += 1
-            elif nums[i] < 0:
+            if num < 0:
                 neg += 1
-        if pos >= neg:
-            return pos
-        else:
-            return neg
+        return max(pos, neg)
+
+# class Solution:
+#     def maximumCount(self, nums: List[int]) -> int:
+#         pos = 0
+#         neg = 0
+#         for i in range(len(nums)):
+#             if nums[i] > 0:
+#                 pos += 1
+#             elif nums[i] < 0:
+#                 neg += 1
+#         if pos >= neg:
+#             return pos
+#         else:
+#             return neg
