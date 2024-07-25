a4ea11858d034fd8faa7376333273954e867b6b8
hkle2 <minnie110598@gmail.com>
2024-07-25 09:07:42 +0900

    Time: 660 ms (92.68%), Space: 23.7 MB (95.4%) - LeetHub

---
+++ LeetCode/Easy/2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        answer = []
+        if len(original) == m * n:
+            for i in range(0, len(original), n):
+                answer.append(original[i:i+n])
+        else:
+            return []
+        return answer
