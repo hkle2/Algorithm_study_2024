54f7011084f97facab9a3ea6c1b7af2119db0d9c
hkle2 <minnie110598@gmail.com>
2024-07-25 10:07:29 +0900

    Time: 648 ms (99.06%), Space: 23.9 MB (70.48%) - LeetHub

--- LeetCode/Easy/2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.py
+++ LeetCode/Easy/2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.py
@@ -1,9 +1,19 @@
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
         answer = []
-        if len(original) == m * n:
-            for i in range(0, len(original), n):
-                answer.append(original[i:i+n])
-        else:
-            return []
+        cnt = len(original)
+        if cnt != m * n:
+            return answer
+        for i in range(0, len(original), n):
+            answer.append(original[i:i+n])
         return answer
+
+# class Solution:
+#     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+#         answer = []
+#         if len(original) == m * n:
+#             for i in range(0, len(original), n):
+#                 answer.append(original[i:i+n])
+#         else:
+#             return []
+#         return answer
