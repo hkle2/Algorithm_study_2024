a71b0709a421856906b8affc80da8f14d254388d
hkle2 <minnie110598@gmail.com>
2024-07-26 09:50:27 +0900

    Time: 43 ms (47.54%), Space: 16.9 MB (6.65%) - LeetHub

--- LeetCode/Easy/3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.py
+++ LeetCode/Easy/3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.py
@@ -1,26 +1,11 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
         answer = 0
-        num = set()
+        collection = set()
         for i, n in enumerate(reversed(nums)):
             print(i, n)
             if n > k:
                 continue
-            num.add(n)
-            if len(num) == k:
+            collection.add(n)
+            if len(collection) == k:
                 return i + 1
-
-# class Solution:
-#     def minOperations(self, nums: List[int], k: int) -> int:
-#         answer = 0
-#         num = []
-#         for i in range(1, k+1):
-#             num.append(i)
-#         nums.reverse()
-#         for i in range(len(nums)):
-#             if nums[i] in num:
-#                 num.remove(nums[i])
-#                 if num != []:
-#                     answer += 1
-#         answer += 1
-#         return answer
