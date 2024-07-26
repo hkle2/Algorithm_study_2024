51ac2c5dc8d022de5ad3cdaea83e537637474f0d
hkle2 <minnie110598@gmail.com>
2024-07-26 10:05:51 +0900

    Time: 37 ms (81.03%), Space: 16.6 MB (6.65%) - LeetHub

--- LeetCode/Easy/3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.py
+++ LeetCode/Easy/3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.py
@@ -7,19 +7,17 @@ class Solution:
             poped_num = nums.pop()
             collections.add(poped_num)
             answer += 1
-            # target이 collections안에 모두 포함되는 경우, break
-            # set intersection을 사용
             if target.intersection(collections) == target:
                 break
         return answer
 
-# class Solution:
-#     def minOperations(self, nums: List[int], k: int) -> int:
-#         answer = 0
-#         collection = set()
-#         for i, n in enumerate(reversed(nums)):
-#             if n > k:
-#                 continue
-#             collection.add(n)
-#             if len(collection) == k:
-#                 return i + 1
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        answer = 0
+        collection = set()
+        for i, n in enumerate(reversed(nums)):
+            if n > k:
+                continue
+            collection.add(n)
+            if len(collection) == k:
+                return i + 1
