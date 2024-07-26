62e2dc8f8f924299d25c68442ea47260a24e1fe3
hkle2 <minnie110598@gmail.com>
2024-07-26 10:03:52 +0900

    Time: 36 ms (48.92%), Space: 16.4 MB (66.17%) - LeetHub

--- LeetCode/Easy/2630-alternating-digit-sum/2630-alternating-digit-sum.py
+++ LeetCode/Easy/2630-alternating-digit-sum/2630-alternating-digit-sum.py
@@ -2,19 +2,18 @@ class Solution:
     def alternateDigitSum(self, n: int) -> int:
         answer = 0
         for i, c in enumerate(str(n)):
-            # i가 홀수일 때와 짝수일 떄 신경써서 answer에 더해주기
             if i % 2 == 0:
                 answer += int(c)
             else:
                 answer -= int(c)
         return answer
 
-# class Solution:
-#     def alternateDigitSum(self, n: int) -> int:
-#         answer = 0
-#         l = list(str(n))
-#         for i in range(0, len(l), 2):
-#             answer += int(l[i])
-#         for j in range(1, len(l), 2):
-#             answer -= int(l[j])
-#         return answer
+class Solution:
+    def alternateDigitSum(self, n: int) -> int:
+        answer = 0
+        l = list(str(n))
+        for i in range(0, len(l), 2):
+            answer += int(l[i])
+        for j in range(1, len(l), 2):
+            answer -= int(l[j])
+        return answer
