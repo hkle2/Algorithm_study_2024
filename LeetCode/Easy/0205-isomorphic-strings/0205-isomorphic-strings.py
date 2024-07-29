676cd3f996f9093b0a2d1bc1c3d8ec8f78e1cbab
hkle2 <minnie110598@gmail.com>
2024-07-29 09:22:46 +0900

    Time: 49 ms (20.46%), Space: 16.7 MB (34%) - LeetHub

--- LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.py
+++ LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -11,12 +11,3 @@ class Solution:
             else:
                 d[s[i]] = t[i]
         return answer
-
-# class Solution:
-#     def isIsomorphic(self, s: str, t: str) -> bool:
-#         answer = True
-#         for i in range(1, len(s)):
-#             if (s[i-1] != t[i-1]) or (s[i] != t[i]):
-#                 if (s[i-1] == s[i] and t[i-1] != t[i]) or (s[i-1] != s[i] and t[i-1] == t[i]):
-#                     answer = False
-#         return answer
