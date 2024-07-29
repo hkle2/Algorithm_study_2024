c9abf90987a8ff790c6daa9e052f7121a54c25d6
hkle2 <minnie110598@gmail.com>
2024-07-29 10:04:07 +0900

    Time: 38 ms (80.49%), Space: 16.7 MB (34%) - LeetHub

--- LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.py
+++ LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,5 +1,19 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        d = {}
+        for i in range(len(s)):
+            print(s[i], t[i], d)
+            if s[i] in d:
+                if d[s[i]] != t[i]:
+                    return False
+            else:
+                if t[i] in d.values():
+                    return False
+                d[s[i]] = t[i]
+        return True
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
         answer = True
         d = {}
         for i in range(len(s)):
