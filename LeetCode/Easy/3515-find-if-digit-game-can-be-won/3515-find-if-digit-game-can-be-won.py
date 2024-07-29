c7da8b465088f1365c129920f63a16ff4d7590f7
hkle2 <minnie110598@gmail.com>
2024-07-29 10:05:03 +0900

    Time: 52 ms (100%), Space: 16.7 MB (100%) - LeetHub

--- LeetCode/Easy/3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.py
+++ LeetCode/Easy/3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.py
@@ -1,12 +1,33 @@
 class Solution:
     def canAliceWin(self, nums: List[int]) -> bool:
-        single = 0
-        double = 0
+        single_digit = []
+        double_digit = []
+        # 1. 한자리 수만 모아서 single_digit에 추가하기
+        # 2. 두자리 수만 모아서 double_digit에 추가하기
+        # 전체 합계에서 single_digits 합계를 빼준 값과 비교
+        # 전체 합계에서 double_digits 합계를 빼준 값과 비교
         for num in nums:
             if num < 10:
-                single += num
-            else:
-                double += num
-        if single != double:
+                single_digit.append(num)
+            elif num < 100:
+                double_digit.append(num)
+        total_sum = sum(nums)
+        single_sum = sum(single_digit)
+        double_sum = sum(double_digit)
+        print(total_sum, single_sum, double_sum)
+        if single_sum != double_sum:
             return True
         return False
+        
+# class Solution:
+#     def canAliceWin(self, nums: List[int]) -> bool:
+#         single = 0
+#         double = 0
+#         for num in nums:
+#             if num < 10:
+#                 single += num
+#             else:
+#                 double += num
+#         if single != double:
+#             return True
+#         return False
