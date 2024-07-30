b24d0acaaf61093593da2a81841c5d807370e4d3
hkle2 <minnie110598@gmail.com>
2024-07-30 10:03:27 +0900

    Time: 117 ms (31.51%), Space: 16.7 MB (97.43%) - LeetHub

--- LeetCode/Easy/2624-difference-between-element-sum-and-digit-sum-of-an-array/2624-difference-between-element-sum-and-digit-sum-of-an-array.py
+++ LeetCode/Easy/2624-difference-between-element-sum-and-digit-sum-of-an-array/2624-difference-between-element-sum-and-digit-sum-of-an-array.py
@@ -2,9 +2,10 @@ class Solution:
     def differenceOfSum(self, nums: List[int]) -> int:
         # for문으로 반복
         # num을 문자열로 만들고, 다시 for문을 돌면 각 자릿수를 가져올 수 있음
-        element_sum = sum(nums)
+        element_sum = 0
         digit_sum = 0
         for num in nums:
+            element_sum += num
             for c in str(num):
                 digit_sum += int(c)
         # 둘의 차이의 절대값을 리턴
