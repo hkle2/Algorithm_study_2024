9c04856b7d8b7439f68ee176f0e1d294958be1a6
hkle2 <minnie110598@gmail.com>
2024-07-30 10:06:57 +0900

    Time: 2514 ms (53.47%), Space: 17.1 MB (5.2%) - LeetHub

--- LeetCode/Easy/2694-find-the-maximum-divisibility-score/2694-find-the-maximum-divisibility-score.py
+++ LeetCode/Easy/2694-find-the-maximum-divisibility-score/2694-find-the-maximum-divisibility-score.py
@@ -1,21 +1,35 @@
 class Solution:
     def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
-        answer = 0
         # 각 divisor 별 divisibility score 저장
         d = {}
         for divisor in divisors:
             score = 0
             for num in nums:
-                # score 계산
                 if num % divisor == 0:
                     score += 1
             d[divisor] = score
-        answer = float("inf")
-        for key, value in d.items():
-            if value == max(d.values()):
-                if key < answer:
-                    answer = key
+        max_score = max(d.values())
+        answer = sorted(d.items(), key=lambda x: (-x[1], x[0]))[0][0]
         return answer
+
+# class Solution:
+#     def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
+#         answer = 0
+#         # 각 divisor 별 divisibility score 저장
+#         d = {}
+#         for divisor in divisors:
+#             score = 0
+#             for num in nums:
+#                 # score 계산
+#                 if num % divisor == 0:
+#                     score += 1
+#             d[divisor] = score
+#         answer = float("inf")
+#         for key, value in d.items():
+#             if value == max(d.values()):
+#                 if key < answer:
+#                     answer = key
+#         return answer
 
 # class Solution:
 #     def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
