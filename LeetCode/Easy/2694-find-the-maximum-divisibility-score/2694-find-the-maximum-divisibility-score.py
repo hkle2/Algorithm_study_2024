63ddb97af2f8100a3397025d834bf3e3aa415cd6
hkle2 <minnie110598@gmail.com>
2024-07-30 10:19:21 +0900

    Time: 3757 ms (24.57%), Space: 16.9 MB (15.32%) - LeetHub

--- LeetCode/Easy/2694-find-the-maximum-divisibility-score/2694-find-the-maximum-divisibility-score.py
+++ LeetCode/Easy/2694-find-the-maximum-divisibility-score/2694-find-the-maximum-divisibility-score.py
@@ -1,6 +1,5 @@
 class Solution:
     def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
-        # 각 divisor 별 divisibility score 저장
         d = {}
         for divisor in divisors:
             score = 0
@@ -16,45 +15,44 @@ class Solution:
         answer = sorted(candidates)[0]
         return answer
 
-# class Solution:
-#     def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
-#         # 각 divisor 별 divisibility score 저장
-#         d = {}
-#         for divisor in divisors:
-#             score = 0
-#             for num in nums:
-#                 if num % divisor == 0:
-#                     score += 1
-#             d[divisor] = score
-#         answer = sorted(d.items(), key=lambda x: (-x[1], x[0]))[0][0]
-#         return answer
+class Solution:
+    def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
+        d = {}
+        for divisor in divisors:
+            score = 0
+            for num in nums:
+                if num % divisor == 0:
+                    score += 1
+            d[divisor] = score
+        answer = sorted(d.items(), key=lambda x: (-x[1], x[0]))[0][0]
+        return answer
 
-# class Solution:
-#     def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
-#         answer = 0
-#         d = {}
-#         for divisor in divisors:
-#             score = 0
-#             for num in nums:
-#                 if num % divisor == 0:
-#                     score += 1
-#             d[divisor] = score
-#         answer = float("inf")
-#         for key, value in d.items():
-#             if value == max(d.values()):
-#                 if key < answer:
-#                     answer = key
-#         return answer
+class Solution:
+    def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
+        answer = 0
+        d = {}
+        for divisor in divisors:
+            score = 0
+            for num in nums:
+                if num % divisor == 0:
+                    score += 1
+            d[divisor] = score
+        answer = float("inf")
+        for key, value in d.items():
+            if value == max(d.values()):
+                if key < answer:
+                    answer = key
+        return answer
 
-# class Solution:
-#     def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
-#         divisors.sort()
-#         div = []
-#         for d in range(len(divisors)):
-#             cnt = 0
-#             for j in range(len(nums)):
-#                 if nums[j] % divisors[d] == 0:
-#                     cnt += 1
-#             div.append(cnt)
-#         i = div.index(max(div))
-#         return divisors[i]
+class Solution:
+    def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
+        divisors.sort()
+        div = []
+        for d in range(len(divisors)):
+            cnt = 0
+            for j in range(len(nums)):
+                if nums[j] % divisors[d] == 0:
+                    cnt += 1
+            div.append(cnt)
+        i = div.index(max(div))
+        return divisors[i]
