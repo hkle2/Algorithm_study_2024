e2d7bdfaa8669aeb48e73e0a21a127e12ff5f076
hkle2 <minnie110598@gmail.com>
2024-07-30 09:31:28 +0900

    Time: 3290 ms (28.62%), Space: 16.7 MB (88.73%) - LeetHub

--- LeetCode/Easy/2694-find-the-maximum-divisibility-score/2694-find-the-maximum-divisibility-score.py
+++ LeetCode/Easy/2694-find-the-maximum-divisibility-score/2694-find-the-maximum-divisibility-score.py
@@ -1,12 +1,31 @@
 class Solution:
     def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
-        divisors.sort()
-        div = []
-        for i in range(len(divisors)):
-            cnt = 0
-            for j in range(len(nums)):
-                if nums[j] % divisors[i] == 0:
-                    cnt += 1
-            div.append(cnt)
-        i = div.index(max(div))
-        return divisors[i]
+        answer = 0
+        # 각 divisor 별 divisibility score 저장
+        d = {}
+        for divisor in divisors:
+            score = 0
+            for num in nums:
+                # score 계산
+                if num % divisor == 0:
+                    score += 1
+            d[divisor] = score
+        answer = float("inf")
+        for key, value in d.items():
+            if value == max(d.values()):
+                if key < answer:
+                    answer = key
+        return answer
+
+# class Solution:
+#     def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
+#         divisors.sort()
+#         div = []
+#         for d in range(len(divisors)):
+#             cnt = 0
+#             for j in range(len(nums)):
+#                 if nums[j] % divisors[d] == 0:
+#                     cnt += 1
+#             div.append(cnt)
+#         i = div.index(max(div))
+#         return divisors[i]
