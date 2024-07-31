e9963265b8369dddc08f82103da9029da4ba789c
hkle2 <minnie110598@gmail.com>
2024-07-31 09:32:44 +0900

    Time: 83 ms (34.44%), Space: 16.7 MB (65.11%) - LeetHub

--- LeetCode/Easy/2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.py
+++ LeetCode/Easy/2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.py
@@ -1,8 +1,18 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
-        answer = 0
+        # for문, sorted, pop
         for i in range(k):
-            gifts.sort(reverse=True)
-            gifts[0] = int(gifts[0]**0.5)
-        answer = sum(gifts)
-        return answer
+            gifts = sorted(gifts)
+            max_gifts = gifts.pop()
+            # 가져온 가장 큰 값에 제곱근을 취해서 다시 gifts에 append
+            gifts.append(int(max_gifts**0.5)) 
+        return sum(gifts)
+
+# class Solution:
+#     def pickGifts(self, gifts: List[int], k: int) -> int:
+#         answer = 0
+#         for i in range(k):
+#             gifts.sort(reverse=True)
+#             gifts[0] = int(gifts[0]**0.5)
+#         answer = sum(gifts)
+#         return answer
