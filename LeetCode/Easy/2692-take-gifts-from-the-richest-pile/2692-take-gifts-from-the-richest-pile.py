9dd1a032386d7ac2ace99e3fc03f9b4cd9f8f665
hkle2 <minnie110598@gmail.com>
2024-07-31 09:27:09 +0900

    Time: 67 ms (45.26%), Space: 16.6 MB (94.44%) - LeetHub

--- LeetCode/Easy/2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.py
+++ LeetCode/Easy/2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.py
@@ -1,6 +1,8 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
+        answer = 0
         for i in range(k):
             gifts.sort(reverse=True)
             gifts[0] = int(gifts[0]**0.5)
-        return sum(gifts)
+        answer = sum(gifts)
+        return answer
