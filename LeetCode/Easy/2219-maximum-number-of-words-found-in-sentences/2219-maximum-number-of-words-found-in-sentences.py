4d02aea2ee2b46bbeb19c44639f1246254a0e713
hkle2 <minnie110598@gmail.com>
2024-08-01 10:02:20 +0900

    Time: 39 ms (90.09%), Space: 16.6 MB (25.12%) - LeetHub

--- LeetCode/Easy/2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.py
+++ LeetCode/Easy/2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.py
@@ -1,8 +1,11 @@
 class Solution:
     def mostWordsFound(self, sentences: List[str]) -> int:
+        # 문자열 split, len, max
         answer = 0
-        for s in sentences:
-            answer = max(answer, len(s.split(" ")))
+        for sentence in sentences:
+            words = sentence.split(" ")
+            # words의 길이와 answer를 비교해서 더 큰 값을 answer로 지정
+            answer = max(len(words), answer)
         return answer
 
 # class Solution:
