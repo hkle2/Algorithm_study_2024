6b21cdff01442249580ab4674784f0c7f2c06dc2
hkle2 <minnie110598@gmail.com>
2024-08-01 10:05:58 +0900

    Time: 47 ms (47.41%), Space: 16.5 MB (64.12%) - LeetHub

--- LeetCode/Easy/2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.py
+++ LeetCode/Easy/2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.py
@@ -1,12 +1,16 @@
+# class Solution:
+#     def mostWordsFound(self, sentences: List[str]) -> int:
+#         # 문자열 split, len, max
+#         answer = 0
+#         for sentence in sentences:
+#             words = sentence.split(" ")
+#             # words의 길이와 answer를 비교해서 더 큰 값을 answer로 지정
+#             answer = max(len(words), answer)
+#         return answer
+
 class Solution:
     def mostWordsFound(self, sentences: List[str]) -> int:
-        # 문자열 split, len, max
-        answer = 0
-        for sentence in sentences:
-            words = sentence.split(" ")
-            # words의 길이와 answer를 비교해서 더 큰 값을 answer로 지정
-            answer = max(len(words), answer)
-        return answer
+        return max([len(sentence.split(" ")) for sentence in sentences])
 
 # class Solution:
 #     def mostWordsFound(self, sentences: List[str]) -> int:
