5168da568b08e8a086eb995e998e9ab5aabeea5e
hkle2 <minnie110598@gmail.com>
2024-08-01 10:13:26 +0900

    Time: 31 ms (83.03%), Space: 16.6 MB (20.8%) - LeetHub

--- LeetCode/Easy/2154-minimum-moves-to-convert-string/2154-minimum-moves-to-convert-string.py
+++ LeetCode/Easy/2154-minimum-moves-to-convert-string/2154-minimum-moves-to-convert-string.py
@@ -1,14 +1,34 @@
 class Solution:
     def minimumMoves(self, s: str) -> int:
         answer = 0
-        i = 0
-        while i < len(s):
-            if s[i] == "X":
+        c_list = [c for c in s]
+        for i, c in enumerate(c_list):
+            if c == "X":
+                # 현재 위치의 문자 변경
+                c_list[i] = "O"
+                # 다음 위치의 문자 변경
+                # 다다음 위치의 문자 변경
+                # 이 때, 다음 혹은 다다음 위치가 범위를 벗어날 경우 예외처리
                 answer += 1
-                i += 3
-            else:
-                i += 1
+                if i + 1 >= len(c_list):
+                    continue
+                c_list[i+1] = "O"
+                if i + 2 >= len(c_list):
+                    continue
+                c_list[i+2] = "O"
         return answer
+
+# class Solution:
+#     def minimumMoves(self, s: str) -> int:
+#         answer = 0
+#         i = 0
+#         while i < len(s):
+#             if s[i] == "X":
+#                 answer += 1
+#                 i += 3
+#             else:
+#                 i += 1
+#         return answer
 
 # class Solution:
 #     def minimumMoves(self, s: str) -> int:
