81fc1407a270d3be5540babd46ea186c4c5ff7f1
hkle2 <minnie110598@gmail.com>
2024-08-01 09:17:17 +0900

    Time: 65 ms (5.99%), Space: 16.5 MB (20.8%) - LeetHub

---
+++ LeetCode/Easy/2154-minimum-moves-to-convert-string/2154-minimum-moves-to-convert-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumMoves(self, s: str) -> int:
+        s = list(s)
+        answer = 0
+        for i in range(len(s)-2):
+            if s[i] == "O" and (i+2 <= len(s)-2):
+                continue
+            cnt = 0
+            for j in range(3):
+                if s[i+j] == "X":
+                    s[i+j] = "O"
+                    cnt += 1
+            if cnt != 0:
+                answer += 1
+            if "X" not in s:
+                return answer
+        return answer
+        
