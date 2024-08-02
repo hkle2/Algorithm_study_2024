c135ba17d17bcf5a8f849fe0c18fa1c193bf02c6
hkle2 <minnie110598@gmail.com>
2024-08-02 10:10:12 +0900

    Time: 32 ms (75.39%), Space: 16.5 MB (16.02%) - LeetHub

--- LeetCode/Easy/2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.py
+++ LeetCode/Easy/2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.py
@@ -1,28 +1,28 @@
-# class Solution:
-#     def minMaxDifference(self, num: int) -> int:
-#         # 가장 큰 수는 무조건 9로 치환한 수
-#         # 가장 작은 수는 무조건 0으로 치환한 수
-#         nums_str = str(num)
-#         candidates = []
-#         for i in range(10):
-#             candidates.append(nums_str.replace(str(i), "9"))
-#             candidates.append(nums_str.replace(str(i), "0"))
-#         return max(candidates) - min(candidates)
-
 class Solution:
     def minMaxDifference(self, num: int) -> int:
-        num_str = str(num)
-        max_num = num_str
-        min_num = num_str
-        for char in num_str:
-            if char != "9":
-                max_num = num_str.replace(char, "9")
-                break
-        for char in num_str:
-            if char != "0":
-                min_num = num_str.replace(char, "0")
-                break
-        return int(max_num) - int(min_num)
+        # 가장 큰 수는 무조건 9로 치환한 수
+        # 가장 작은 수는 무조건 0으로 치환한 수
+        nums_str = str(num)
+        candidates = []
+        for i in range(10):
+            candidates.append(int(nums_str.replace(str(i), "9")))
+            candidates.append(int(nums_str.replace(str(i), "0")))
+        return max(candidates) - min(candidates)
+
+# class Solution:
+#     def minMaxDifference(self, num: int) -> int:
+#         num_str = str(num)
+#         max_num = num_str
+#         min_num = num_str
+#         for char in num_str:
+#             if char != "9":
+#                 max_num = num_str.replace(char, "9")
+#                 break
+#         for char in num_str:
+#             if char != "0":
+#                 min_num = num_str.replace(char, "0")
+#                 break
+#         return int(max_num) - int(min_num)
 
 # class Solution:
 #     def minMaxDifference(self, num: int) -> int:
