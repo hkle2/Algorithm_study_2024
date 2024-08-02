5ddd1722fef0b07df25c47d4757ab815f772d0bc
hkle2 <minnie110598@gmail.com>
2024-08-02 09:34:11 +0900

    Time: 42 ms (88.01%), Space: 16.5 MB (71.99%) - LeetHub

--- LeetCode/Easy/2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.py
+++ LeetCode/Easy/2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.py
@@ -1,8 +1,13 @@
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
-        answer = []
         nums.sort()
-        for i in range(len(nums)):
-            if nums[i] == target:
-                answer.append(i)
-        return answer
+        return [i for i in range(len(nums)) if nums[i] == target]
+
+# class Solution:
+#     def targetIndices(self, nums: List[int], target: int) -> List[int]:
+#         answer = []
+#         nums.sort()
+#         for i in range(len(nums)):
+#             if nums[i] == target:
+#                 answer.append(i)
+#         return answer
