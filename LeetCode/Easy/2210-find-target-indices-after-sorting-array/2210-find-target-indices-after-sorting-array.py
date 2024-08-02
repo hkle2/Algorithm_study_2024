2e806675148d044407561298b704d1e975a90328
hkle2 <minnie110598@gmail.com>
2024-08-02 10:02:32 +0900

    Time: 44 ms (78.91%), Space: 16.6 MB (28.39%) - LeetHub

--- LeetCode/Easy/2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.py
+++ LeetCode/Easy/2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.py
@@ -1,7 +1,17 @@
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
-        nums.sort()
-        return [i for i in range(len(nums)) if nums[i] == target]
+        # for문
+        answer = []
+        nums = sorted(nums)
+        for i, num in enumerate(nums):
+            # num이 target과 같을 경우, answer에 index를 추가해볼 것
+            if num == target:
+                answer.append(i)
+        return answer
+
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        return [i for i, x in enumerate(sorted(nums)) if x == target]
 
 # class Solution:
 #     def targetIndices(self, nums: List[int], target: int) -> List[int]:
