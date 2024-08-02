2892d5cbf2cdd67caa58d557556d83dd07ff0a0a
hkle2 <minnie110598@gmail.com>
2024-08-02 10:04:13 +0900

    Time: 48 ms (55.94%), Space: 16.4 MB (71.99%) - LeetHub

--- LeetCode/Easy/2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.py
+++ LeetCode/Easy/2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.py
@@ -1,23 +1,21 @@
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
-        # for문
         answer = []
         nums = sorted(nums)
         for i, num in enumerate(nums):
-            # num이 target과 같을 경우, answer에 index를 추가해볼 것
             if num == target:
                 answer.append(i)
         return answer
 
-# class Solution:
-#     def targetIndices(self, nums: List[int], target: int) -> List[int]:
-#         return [i for i, x in enumerate(sorted(nums)) if x == target]
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        return [i for i, x in enumerate(sorted(nums)) if x == target]
 
-# class Solution:
-#     def targetIndices(self, nums: List[int], target: int) -> List[int]:
-#         answer = []
-#         nums.sort()
-#         for i in range(len(nums)):
-#             if nums[i] == target:
-#                 answer.append(i)
-#         return answer
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        answer = []
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] == target:
+                answer.append(i)
+        return answer
