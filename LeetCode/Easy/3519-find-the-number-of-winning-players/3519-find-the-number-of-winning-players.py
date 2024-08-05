83284fdedb1a3d11e56464c6c9c0fa4a128aff91
hkle2 <minnie110598@gmail.com>
2024-08-05 10:04:01 +0900

    Time: 167 ms (50%), Space: 16.7 MB (50%) - LeetHub

--- LeetCode/Easy/3519-find-the-number-of-winning-players/3519-find-the-number-of-winning-players.py
+++ LeetCode/Easy/3519-find-the-number-of-winning-players/3519-find-the-number-of-winning-players.py
@@ -2,11 +2,22 @@ class Solution:
     def winningPlayerCount(self, n: int, pick: List[List[int]]) -> int:
         answer = 0
         d = defaultdict(Counter)
-        for p, c in pick:
-            d[p][c] += 1
-        for key, value in d.items():
-            for i in value:
-                if value[i] >= key + 1:
-                    answer += 1
-                    break
+        for player, ball_color in pick:
+            d[player][ball_color] += 1
+        for player, ball_counter in d.items():
+            if player + 1 <= max(ball_counter.values()):
+                answer += 1
         return answer
+
+# class Solution:
+#     def winningPlayerCount(self, n: int, pick: List[List[int]]) -> int:
+#         answer = 0
+#         d = defaultdict(Counter)
+#         for p, c in pick:
+#             d[p][c] += 1
+#         for key, value in d.items():
+#             for i in value:
+#                 if value[i] >= key + 1:
+#                     answer += 1
+#                     break
+#         return answer
