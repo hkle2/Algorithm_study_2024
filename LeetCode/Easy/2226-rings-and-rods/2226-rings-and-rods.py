9bb50f8f1b1c346e943445c4a31d837bfb243223
hkle2 <minnie110598@gmail.com>
2024-08-06 09:14:39 +0900

    Time: 36 ms (44.85%), Space: 16.3 MB (92.49%) - LeetHub

---
+++ LeetCode/Easy/2226-rings-and-rods/2226-rings-and-rods.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countPoints(self, rings: str) -> int:
+        answer = 0
+        d = defaultdict(set)
+        for i in range(0, len(rings), 2):
+            if rings[i+1] not in d:
+                d[rings[i+1]] = [rings[i]]
+            else:
+                d[rings[i+1]] += [rings[i]]
+        for key, value in d.items():
+            if len(set(value)) == 3:
+                answer += 1
+        return answer
