d8151c2b264d253a351e7bdd79b7da8658cc2c8d
hkle2 <minnie110598@gmail.com>
2024-08-06 09:46:01 +0900

    Time: 41 ms (15.88%), Space: 16.3 MB (92.49%) - LeetHub

--- LeetCode/Easy/2226-rings-and-rods/2226-rings-and-rods.py
+++ LeetCode/Easy/2226-rings-and-rods/2226-rings-and-rods.py
@@ -1,28 +1,31 @@
-# class Solution:
-#     def countPoints(self, rings: str) -> int:
-#         # key: rod
-#         # value: ring color
-#         d = defaultdict(set)
-#         for i in range(0, len(rings), 2):
-#             ring = rings[i]
-#             rod = rings[i+1]
-#             # rod 키에다가 ring을 add
-#             d[rod].add(ring)
-#         print(d)
-#         # dictionary를 다 생성하고 나서
-#         # RGB 세가지 색상이 모두 포함된 rod의 개수 세어보기
-#         return 0
-
 class Solution:
     def countPoints(self, rings: str) -> int:
         answer = 0
-        d = defaultdict()
+        # key: rod
+        # value: ring color
+        d = defaultdict(set)
         for i in range(0, len(rings), 2):
-            if rings[i+1] not in d:
-                d[rings[i+1]] = [rings[i]]
-            else:
-                d[rings[i+1]] += [rings[i]]
+            ring = rings[i]
+            rod = rings[i+1]
+            # rod 키에다가 ring을 add
+            d[rod].add(ring)
+        # dictionary를 다 생성하고 나서
+        # RGB 세가지 색상이 모두 포함된 rod의 개수 세어보기
         for key, value in d.items():
-            if len(set(value)) == 3:
+            if len(value) == 3:
                 answer += 1
         return answer
+
+# class Solution:
+#     def countPoints(self, rings: str) -> int:
+#         answer = 0
+#         d = defaultdict()
+#         for i in range(0, len(rings), 2):
+#             if rings[i+1] not in d:
+#                 d[rings[i+1]] = [rings[i]]
+#             else:
+#                 d[rings[i+1]] += [rings[i]]
+#         for key, value in d.items():
+#             if len(set(value)) == 3:
+#                 answer += 1
+#         return answer
