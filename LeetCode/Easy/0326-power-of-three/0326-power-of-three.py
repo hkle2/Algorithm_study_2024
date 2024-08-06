77f457186bf3d47b26737553a7d0e4fd585e62f2
hkle2 <minnie110598@gmail.com>
2024-08-06 10:22:01 +0900

    Time: 62 ms (69.43%), Space: 16.4 MB (88.18%) - LeetHub

--- LeetCode/Easy/0326-power-of-three/0326-power-of-three.py
+++ LeetCode/Easy/0326-power-of-three/0326-power-of-three.py
@@ -1,5 +1,18 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while True:
+            if n == 1:
+                break
+            if n % 3 == 0:
+                n /= 3
+            else:
+                return False
+        return True
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
         for i in range(31):
             squared_num = math.pow(3, i)
             if n == squared_num:
@@ -8,26 +21,13 @@ class Solution:
                 return False
         return False
 
-# class Solution:
-#     def isPowerOfThree(self, n: int) -> bool:
-#         if n <= 0:
-#             return False
-#         while True:
-#             if n == 1:
-#                 break
-#             if n % 3 == 0:
-#                 n /= 3
-#             else:
-#                 return False
-#         return True
-
-# class Solution:
-#     def isPowerOfThree(self, n: int) -> bool:
-#         if n <= 0:
-#             return False
-#         while n > 0:
-#             if n == 1:
-#                 return True
-#             elif n % 3 != 0:
-#                 return False
-#             n /= 3
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n > 0:
+            if n == 1:
+                return True
+            elif n % 3 != 0:
+                return False
+            n /= 3
