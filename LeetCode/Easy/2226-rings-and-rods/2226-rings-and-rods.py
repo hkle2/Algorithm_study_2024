a008b1941df4d633c47bb6fdd5a1ec1a35815d44
hkle2 <minnie110598@gmail.com>
2024-08-06 09:45:05 +0900

    Time: 28 ms (92.17%), Space: 16.4 MB (59.87%) - LeetHub

--- LeetCode/Easy/2226-rings-and-rods/2226-rings-and-rods.py
+++ LeetCode/Easy/2226-rings-and-rods/2226-rings-and-rods.py
@@ -1,3 +1,18 @@
+# class Solution:
+#     def countPoints(self, rings: str) -> int:
+#         # key: rod
+#         # value: ring color
+#         d = defaultdict(set)
+#         for i in range(0, len(rings), 2):
+#             ring = rings[i]
+#             rod = rings[i+1]
+#             # rod 키에다가 ring을 add
+#             d[rod].add(ring)
+#         print(d)
+#         # dictionary를 다 생성하고 나서
+#         # RGB 세가지 색상이 모두 포함된 rod의 개수 세어보기
+#         return 0
+
 class Solution:
     def countPoints(self, rings: str) -> int:
         answer = 0
