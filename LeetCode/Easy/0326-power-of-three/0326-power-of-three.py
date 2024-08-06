d6238a06ef5e980c108795f4418a469c3f4f9013
hkle2 <minnie110598@gmail.com>
2024-08-06 10:19:26 +0900

    Time: 76 ms (28.91%), Space: 16.5 MB (88.18%) - LeetHub

--- LeetCode/Easy/0326-power-of-three/0326-power-of-three.py
+++ LeetCode/Easy/0326-power-of-three/0326-power-of-three.py
@@ -1,40 +1,25 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        for i in range(31):
+            squared_num = math.pow(3, i)
+            if n == squared_num:
+                return True
+            elif n < squared_num:
+                return False
+        return False
+
 # class Solution:
 #     def isPowerOfThree(self, n: int) -> bool:
-#         # while문 사용
-#         # n이 1이 될 때까지 3으로 나눠보고
-#         # 만약 3으로 나눠떨어지지 않는다면 3의 제곱수가 아닌 것
-#         # n이 0보다 같거나 작은 경우 예외처리
 #         if n <= 0:
 #             return False
 #         while True:
 #             if n == 1:
 #                 break
-#             # 3으로 나눠떨어지면 3으로 나눠주고
-#             # 만약 그렇지 않으면 False를 리턴
 #             if n % 3 == 0:
 #                 n /= 3
 #             else:
 #                 return False
 #         return True
-
-class Solution:
-    def isPowerOfThree(self, n: int) -> bool:
-        # while문 사용
-        # n이 1이 될 때까지 3으로 나눠보고
-        # 만약 3으로 나눠떨어지지 않는다면 3의 제곱수가 아닌 것
-        # n이 0보다 같거나 작은 경우 예외처리
-        if n <= 0:
-            return False
-        while True:
-            # 3으로 나눠떨어지면 3으로 나눠주고
-            # 만약 그렇지 않으면 False를 리턴
-            if n == 1:
-                break
-            if n % 3 == 0:
-                n /= 3
-            else:
-                return False
-        return True
 
 # class Solution:
 #     def isPowerOfThree(self, n: int) -> bool:
