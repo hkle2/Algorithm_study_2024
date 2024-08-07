181b4f883c4db853addd01eae4c5ec8554a8e640
hkle2 <minnie110598@gmail.com>
2024-08-07 09:15:47 +0900

    Time: 28 ms (91.7%), Space: 16.4 MB (97.86%) - LeetHub

--- LeetCode/Easy/1490-generate-a-string-with-characters-that-have-odd-counts/1490-generate-a-string-with-characters-that-have-odd-counts.py
+++ LeetCode/Easy/1490-generate-a-string-with-characters-that-have-odd-counts/1490-generate-a-string-with-characters-that-have-odd-counts.py
@@ -2,7 +2,6 @@ class Solution:
     def generateTheString(self, n: int) -> str:
         for i in range(n):
             if n % 2 == 0:
-                answer = "a" * (n-1) + "b"
+                return "a" * (n-1) + "b"
             else:
-                answer = "a" * n
-        return answer
+                return "a" * n
