0add60190388d2c8e0be33a10ead69ca3763ea2c
hkle2 <minnie110598@gmail.com>
2024-08-07 10:02:44 +0900

    Time: 33 ms (71.45%), Space: 16.5 MB (76.6%) - LeetHub

--- LeetCode/Easy/1490-generate-a-string-with-characters-that-have-odd-counts/1490-generate-a-string-with-characters-that-have-odd-counts.py
+++ LeetCode/Easy/1490-generate-a-string-with-characters-that-have-odd-counts/1490-generate-a-string-with-characters-that-have-odd-counts.py
@@ -1,15 +1,7 @@
 class Solution:
     def generateTheString(self, n: int) -> str:
-        # n이 홀수일 때와 짝수일 때를 구분지어 볼 것
-        if n % 2 == 1:
-            return "a" * n
-        else:
-            return "a" * (n-1) + "b"
-
-# class Solution:
-#     def generateTheString(self, n: int) -> str:
-#         for i in range(n):
-#             if n % 2 == 0:
-#                 return "a" * (n-1) + "b"
-#             else:
-#                 return "a" * n
+        for i in range(n):
+            if n % 2 == 0:
+                return "a" * (n-1) + "b"
+            else:
+                return "a" * n
