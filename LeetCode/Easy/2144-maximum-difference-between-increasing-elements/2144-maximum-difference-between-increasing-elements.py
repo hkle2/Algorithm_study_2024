b00139ebd10c4af7571a0101b91abcbd74d11104
hkle2 <minnie110598@gmail.com>
2024-08-07 10:03:45 +0900

    Time: 175 ms (20.59%), Space: 16.7 MB (19.91%) - LeetHub

--- LeetCode/Easy/2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.py
+++ LeetCode/Easy/2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.py
@@ -9,11 +9,11 @@ class Solution:
                 min_num = num
         return answer
 
-# class Solution:
-#     def maximumDifference(self, nums: List[int]) -> int:
-#         answer = -1
-#         for i in range(len(nums)):
-#             for j in range(i+1, len(nums)):
-#                 if nums[i] < nums[j]:
-#                     answer = max(answer, (nums[j] - nums[i]))
-#         return answer
+class Solution:
+    def maximumDifference(self, nums: List[int]) -> int:
+        answer = -1
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] < nums[j]:
+                    answer = max(answer, (nums[j] - nums[i]))
+        return answer
