f108bbbf42a2fe8a761a1cff0ebb9d08b1df9f70
hkle2 <minnie110598@gmail.com>
2024-08-08 09:33:36 +0900

    Time: 32 ms (80.49%), Space: 16.4 MB (98.87%) - LeetHub

--- LeetCode/Easy/0263-ugly-number/0263-ugly-number.py
+++ LeetCode/Easy/0263-ugly-number/0263-ugly-number.py
@@ -1,25 +1,25 @@
-class Solution:
-    def isUgly(self, n: int) -> bool:
-        if n == 0:
-            return False
-        for i in [2, 3, 5]:
-            while n % i == 0:
-                n /= i
-            if n == 1:
-                return True
-
 # class Solution:
 #     def isUgly(self, n: int) -> bool:
-#         while True:
-#             if n == 0:
-#                 return False
+#         if n == 0:
+#             return False
+#         for i in [2, 3, 5]:
+#             while n % i == 0:
+#                 n /= i
 #             if n == 1:
 #                 return True
-#             elif n % 2 == 0:
-#                 n /= 2
-#             elif n % 3 == 0:
-#                 n /= 3
-#             elif n % 5 == 0:
-#                 n /= 5
-#             else:
-#                 return False
+
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        while True:
+            if n == 0:
+                return False
+            if n == 1:
+                return True
+            elif n % 2 == 0:
+                n /= 2
+            elif n % 3 == 0:
+                n /= 3
+            elif n % 5 == 0:
+                n /= 5
+            else:
+                return False
