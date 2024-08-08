2ae89c7e474efaac9e3b8c953e3d62be134d190e
hkle2 <minnie110598@gmail.com>
2024-08-08 10:11:10 +0900

    Time: 79 ms (63.86%), Space: 18.2 MB (26.2%) - LeetHub

--- LeetCode/Easy/1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.py
+++ LeetCode/Easy/1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.py
@@ -3,24 +3,22 @@ class Solution:
         quater = len(arr) / 4
         c = Counter(arr)
         for num, cnt in c.items():
-            # cnt를 quater와 비교
-            # 만약 quater 보다 크다면, answer로 지정할 것
             if cnt > quater:
                 answer = num
         return answer
 
-# class Solution:
-#     def findSpecialInteger(self, arr: List[int]) -> int:
-#         for n in arr:
-#             if arr.count(n) > len(arr) * 0.25:
-#                 return n
-#         return 0
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        for n in arr:
+            if arr.count(n) > len(arr) * 0.25:
+                return n
+        return 0
 
-# from collections import Counter
+from collections import Counter
 
-# class Solution:
-#     def findSpecialInteger(self, arr: List[int]) -> int:
-#         c = Counter(arr)
-#         for key, value in c.items():
-#             if value > len(arr) * 0.25:
-#                 return key
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        c = Counter(arr)
+        for key, value in c.items():
+            if value > len(arr) * 0.25:
+                return key
