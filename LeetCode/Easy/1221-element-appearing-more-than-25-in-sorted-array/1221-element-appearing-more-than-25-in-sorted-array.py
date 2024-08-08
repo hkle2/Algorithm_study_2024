ff6618ff098497cc9422e57956013b0b9028e006
hkle2 <minnie110598@gmail.com>
2024-08-08 09:25:52 +0900

    Time: 91 ms (21.34%), Space: 18.3 MB (10.4%) - LeetHub

--- LeetCode/Easy/1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.py
+++ LeetCode/Easy/1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.py
@@ -1,6 +1,15 @@
+from collections import Counter
+
 class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
-        for n in arr:
-            if arr.count(n) > len(arr) * 0.25:
-                return n
-        return 0
+        c = Counter(arr)
+        for key, value in c.items():
+            if value > len(arr) * 0.25:
+                return key
+
+# class Solution:
+#     def findSpecialInteger(self, arr: List[int]) -> int:
+#         for n in arr:
+#             if arr.count(n) > len(arr) * 0.25:
+#                 return n
+#         return 0
