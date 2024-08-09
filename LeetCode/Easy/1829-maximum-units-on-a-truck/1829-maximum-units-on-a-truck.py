f68c940531f6e84a6cace8ec1c2493cf95928515
hkle2 <minnie110598@gmail.com>
2024-08-09 09:28:19 +0900

    Time: 125 ms (97.09%), Space: 16.9 MB (63.74%) - LeetHub

--- LeetCode/Easy/1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.py
+++ LeetCode/Easy/1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.py
@@ -2,12 +2,25 @@ class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
         answer = 0
         boxTypes.sort(key=lambda x: -x[1])
-        for i in range(len(boxTypes)):
-            if truckSize > 0:
-                if truckSize >= boxTypes[i][0]:
-                    answer += boxTypes[i][0] * boxTypes[i][1]
-                    truckSize -= boxTypes[i][0]
-                else:
-                    answer += truckSize * boxTypes[i][1]
-                    truckSize -= truckSize
+        for i, [num, unit] in enumerate(boxTypes):
+            if truckSize >= num:
+                answer += num * unit
+                truckSize -= num
+            else:
+                answer += truckSize * unit
+                break
         return answer
+
+# class Solution:
+#     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+#         answer = 0
+#         boxTypes.sort(key=lambda x: -x[1])
+#         for i in range(len(boxTypes)):
+#             if truckSize > 0:
+#                 if truckSize >= boxTypes[i][0]:
+#                     answer += boxTypes[i][0] * boxTypes[i][1]
+#                     truckSize -= boxTypes[i][0]
+#                 else:
+#                     answer += truckSize * boxTypes[i][1]
+#                     truckSize -= truckSize
+#         return answer
