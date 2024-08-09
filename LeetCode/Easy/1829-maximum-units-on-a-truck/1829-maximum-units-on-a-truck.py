633e13a4b00b8deef0f194a12c7591701e2cc585
hkle2 <minnie110598@gmail.com>
2024-08-09 09:45:35 +0900

    Time: 136 ms (63.06%), Space: 17.1 MB (31.6%) - LeetHub

--- LeetCode/Easy/1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.py
+++ LeetCode/Easy/1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.py
@@ -1,13 +1,18 @@
-# class Solution:
-#     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
-#         # sorting, for
-#         sortedBoxTypes = sorted(boxTypes, key=lambda x: x[1], reverse=True)
-#         answer = 0
-#         for numBoxes, unit in sortedBoxTypes:
-#             # 트럭에 실을 수 있는 수량만큼 싣고
-#             # 실을 수량 * 유닛만큼 answer에 더해주기
-#             print(numBoxes, unit)
-#         return answer
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        # sorting, for
+        sortedBoxTypes = sorted(boxTypes, key=lambda x: x[1], reverse=True)
+        answer = 0
+        for numBoxes, unit in sortedBoxTypes:
+            # 트럭에 실을 수 있는 수량만큼 싣고
+            # 실을 수량 * 유닛만큼 answer에 더해주기
+            if truckSize >= numBoxes:
+                answer += numBoxes * unit
+                truckSize -= numBoxes
+            else:
+                answer += truckSize * unit
+                break
+        return answer
 
 # class Solution:
 #     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
@@ -34,16 +39,3 @@
 #                 answer += truckSize * unit
 #                 break
 #         return answer
-
-class Solution:
-    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
-        answer = 0
-        boxTypes.sort(key=lambda x: -x[1])
-        for num, unit in boxTypes:
-            if truckSize >= num:
-                answer += num * unit
-                truckSize -= num
-            else:
-                answer += truckSize * unit
-                break
-        return answer
