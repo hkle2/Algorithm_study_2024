16f90deb3bf13205138ed1f2d76bfa04e08f9564
hkle2 <minnie110598@gmail.com>
2024-08-09 10:04:40 +0900

    Time: 223 ms (89.94%), Space: 22.9 MB (43.16%) - LeetHub

--- LeetCode/Easy/1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.py
+++ LeetCode/Easy/1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.py
@@ -1,29 +1,26 @@
-# class Solution:
-#     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
-#         answer = 0
-#         for type_, color, name in items:
-#             # ruleKey가 type이고, type이 ruleValue와 같으면 answer += 1
-#             # ruleKey가 color이고, color가 ruleValue와 같으면 answer += 1
-#             # ruleKey가 name이고, name이 ruleValue와 같으면 answer += 1
-#             if ruleKey == "type" and type_ == ruleValue:
-#                 answer += 1
-#             elif ruleKey == "color" and color == ruleValue:
-#                 answer += 1
-#             elif ruleKey == "name" and name == ruleValue:
-#                 answer += 1
-#         return answer
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        answer = 0
+        for type_, color, name in items:
+            if ruleKey == "type" and type_ == ruleValue:
+                answer += 1
+            elif ruleKey == "color" and color == ruleValue:
+                answer += 1
+            elif ruleKey == "name" and name == ruleValue:
+                answer += 1
+        return answer
 
-# class Solution:
-#     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
-#         answer = 0
-#         for i in range(len(items)):
-#             if ruleKey == "type" and items[i][0] == ruleValue:
-#                     answer += 1
-#             elif ruleKey == "color" and items[i][1] == ruleValue:
-#                     answer += 1
-#             elif ruleKey == "name" and items[i][2] == ruleValue:
-#                     answer += 1
-#         return answer
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        answer = 0
+        for i in range(len(items)):
+            if ruleKey == "type" and items[i][0] == ruleValue:
+                    answer += 1
+            elif ruleKey == "color" and items[i][1] == ruleValue:
+                    answer += 1
+            elif ruleKey == "name" and items[i][2] == ruleValue:
+                    answer += 1
+        return answer
 
 class Solution:
     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
