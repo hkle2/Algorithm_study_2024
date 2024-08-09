ffefa10c11676e110d98e46d8a00567ba813c1ec
hkle2 <minnie110598@gmail.com>
2024-08-09 09:47:13 +0900

    Time: 225 ms (84.62%), Space: 22.8 MB (43.16%) - LeetHub

--- LeetCode/Easy/1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.py
+++ LeetCode/Easy/1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.py
@@ -1,3 +1,33 @@
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        answer = 0
+        for type_, color, name in items:
+            # ruleKey가 type이고, type이 ruleValue와 같으면 answer += 1
+            # ruleKey가 color이고, color가 ruleValue와 같으면 answer += 1
+            # ruleKey가 name이고, name이 ruleValue와 같으면 answer += 1
+            if ruleKey == "type" and type_ == ruleValue:
+                answer += 1
+            elif ruleKey == "color" and color == ruleValue:
+                answer += 1
+            elif ruleKey == "name" and name == ruleValue:
+                answer += 1
+        return answer
+
+# class Solution:
+#     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+#         answer = 0
+#         for i in range(len(items)):
+#             if ruleKey == "type":
+#                 if items[i][0] == ruleValue:
+#                     answer += 1
+#             elif ruleKey == "color":
+#                 if items[i][1] == ruleValue:
+#                     answer += 1
+#             else:
+#                 if items[i][2] == ruleValue:
+#                     answer += 1
+#         return answer
+
 # class Solution:
 #     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
 #         answer = 0
@@ -11,18 +41,3 @@
 #             if items[i][n] == ruleValue:
 #                 answer += 1
 #         return answer
-
-class Solution:
-    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
-        answer = 0
-        for i in range(len(items)):
-            if ruleKey == "type":
-                if items[i][0] == ruleValue:
-                    answer += 1
-            elif ruleKey == "color":
-                if items[i][1] == ruleValue:
-                    answer += 1
-            else:
-                if items[i][2] == ruleValue:
-                    answer += 1
-        return answer
