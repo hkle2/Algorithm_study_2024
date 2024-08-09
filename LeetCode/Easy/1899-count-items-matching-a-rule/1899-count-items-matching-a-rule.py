1ce86378f6509f8746d45f77c3f4a3e3a0ca3f75
hkle2 <minnie110598@gmail.com>
2024-08-09 09:37:23 +0900

    Time: 240 ms (17.49%), Space: 22.6 MB (85.67%) - LeetHub

--- LeetCode/Easy/1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.py
+++ LeetCode/Easy/1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.py
@@ -1,29 +1,28 @@
-class Solution:
-    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
-        answer = 0
-        for i, [t, c, n] in enumerate(items):
-            if ruleKey == "type":
-                if t == ruleValue:
-                    answer += 1
-            elif ruleKey == "color":
-                if c == ruleValue:
-                    answer += 1
-            else:
-                if n == ruleValue:
-                    answer += 1
-        return answer
-
 # class Solution:
 #     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
 #         answer = 0
+#         if ruleKey == "type":
+#            n = 0
+#         elif ruleKey == "color":
+#             n = 1
+#         else:
+#             n = 2
 #         for i in range(len(items)):
-#             if ruleKey == "type":
-#                 if items[i][0] == ruleValue:
-#                     answer += 1
-#             elif ruleKey == "color":
-#                 if items[i][1] == ruleValue:
-#                     answer += 1
-#             else:
-#                 if items[i][2] == ruleValue:
-#                     answer += 1
+#             if items[i][n] == ruleValue:
+#                 answer += 1
 #         return answer
+
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        answer = 0
+        for i in range(len(items)):
+            if ruleKey == "type":
+                if items[i][0] == ruleValue:
+                    answer += 1
+            elif ruleKey == "color":
+                if items[i][1] == ruleValue:
+                    answer += 1
+            else:
+                if items[i][2] == ruleValue:
+                    answer += 1
+        return answer
