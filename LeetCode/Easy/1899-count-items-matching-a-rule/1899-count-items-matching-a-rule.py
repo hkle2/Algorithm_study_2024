9fda42a7f0f2306188523bb8c9df273e089021fe
hkle2 <minnie110598@gmail.com>
2024-08-09 09:33:51 +0900

    Time: 232 ms (55.05%), Space: 22.8 MB (43.16%) - LeetHub

--- LeetCode/Easy/1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.py
+++ LeetCode/Easy/1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.py
@@ -1,14 +1,29 @@
 class Solution:
     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
         answer = 0
-        for i in range(len(items)):
+        for i, [t, c, n] in enumerate(items):
             if ruleKey == "type":
-                if items[i][0] == ruleValue:
+                if t == ruleValue:
                     answer += 1
             elif ruleKey == "color":
-                if items[i][1] == ruleValue:
+                if c == ruleValue:
                     answer += 1
             else:
-                if items[i][2] == ruleValue:
+                if n == ruleValue:
                     answer += 1
         return answer
+
+# class Solution:
+#     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+#         answer = 0
+#         for i in range(len(items)):
+#             if ruleKey == "type":
+#                 if items[i][0] == ruleValue:
+#                     answer += 1
+#             elif ruleKey == "color":
+#                 if items[i][1] == ruleValue:
+#                     answer += 1
+#             else:
+#                 if items[i][2] == ruleValue:
+#                     answer += 1
+#         return answer
