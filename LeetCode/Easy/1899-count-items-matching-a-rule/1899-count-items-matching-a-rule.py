d448663390e40eb98a1ea7cd07cf6cbacf7381d5
hkle2 <minnie110598@gmail.com>
2024-08-09 09:08:24 +0900

    Time: 227 ms (77.82%), Space: 22.7 MB (43.16%) - LeetHub

---
+++ LeetCode/Easy/1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        answer = 0
+        for i in range(len(items)):
+            if ruleKey == "type":
+                if items[i][0] == ruleValue:
+                    answer += 1
+            elif ruleKey == "color":
+                if items[i][1] == ruleValue:
+                    answer += 1
+            else:
+                if items[i][2] == ruleValue:
+                    answer += 1
+        return answer
