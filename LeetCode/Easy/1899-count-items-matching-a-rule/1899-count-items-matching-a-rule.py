db092d6d8b6aa1d89be2efe42e187982849431ef
hkle2 <minnie110598@gmail.com>
2024-08-09 09:48:58 +0900

    Time: 216 ms (98.35%), Space: 22.8 MB (43.16%) - LeetHub

--- LeetCode/Easy/1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.py
+++ LeetCode/Easy/1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.py
@@ -13,21 +13,6 @@
 #                 answer += 1
 #         return answer
 
-class Solution:
-    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
-        answer = 0
-        for i in range(len(items)):
-            if ruleKey == "type":
-                if items[i][0] == ruleValue:
-                    answer += 1
-            elif ruleKey == "color":
-                if items[i][1] == ruleValue:
-                    answer += 1
-            else:
-                if items[i][2] == ruleValue:
-                    answer += 1
-        return answer
-
 # class Solution:
 #     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
 #         answer = 0
@@ -40,16 +25,16 @@ class Solution:
 #                     answer += 1
 #         return answer
 
-# class Solution:
-#     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
-#         answer = 0
-#         if ruleKey == "type":
-#            n = 0
-#         elif ruleKey == "color":
-#             n = 1
-#         else:
-#             n = 2
-#         for i in range(len(items)):
-#             if items[i][n] == ruleValue:
-#                 answer += 1
-#         return answer
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        answer = 0
+        if ruleKey == "type":
+           n = 0
+        elif ruleKey == "color":
+            n = 1
+        else:
+            n = 2
+        for i in range(len(items)):
+            if items[i][n] == ruleValue:
+                answer += 1
+        return answer
