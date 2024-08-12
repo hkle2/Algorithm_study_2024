27786f6a2ddd518a7a90f43db8e5b09b94bf57ea
hkle2 <minnie110598@gmail.com>
2024-08-12 10:04:35 +0900

    Time: 27 ms (92.23%), Space: 16.6 MB (27.3%) - LeetHub

--- LeetCode/Easy/3371-harshad-number/3371-harshad-number.py
+++ LeetCode/Easy/3371-harshad-number/3371-harshad-number.py
@@ -1,5 +1,15 @@
 class Solution:
     def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
+        if x < 10:
+            return x
+        answer = -1
+        digit_sum = sum([int(c) for c in str(x)])
+        if x % digit_sum == 0:
+            return digit_sum
+        return answer
+
+class Solution:
+    def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
         s = str(x)
         num = 0
         for i in range(len(s)):
