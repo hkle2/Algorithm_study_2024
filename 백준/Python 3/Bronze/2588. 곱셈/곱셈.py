408ad9b800035e10d6c3429feca4bf420a92cc1d
hkle2 <minnie110598@gmail.com>
2024-08-18 20:57:47 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Python 3/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,9 @@
+import sys
+
+num1 = sys.stdin.readline().strip()
+num2 = sys.stdin.readline().strip()
+
+print(int(num1) * int(num2[2]))
+print(int(num1) * int(num2[1]))
+print(int(num1) * int(num2[0]))
+print(int(num1) * int(num2))
