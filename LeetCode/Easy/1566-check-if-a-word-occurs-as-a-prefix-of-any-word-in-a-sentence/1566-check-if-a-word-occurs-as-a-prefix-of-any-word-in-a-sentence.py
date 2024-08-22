ca672043101739fb9e9af77e0792b5be20b84ac4
hkle2 <minnie110598@gmail.com>
2024-08-22 09:11:22 +0900

    Time: 35 ms (52.12%), Space: 16.5 MB (19.01%) - LeetHub

--- LeetCode/Easy/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
+++ LeetCode/Easy/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
@@ -1,7 +1,15 @@
 class Solution:
     def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
-        s = sentence.split(" ")
-        for i in range(len(s)):
-            if s[i].startswith(searchWord):
+        # split, enumerate, startswith
+        for i, s in enumerate(sentence.split(" ")):
+            if s.startswith(searchWord):
                 return i + 1
         return -1
+
+# class Solution:
+#     def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+#         s = sentence.split(" ")
+#         for i in range(len(s)):
+#             if s[i].startswith(searchWord):
+#                 return i + 1
+#         return -1
