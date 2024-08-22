738b65e6b16b91189caa67f36dd3cbc8e32eb72d
hkle2 <minnie110598@gmail.com>
2024-08-22 10:08:03 +0900

    Time: 63 ms (91.01%), Space: 16.6 MB (7.62%) - LeetHub

--- LeetCode/Easy/3533-snake-in-matrix/3533-snake-in-matrix.py
+++ LeetCode/Easy/3533-snake-in-matrix/3533-snake-in-matrix.py
@@ -1,23 +1,21 @@
 class Solution:
     def finalPositionOfSnake(self, n: int, commands: List[str]) -> int:
-        # 위아래로 이동한 양, 좌우로 이동한 양 계산
-        # 이를 그리드 상의 값으로 매핑
         c = Counter(commands)
         move_y = c['DOWN'] - c['UP']
         move_x = c['RIGHT'] - c['LEFT']
         answer = move_y * n  +move_x
         return answer
 
-# class Solution:
-#     def finalPositionOfSnake(self, n: int, commands: List[str]) -> int:
-#         answer = 0
-#         for command in commands:
-#             if command == "UP":
-#                 answer -= n
-#             elif command == "RIGHT":
-#                 answer += 1
-#             elif command == "DOWN":
-#                 answer += n
-#             else:
-#                 answer -= 1
-#         return answer
+class Solution:
+    def finalPositionOfSnake(self, n: int, commands: List[str]) -> int:
+        answer = 0
+        for command in commands:
+            if command == "UP":
+                answer -= n
+            elif command == "RIGHT":
+                answer += 1
+            elif command == "DOWN":
+                answer += n
+            else:
+                answer -= 1
+        return answer
