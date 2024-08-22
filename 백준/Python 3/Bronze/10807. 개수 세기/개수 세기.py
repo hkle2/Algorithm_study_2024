e7beba4138736a1b10c7a4abfeb6b5525ac786b0
hkle2 <minnie110598@gmail.com>
2024-08-22 21:24:30 +0900

    [Bronze V] Title: 개수 세기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Python 3/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Python 3/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,6 +1,8 @@
 import sys
 
-N = int(sys.stdin.readline().rstrip())
-l = list(map(int, sys.stdin.readline().rstrip().split()))
-v = int(sys.stdin.readline().rstrip())
+N = int(sys.stdin.readline().strip())
+l = list(map(int, sys.stdin.readline().strip().split(" ")))
+v = int(sys.stdin.readline().strip())
+
 print(l.count(v))
+
