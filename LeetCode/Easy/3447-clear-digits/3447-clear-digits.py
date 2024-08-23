f6bcb4b10199e8f1fa298f45f28e043eb0d1d432
hkle2 <minnie110598@gmail.com>
2024-08-23 10:20:21 +0900

    Time: 27 ms (97.28%), Space: 16.4 MB (92.62%) - LeetHub

--- LeetCode/Easy/3447-clear-digits/3447-clear-digits.py
+++ LeetCode/Easy/3447-clear-digits/3447-clear-digits.py
@@ -1,12 +1,7 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
-        # while, 숫자를 지울 수 있는 조건 잘 살펴보기
         while s:
             flag = False
-            # 첫 번째 숫자 찾아서 제거
-            # 숫자 제거, 문자열 slicing 활용
-            # 3번째 위치한 문자를 제거
-            # s = s[:3] + s[4:]
             for i, c in enumerate(s):
                 if c.isnumeric():
                     flag = True
@@ -19,12 +14,12 @@ class Solution:
                 break
         return s
 
-# class Solution:
-#     def clearDigits(self, s: str) -> str:
-#         answer = []
-#         for i in s:
-#             if i.isdigit():
-#                 answer.pop()
-#             else:
-#                 answer.append(i)
-#         return "".join(answer)
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        answer = []
+        for i in s:
+            if i.isdigit():
+                answer.pop()
+            else:
+                answer.append(i)
+        return "".join(answer)
