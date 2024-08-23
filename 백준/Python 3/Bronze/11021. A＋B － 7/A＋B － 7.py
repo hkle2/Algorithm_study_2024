a01f3cba6c53a35d38c610426bd516b5467db13b
hkle2 <minnie110598@gmail.com>
2024-08-23 15:47:33 +0900

    [Bronze V] Title: A+B - 7, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Python 3/Bronze/11021. A＋B － 7/A＋B － 7.py
+++ 백준/Python 3/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -3,5 +3,5 @@ import sys
 x = int(sys.stdin.readline().strip())
 
 for i in range(1, x+1):
-    A, B = map(int, sys.stdin.readline().split(" "))
+    A, B = map(int, sys.stdin.readline().split())
     print(f"Case #{i}: {A + B}")
