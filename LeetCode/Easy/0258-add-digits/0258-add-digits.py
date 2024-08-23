3870ac20ca12152d9dbb9ffc7bfb4c6651ad3314
hkle2 <minnie110598@gmail.com>
2024-08-23 09:45:38 +0900

    Time: 46 ms (6.34%), Space: 16.5 MB (70.83%) - LeetHub

--- LeetCode/Easy/0258-add-digits/0258-add-digits.py
+++ LeetCode/Easy/0258-add-digits/0258-add-digits.py
@@ -1,11 +1,22 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        s = str(num)
-        while True:
-            answer = 0
-            for i in range(len(s)):
-                answer += int(s[i])
-            s = str(answer)
-            if len(s) == 1:
-                break
-        return int(s)
+        # while문, 숫자 -> 문자열 -> 숫자
+        while num >= 10:
+            # 각 자릿 수의 합계를 num에다가 입력
+            digit_sum = 0
+            for c in str(num):
+                digit_sum += int(c)
+            num = digit_sum
+        return num
+
+# class Solution:
+#     def addDigits(self, num: int) -> int:
+#         s = str(num)
+#         while True:
+#             answer = 0
+#             for i in range(len(s)):
+#                 answer += int(s[i])
+#             s = str(answer)
+#             if len(s) == 1:
+#                 break
+#         return int(s)
