341c05f78bf8527adfeb7d5bd838e7087ec66e5e
hkle2 <minnie110598@gmail.com>
2024-08-23 10:04:20 +0900

    Time: 33 ms (76.67%), Space: 16.5 MB (29.75%) - LeetHub

--- LeetCode/Easy/0258-add-digits/0258-add-digits.py
+++ LeetCode/Easy/0258-add-digits/0258-add-digits.py
@@ -13,10 +13,10 @@ class Solution:
     def addDigits(self, num: int) -> int:
         s = str(num)
         while True:
-            if len(s) == 1:
-                break
             answer = 0
             for i in range(len(s)):
                 answer += int(s[i])
             s = str(answer)
+            if len(s) == 1:
+                break
         return int(s)
