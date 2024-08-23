e24a081bbcd3899c9685778203453b19eb0d02d7
hkle2 <minnie110598@gmail.com>
2024-08-23 10:18:36 +0900

    Time: 34 ms (78.22%), Space: 16.5 MB (63.72%) - LeetHub

--- LeetCode/Easy/3447-clear-digits/3447-clear-digits.py
+++ LeetCode/Easy/3447-clear-digits/3447-clear-digits.py
@@ -1,9 +1,30 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
-        answer = []
-        for i in s:
-            if i.isdigit():
-                answer.pop()
-            else:
-                answer.append(i)
-        return "".join(answer)
+        # while, 숫자를 지울 수 있는 조건 잘 살펴보기
+        while s:
+            flag = False
+            # 첫 번째 숫자 찾아서 제거
+            # 숫자 제거, 문자열 slicing 활용
+            # 3번째 위치한 문자를 제거
+            # s = s[:3] + s[4:]
+            for i, c in enumerate(s):
+                if c.isnumeric():
+                    flag = True
+                    if i == 0:
+                        s = s[i+1:]
+                    else:
+                        s = s[:i-1] + s[i+1:]
+                    break
+            if not flag:
+                break
+        return s
+
+# class Solution:
+#     def clearDigits(self, s: str) -> str:
+#         answer = []
+#         for i in s:
+#             if i.isdigit():
+#                 answer.pop()
+#             else:
+#                 answer.append(i)
+#         return "".join(answer)
