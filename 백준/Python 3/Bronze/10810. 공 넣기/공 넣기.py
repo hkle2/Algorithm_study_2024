bcb846cc6ec3de01bf8cb096044995bf4fb3b558
hkle2 <minnie110598@gmail.com>
2024-08-23 16:18:20 +0900

    [Bronze III] Title: 공 넣기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Python 3/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Python 3/Bronze/10810. 공 넣기/공 넣기.py
@@ -1,10 +1,10 @@
 import sys
 
-N, M = map(int, sys.stdin.readline().split(" "))
+N, M = map(int, sys.stdin.readline().split())
 l = ["0"] * N
 
 for m in range(M):
-    i, j, k = map(int, sys.stdin.readline().split(" "))
+    i, j, k = map(int, sys.stdin.readline().split())
     for c in range(i, j+1):
         l[c-1] = str(k)
 
