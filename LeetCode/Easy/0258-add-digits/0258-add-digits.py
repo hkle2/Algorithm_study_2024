78cd86c6de51b3ee24a2d4228fbdc44ea8ca99b2
hkle2 <minnie110598@gmail.com>
2024-08-23 10:07:24 +0900

    Time: 44 ms (9.45%), Space: 16.4 MB (70.83%) - LeetHub

--- LeetCode/Easy/0258-add-digits/0258-add-digits.py
+++ LeetCode/Easy/0258-add-digits/0258-add-digits.py
@@ -1,20 +1,17 @@
 class Solution:
     def addDigits(self, num: int) -> int:
         while num >= 10:
-            digit_sum = 0
-            for c in str(num):
-                digit_sum += int(c)
-            num = digit_sum
+            num = sum([int(c) for c in str(num)])
         return num
 
-class Solution:
-    def addDigits(self, num: int) -> int:
-        s = str(num)
-        while True:
-            sum_ = 0
-            for i in range(len(s)):
-                sum_ += int(s[i])
-            s = str(sum_)
-            if len(s) == 1:
-                break
-        return int(s)
+# class Solution:
+#     def addDigits(self, num: int) -> int:
+#         s = str(num)
+#         while True:
+#             sum_ = 0
+#             for i in range(len(s)):
+#                 sum_ += int(s[i])
+#             s = str(sum_)
+#             if len(s) == 1:
+#                 break
+#         return int(s)
