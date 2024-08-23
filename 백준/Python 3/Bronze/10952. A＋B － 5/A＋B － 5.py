fdad578374c41867fb53933c73f6be13912c917a
hkle2 <minnie110598@gmail.com>
2024-08-23 15:53:20 +0900

    [Bronze V] Title: A+B - 5, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Python 3/Bronze/10952. A＋B － 5/A＋B － 5.py
+++ 백준/Python 3/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -1,7 +1,7 @@
 import sys
 
 while True:
-    A, B = map(int, sys.stdin.readline().split(" "))
+    A, B = map(int, sys.stdin.readline().split())
     if A + B != 0:
         print(A + B)
     else:
