3f5db748c5e3ca07ac4137289fe4a9ad3acad9cb
hkle2 <minnie110598@gmail.com>
2024-08-24 00:21:39 +0900

    [Bronze III] Title: 공 넣기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Python 3/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Python 3/Bronze/10810. 공 넣기/공 넣기.py
@@ -1,11 +1,12 @@
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
-l = ["0"] * N
+basket = [0] * N
 
 for m in range(M):
     i, j, k = map(int, sys.stdin.readline().split())
     for c in range(i, j+1):
-        l[c-1] = str(k)
+        basket[c-1] = k
 
-print(" ".join(l))
+for ball in basket:
+    print(ball, end=" ")
