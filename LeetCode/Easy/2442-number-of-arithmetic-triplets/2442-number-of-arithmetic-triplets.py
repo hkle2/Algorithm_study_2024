779369e56c7ab81cb1ef33fad5f0cd28525146f9
hkle2 <minnie110598@gmail.com>
2024-08-26 10:07:09 +0900

    Time: 531 ms (34.18%), Space: 16.5 MB (30.82%) - LeetHub

--- LeetCode/Easy/2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.py
+++ LeetCode/Easy/2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.py
@@ -1,30 +1,17 @@
 class Solution:
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
-        # 3중 for문 사용
         answer = 0
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 for k in range(j+1, len(nums)):
-                    # nums[j] - nums[i] == diff, and
-                    # nums[k] - nums[j] == diff일 때 answer에 1 더해주기
                     if (nums[j] - nums[i] == diff) and (nums[k] - nums[j] == diff):
                         answer += 1
         return answer
 
-# class Solution:
-#     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
-#         answer = 0
-#         for i in range(len(nums)):
-#             for j in range(i+1, len(nums)):
-#                 for k in range(j+1, len(nums)):
-#                     if (nums[j] - nums[i] == diff) and (nums[k] - nums[j] == diff):
-#                         answer += 1
-#         return answer
-
-# class Solution:
-#     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
-#         answer = 0
-#         for a, b, c in combinations(nums, 3):
-#             if (b - a == diff) and (c - b == diff):
-#                 answer += 1
-#         return answer
+class Solution:
+    def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        answer = 0
+        for a, b, c in combinations(nums, 3):
+            if (b - a == diff) and (c - b == diff):
+                answer += 1
+        return answer
