afeea405e0c9dcb5120d4815f1652cbcb7c17c9b
hkle2 <minnie110598@gmail.com>
2024-08-26 09:35:32 +0900

    Time: 117 ms (15.96%), Space: 17.3 MB (54.05%) - LeetHub

--- LeetCode/Easy/2447-merge-similar-items/2447-merge-similar-items.py
+++ LeetCode/Easy/2447-merge-similar-items/2447-merge-similar-items.py
@@ -1,13 +1,12 @@
+from collections import defaultdict
+
 class Solution:
     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
         items = items1 + items2
-        items_dict = {}
+        items_dict = defaultdict(int)
         answer = []
         for value, weight in items:
-            if value not in items_dict:
-                items_dict[value] = weight
-            else:
-                items_dict[value] += weight
+            items_dict[value] += weight
         for key, value in items_dict.items():
             answer.append([key, value])
         return sorted(answer)
