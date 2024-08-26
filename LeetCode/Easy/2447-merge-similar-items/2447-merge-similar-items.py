4b61f0488db568319c52f2c3c96f0b2fd7bf8184
hkle2 <minnie110598@gmail.com>
2024-08-26 10:05:09 +0900

    Time: 105 ms (78.72%), Space: 17.4 MB (54.05%) - LeetHub

--- LeetCode/Easy/2447-merge-similar-items/2447-merge-similar-items.py
+++ LeetCode/Easy/2447-merge-similar-items/2447-merge-similar-items.py
@@ -1,10 +1,33 @@
-from collections import defaultdict
-
 class Solution:
     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
-        items = items1 + items2
-        items_dict = defaultdict(int)
+        # dictionary, for
+        count_dict = defaultdict(int)
+        for value, weight in items1 + items2:
+            count_dict[value] += weight
+        sorted_keys = sorted(count_dict.keys())
         answer = []
-        for value, weight in items:
-            items_dict[value] += weight
-        return sorted(items_dict.items())
+        for key in sorted_keys:
+            answer.append([key, count_dict[key]])
+        return answer
+
+# from collections import defaultdict
+
+# class Solution:
+#     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
+#         items = items1 + items2
+#         items_dict = defaultdict(int)
+#         answer = []
+#         for value, weight in items:
+#             items_dict[value] += weight
+#         return sorted(items_dict.items())
+
+# class Solution:
+#     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
+#         items = items1 + items2
+#         items_dict = defaultdict(int)
+#         answer = []
+#         for value, weight in items:
+#             items_dict[value] += weight
+#         for key, value in items_dict.items():
+#             answer.append([key, value])
+#         return sorted(answer)
