8dfd3a195f6bf870bc93696f1e3ca3d930100cf2
hkle2 <minnie110598@gmail.com>
2024-08-27 09:55:24 +0900

    Time: 470 ms (25.3%), Space: 18.7 MB (19.52%) - LeetHub

--- LeetCode/Easy/1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
+++ LeetCode/Easy/1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
@@ -2,11 +2,8 @@ class Solution:
     def makeFancyString(self, s: str) -> str:
         seen = []
         for i in range(len(s)):
-            if s[i] not in seen:
-                seen.append(s[i])
+            if (len(seen) >= 2) and (s[i] == seen[-1]) and (s[i] == seen[-2]):
+                continue
             else:
-                if len(seen) >= 2 and (s[i] == seen[-1]) and (s[i] == seen[-2]):
-                    continue
-                else:
-                    seen.append(s[i])
+                seen.append(s[i])
         return "".join(seen)
