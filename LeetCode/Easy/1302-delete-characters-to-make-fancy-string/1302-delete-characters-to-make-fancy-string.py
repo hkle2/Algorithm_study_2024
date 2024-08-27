910fab55daabe428ee7a44de7f4a4081a35453e0
hkle2 <minnie110598@gmail.com>
2024-08-27 10:02:35 +0900

    Time: 247 ms (88.45%), Space: 18.6 MB (50.6%) - LeetHub

--- LeetCode/Easy/1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
+++ LeetCode/Easy/1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
@@ -1,9 +1,27 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        seen = []
-        for i in range(len(s)):
-            if (len(seen) >= 2) and (s[i] == seen[-1]) and (s[i] == seen[-2]):
-                continue
+        answer = ""
+        prev = ""
+        cnt = 0
+        for c in s:
+            # same: cnt 값을 1 더해줌
+            # differenct: cnt 값을 1로 초기화
+            # cnt 값이 3보다 작으면 answer에 추가
+            if c == prev:
+                cnt += 1
             else:
-                seen.append(s[i])
-        return "".join(seen)
+                cnt = 1
+            prev = c
+            if cnt < 3:
+                answer += c
+        return answer
+
+# class Solution:
+#     def makeFancyString(self, s: str) -> str:
+#         seen = []
+#         for i in range(len(s)):
+#             if (len(seen) >= 2) and (s[i] == seen[-1]) and (s[i] == seen[-2]):
+#                 continue
+#             else:
+#                 seen.append(s[i])
+#         return "".join(seen)
