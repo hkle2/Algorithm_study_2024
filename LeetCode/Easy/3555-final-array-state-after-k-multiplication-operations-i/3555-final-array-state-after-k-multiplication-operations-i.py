d41dc5de6c313d487b034bd82aa59bb74fbe8f9b
hkle2 <minnie110598@gmail.com>
2024-08-27 10:09:56 +0900

    Time: 60 ms (44.56%), Space: 16.5 MB (54.53%) - LeetHub

--- LeetCode/Easy/3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.py
+++ LeetCode/Easy/3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.py
@@ -1,6 +1,14 @@
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
         for i in range(k):
+            min_value = min(nums)
+            min_index = nums.index(min_value)
+            nums[min_index] *= multiplier
+        return nums
+        
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        for i in range(k):
             min_index = nums.index(min(nums))
             nums[min_index] *= multiplier
         return nums
