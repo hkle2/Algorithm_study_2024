48d2ae598965e56f1eebfc7f6d5784282997f63a
hkle2 <minnie110598@gmail.com>
2024-08-28 16:32:06 +0900

    Time: 45 ms (50.31%), Space: 16.7 MB (29.01%) - LeetHub

--- LeetCode/Easy/2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.py
+++ LeetCode/Easy/2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.py
@@ -1,31 +1,31 @@
-# class Solution:
-#     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-#         answer = 0
-#         for i in range(len(tickets)):
-#             if i <= k:
-#                 if tickets[i] < tickets[k]:
-#                     answer += tickets[i]
-#                 else:
-#                     answer += tickets[k]
-#             else:
-#                 if tickets[i] < tickets[k]:
-#                     answer += tickets[i]
-#                 else:
-#                     answer += (tickets[k] - 1)
-#         return answer
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        answer = 0
+        for i in range(len(tickets)):
+            if i <= k:
+                if tickets[i] < tickets[k]:
+                    answer += tickets[i]
+                else:
+                    answer += tickets[k]
+            else:
+                if tickets[i] < tickets[k]:
+                    answer += tickets[i]
+                else:
+                    answer += (tickets[k] - 1)
+        return answer
 
-# class Solution:
-#     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-#         time = 0
-#         for i in range(len(tickets)):
-#             if tickets[i] >= tickets[k]:
-#                 if i <= k:
-#                     time += tickets[k]
-#                 else:
-#                     time += tickets[k] - 1
-#             else:
-#                 time += tickets[i]
-#         return time
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        time = 0
+        for i in range(len(tickets)):
+            if tickets[i] >= tickets[k]:
+                if i <= k:
+                    time += tickets[k]
+                else:
+                    time += tickets[k] - 1
+            else:
+                time += tickets[i]
+        return time
 
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
