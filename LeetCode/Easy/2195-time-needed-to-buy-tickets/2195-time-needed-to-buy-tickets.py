d9f352e8a5f8e0bee5de2140390de54e8b626545
hkle2 <minnie110598@gmail.com>
2024-08-28 16:31:54 +0900

    Time: 52 ms (21.38%), Space: 16.6 MB (29.01%) - LeetHub

--- LeetCode/Easy/2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.py
+++ LeetCode/Easy/2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.py
@@ -1,18 +1,18 @@
-class Solution:
-    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        answer = 0
-        for i in range(len(tickets)):
-            if i <= k:
-                if tickets[i] < tickets[k]:
-                    answer += tickets[i]
-                else:
-                    answer += tickets[k]
-            else:
-                if tickets[i] < tickets[k]:
-                    answer += tickets[i]
-                else:
-                    answer += (tickets[k] - 1)
-        return answer
+# class Solution:
+#     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+#         answer = 0
+#         for i in range(len(tickets)):
+#             if i <= k:
+#                 if tickets[i] < tickets[k]:
+#                     answer += tickets[i]
+#                 else:
+#                     answer += tickets[k]
+#             else:
+#                 if tickets[i] < tickets[k]:
+#                     answer += tickets[i]
+#                 else:
+#                     answer += (tickets[k] - 1)
+#         return answer
 
 # class Solution:
 #     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
@@ -27,15 +27,15 @@ class Solution:
 #                 time += tickets[i]
 #         return time
 
-# class Solution:
-#     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-#         n = len(tickets)
-#         i = 0
-#         time = 0
-#         while tickets[k] != 0:
-#             i %= n
-#             if tickets[i] != 0:
-#                 tickets[i] -= 1
-#                 time += 1
-#             i += 1
-#         return time
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        n = len(tickets)
+        i = 0
+        time = 0
+        while tickets[k] != 0:
+            i %= n
+            if tickets[i] != 0:
+                tickets[i] -= 1
+                time += 1
+            i += 1
+        return time
