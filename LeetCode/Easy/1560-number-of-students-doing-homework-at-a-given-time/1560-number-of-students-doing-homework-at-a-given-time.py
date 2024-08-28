05db6bdaaa8822a95c33ad0556c48e901eb667e9
hkle2 <minnie110598@gmail.com>
2024-08-28 09:02:24 +0900

    Time: 30 ms (97.53%), Space: 16.6 MB (5.7%) - LeetHub

--- LeetCode/Easy/1560-number-of-students-doing-homework-at-a-given-time/1560-number-of-students-doing-homework-at-a-given-time.py
+++ LeetCode/Easy/1560-number-of-students-doing-homework-at-a-given-time/1560-number-of-students-doing-homework-at-a-given-time.py
@@ -1,7 +1,7 @@
 class Solution:
     def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
         answer = 0
-        for i, j in zip(startTime, endTime):
-            if i <= queryTime and j >= queryTime:
+        for s, e in zip(startTime, endTime):
+            if s <= queryTime and e >= queryTime:
                 answer += 1
         return answer
