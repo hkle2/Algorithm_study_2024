1faf81f00623dcc1d5270314dfdc9e913cbc6180
hkle2 <minnie110598@gmail.com>
2024-08-28 16:31:36 +0900

    Time: 42 ms (62.33%), Space: 16.5 MB (29.01%) - LeetHub

--- LeetCode/Easy/2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.py
+++ LeetCode/Easy/2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.py
@@ -14,28 +14,28 @@ class Solution:
                     answer += (tickets[k] - 1)
         return answer
 
-class Solution:
-    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        time = 0
-        for i in range(len(tickets)):
-            if tickets[i] >= tickets[k]:
-                if i <= k:
-                    time += tickets[k]
-                else:
-                    time += tickets[k] - 1
-            else:
-                time += tickets[i]
-        return time
+# class Solution:
+#     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+#         time = 0
+#         for i in range(len(tickets)):
+#             if tickets[i] >= tickets[k]:
+#                 if i <= k:
+#                     time += tickets[k]
+#                 else:
+#                     time += tickets[k] - 1
+#             else:
+#                 time += tickets[i]
+#         return time
 
-class Solution:
-    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        n = len(tickets)
-        i = 0
-        time = 0
-        while tickets[k] != 0:
-            i %= n
-            if tickets[i] != 0:
-                tickets[i] -= 1
-                time += 1
-            i += 1
-        return time
+# class Solution:
+#     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+#         n = len(tickets)
+#         i = 0
+#         time = 0
+#         while tickets[k] != 0:
+#             i %= n
+#             if tickets[i] != 0:
+#                 tickets[i] -= 1
+#                 time += 1
+#             i += 1
+#         return time
