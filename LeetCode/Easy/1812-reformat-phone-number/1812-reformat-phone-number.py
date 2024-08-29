54f91da01aae9a1ecb05b401d2203ff3340fc8b2
hkle2 <minnie110598@gmail.com>
2024-08-29 10:03:31 +0900

    Time: 36 ms (50.15%), Space: 16.6 MB (34.77%) - LeetHub

--- LeetCode/Easy/1812-reformat-phone-number/1812-reformat-phone-number.py
+++ LeetCode/Easy/1812-reformat-phone-number/1812-reformat-phone-number.py
@@ -1,3 +1,22 @@
+class Solution:
+    def reformatNumber(self, number: str) -> str:
+        # replace, slice
+        answer = []
+        number = number.replace(" ", "")
+        number = number.replace("-", "")
+        while len(number) > 4:
+            answer.append(number[:3])
+            number = number[3:]
+        # 2 digits: A single block of length 2.
+        # 3 digits: A single block of length 3.
+        # 4 digits: Two blocks of length 2 each.
+        if len(number) == 4:
+            answer.append(number[:2])
+            answer.append(number[2:])
+        else:
+            answer.append(number)
+        return "-".join(answer)
+
 # class Solution:
 #     def reformatNumber(self, number: str) -> str:
 #         l = []
@@ -9,21 +28,21 @@
 #             num[i:i+3]
 #         return ""
 
-class Solution:
-    def reformatNumber(self, number: str) -> str:
-        answer = []
-        number = number.replace(" ", "")
-        number = number.replace("-", "")
-        if len(number) % 3 == 0:
-            for i in range(0, len(number), 3):
-                answer.append(number[i:i+3])
-        elif len(number) % 3 == 1:
-            for i in range(0, len(number)-4, 3):
-                answer.append(number[i:i+3])
-            answer.append(number[-4:-2])
-            answer.append(number[-2:])
-        else:
-            for i in range(0, len(number)-2, 3):
-                answer.append(number[i:i+3])
-            answer.append(number[-2:])
-        return "-".join(answer)
+# class Solution:
+#     def reformatNumber(self, number: str) -> str:
+#         answer = []
+#         number = number.replace(" ", "")
+#         number = number.replace("-", "")
+#         if len(number) % 3 == 0:
+#             for i in range(0, len(number), 3):
+#                 answer.append(number[i:i+3])
+#         elif len(number) % 3 == 1:
+#             for i in range(0, len(number)-4, 3):
+#                 answer.append(number[i:i+3])
+#             answer.append(number[-4:-2])
+#             answer.append(number[-2:])
+#         else:
+#             for i in range(0, len(number)-2, 3):
+#                 answer.append(number[i:i+3])
+#             answer.append(number[-2:])
+#         return "-".join(answer)
