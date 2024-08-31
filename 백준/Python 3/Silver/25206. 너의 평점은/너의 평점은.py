14b694e9a1b360807caa9cc427e0c957c78d05d4
hkle2 <minnie110598@gmail.com>
2024-08-31 15:46:21 +0900

    [Silver V] Title: 너의 평점은, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Python 3/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Python 3/Silver/25206. 너의 평점은/너의 평점은.py
@@ -4,7 +4,7 @@ score_dict = {"A+": 4.5, "A0": 4.0, "B+": 3.5, "B0": 3.0, "C+": 2.5, "C0": 2.0, 
 total_credit = 0
 total_score = 0
 
-for i in range(20):
+for _ in range(20):
     lecture, credit, grade = sys.stdin.readline().split()
     if grade != "P":
         total_credit += float(credit)
