97625e7113b06e9dc8fa1435267454a13741539b
hkle2 <minnie110598@gmail.com>
2024-08-31 15:33:36 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Python 3/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Python 3/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -3,7 +3,7 @@ import sys
 N, M = map(int, sys.stdin.readline().split())
 baskets = [n for n in range(1, N+1)]
 
-for i in range(M):
+for _ in range(M):
     i, j = map(int, sys.stdin.readline().split())
     baskets[i-1:j] = baskets[i-1:j][::-1]
 
