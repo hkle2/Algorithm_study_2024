9728f6c0af0235434e498367d051a57631947242
hkle2 <minnie110598@gmail.com>
2024-08-31 15:51:37 +0900

    [Bronze I] Title: 세로읽기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Python 3/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Python 3/Bronze/10798. 세로읽기/세로읽기.py
@@ -4,11 +4,10 @@ boards = []
 length = []
 answer = ""
 
-for n in range(5):
+for _ in range(5):
     s = sys.stdin.readline().strip()
     boards.append(s)
     length.append(len(s))
-
 
 for i in range(max(length)):
     for j in range(len(boards)): 
