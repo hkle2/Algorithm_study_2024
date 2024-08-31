f0da37838701947e17cafeacdec0c80d72120ba7
hkle2 <minnie110598@gmail.com>
2024-08-31 16:52:20 +0900

    Time: 37 ms (47.85%), Space: 16.4 MB (63.75%) - LeetHub

--- LeetCode/Easy/1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.py
+++ LeetCode/Easy/1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.py
@@ -1,8 +1,7 @@
 # class Solution:
 #     def getNoZeroIntegers(self, n: int) -> List[int]:
 #         # for
-#         # 숫자를 문자열로 바꿔주기 / %를 사용해도 됨!
-#         answer = []
+#         # 숫자를 문자열로 바꿔주기
 #         for i in range(1, n):
 #             a = i
 #             b = n - a
@@ -10,18 +9,9 @@
 #             # [a, b]를 answer로 지정하고 break
 #             print(a, b)
 #         return answer
-        
-# class Solution:
-#     def getNoZeroIntegers(self, n: int) -> List[int]:
-#         for i in range(1, n):
-#             a = i
-#             b = n - a
-#         return answer
 
 class Solution:
     def getNoZeroIntegers(self, n: int) -> List[int]:
         for i in range(1, n):
             if "0" not in str(i) and "0" not in str(n-i):
-                answer = [i, n-i]
-                break
-        return answer
+                return [i, n-i]
