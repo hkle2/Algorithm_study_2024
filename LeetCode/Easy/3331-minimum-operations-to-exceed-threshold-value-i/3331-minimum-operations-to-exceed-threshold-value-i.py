060e9cf3bf20223792e0a5f5cbdbf4de48830cab
hkle2 <minnie110598@gmail.com>
2024-08-31 17:05:55 +0900

    Time: 49 ms (40.38%), Space: 16.5 MB (28.84%) - LeetHub

--- LeetCode/Easy/3331-minimum-operations-to-exceed-threshold-value-i/3331-minimum-operations-to-exceed-threshold-value-i.py
+++ LeetCode/Easy/3331-minimum-operations-to-exceed-threshold-value-i/3331-minimum-operations-to-exceed-threshold-value-i.py
@@ -5,9 +5,7 @@ class Solution:
         nums = sorted(nums)
         for i in range(len(nums)):
             if nums[i] >= k:
-                answer = i
-                break
-        return answer
+                return i
 
 # class Solution:
 #     def minOperations(self, nums: List[int], k: int) -> int:
