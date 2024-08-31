a5955db06430d05f539f97cb2d2b4560bea75010
hkle2 <minnie110598@gmail.com>
2024-08-31 15:31:10 +0900

    [Bronze II] Title: 공 바꾸기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Python 3/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Python 3/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -3,7 +3,7 @@ import sys
 N, M = map(int, sys.stdin.readline().split())
 basket = [n for n in range(1, N+1)]
 
-for m in range(M):
+for _ in range(M):
     i, j = map(int, sys.stdin.readline().split())
     basket[i-1], basket[j-1] = basket[j-1], basket[i-1]
 
