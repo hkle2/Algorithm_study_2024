1a7aa00c8192269b7690a9265bc2cf11f1e256a7
hkle2 <minnie110598@gmail.com>
2024-09-02 11:33:16 +0900

    Time: 108 ms (62.9%), Space: 17.3 MB (86.83%) - LeetHub

--- LeetCode/Easy/2447-merge-similar-items/2447-merge-similar-items.py
+++ LeetCode/Easy/2447-merge-similar-items/2447-merge-similar-items.py
@@ -1,13 +1,13 @@
-class Solution:
-    def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
-        count_dict = defaultdict(int)
-        for value, weight in items1 + items2:
-            count_dict[value] += weight
-        sorted_keys = sorted(count_dict.keys())
-        answer = []
-        for key in sorted_keys:
-            answer.append([key, count_dict[key]])
-        return answer
+# class Solution:
+#     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
+#         count_dict = defaultdict(int)
+#         for value, weight in items1 + items2:
+#             count_dict[value] += weight
+#         sorted_keys = sorted(count_dict.keys())
+#         answer = []
+#         for key in sorted_keys:
+#             answer.append([key, count_dict[key]])
+#         return answer
 
 from collections import defaultdict
 
@@ -15,18 +15,17 @@ class Solution:
     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
         items = items1 + items2
         items_dict = defaultdict(int)
-        answer = []
         for value, weight in items:
             items_dict[value] += weight
         return sorted(items_dict.items())
 
-class Solution:
-    def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
-        items = items1 + items2
-        items_dict = defaultdict(int)
-        answer = []
-        for value, weight in items:
-            items_dict[value] += weight
-        for key, value in items_dict.items():
-            answer.append([key, value])
-        return sorted(answer)
+# class Solution:
+#     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
+#         items = items1 + items2
+#         items_dict = defaultdict(int)
+#         answer = []
+#         for value, weight in items:
+#             items_dict[value] += weight
+#         for key, value in items_dict.items():
+#             answer.append([key, value])
+#         return sorted(answer)
