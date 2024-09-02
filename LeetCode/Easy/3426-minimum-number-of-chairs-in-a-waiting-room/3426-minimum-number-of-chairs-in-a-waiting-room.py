743d8959180b5fe00c869df091c241c8363e49db
hkle2 <minnie110598@gmail.com>
2024-09-02 10:02:08 +0900

    Time: 41 ms (29.33%), Space: 16.5 MB (20.83%) - LeetHub

--- LeetCode/Easy/3426-minimum-number-of-chairs-in-a-waiting-room/3426-minimum-number-of-chairs-in-a-waiting-room.py
+++ LeetCode/Easy/3426-minimum-number-of-chairs-in-a-waiting-room/3426-minimum-number-of-chairs-in-a-waiting-room.py
@@ -1,12 +1,25 @@
 class Solution:
     def minimumChairs(self, s: str) -> int:
-        chair = 0
         answer = 0
-        for i in range(len(s)):
-            if s[i] == "E":
-                chair += 1
-                if answer < chair:
-                    answer = chair
+        cnt = 0
+        for c in s:
+            if c == "E":
+                cnt += 1
             else:
-                chair -= 1
+                cnt -= 1
+            # 방안에 머무는 사람의 최대 수를 answer로 지정
+            answer = max(cnt, answer)
         return answer
+
+# class Solution:
+#     def minimumChairs(self, s: str) -> int:
+#         chair = 0
+#         answer = 0
+#         for i in range(len(s)):
+#             if s[i] == "E":
+#                 chair += 1
+#                 if answer < chair:
+#                     answer = chair
+#             else:
+#                 chair -= 1
+#         return answer
