a67e5008437bb13f9583d51ead327448567a7eca
hkle2 <minnie110598@gmail.com>
2024-09-02 09:40:53 +0900

    Time: 23 ms (100%), Space: 16.4 MB (100%) - LeetHub

--- LeetCode/Easy/3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.py
+++ LeetCode/Easy/3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.py
@@ -1,19 +1,19 @@
-# class Solution:
-#     def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
-#         x1, y1 = coordinate1
-#         x2, y2 = coordinate2
-#         if (ord(x1) + int(y1)) % 2 == (ord(x2) + int(y2)):
-#             return True
-#         return False
-
 class Solution:
     def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
-        col1 = "aceg"
-        col2 = "bdfh"
-        if int(coordinate1[1]) % 2 == int(coordinate2[1]) % 2:
-            if (coordinate1[0] in col1 and coordinate2[0] in col1) or (coordinate1[0] in col2 and coordinate2[0] in col2):
-                return True
-        else:
-            if (coordinate1[0] in col1 and coordinate2[0] in col2) or (coordinate1[0] in col2 and coordinate2[0] in col1):
-                return True
+        x1, y1 = coordinate1
+        x2, y2 = coordinate2
+        if (ord(x1) + int(y1)) % 2 == (ord(x2) + int(y2)) % 2:
+            return True
         return False
+
+# class Solution:
+#     def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
+#         col1 = "aceg"
+#         col2 = "bdfh"
+#         if int(coordinate1[1]) % 2 == int(coordinate2[1]) % 2:
+#             if (coordinate1[0] in col1 and coordinate2[0] in col1) or (coordinate1[0] in col2 and coordinate2[0] in col2):
+#                 return True
+#         else:
+#             if (coordinate1[0] in col1 and coordinate2[0] in col2) or (coordinate1[0] in col2 and coordinate2[0] in col1):
+#                 return True
+#         return False
