96df95fb8846d126b12d46bbd8cd5d2e389b6d45
hkle2 <minnie110598@gmail.com>
2024-09-02 10:04:33 +0900

    Time: 35 ms (100%), Space: 16.5 MB (100%) - LeetHub

--- LeetCode/Easy/3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.py
+++ LeetCode/Easy/3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.py
@@ -6,14 +6,14 @@ class Solution:
             return True
         return False
 
-# class Solution:
-#     def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
-#         col1 = "aceg"
-#         col2 = "bdfh"
-#         if int(coordinate1[1]) % 2 == int(coordinate2[1]) % 2:
-#             if (coordinate1[0] in col1 and coordinate2[0] in col1) or (coordinate1[0] in col2 and coordinate2[0] in col2):
-#                 return True
-#         else:
-#             if (coordinate1[0] in col1 and coordinate2[0] in col2) or (coordinate1[0] in col2 and coordinate2[0] in col1):
-#                 return True
-#         return False
+class Solution:
+    def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
+        col1 = "aceg"
+        col2 = "bdfh"
+        if int(coordinate1[1]) % 2 == int(coordinate2[1]) % 2:
+            if (coordinate1[0] in col1 and coordinate2[0] in col1) or (coordinate1[0] in col2 and coordinate2[0] in col2):
+                return True
+        else:
+            if (coordinate1[0] in col1 and coordinate2[0] in col2) or (coordinate1[0] in col2 and coordinate2[0] in col1):
+                return True
+        return False
