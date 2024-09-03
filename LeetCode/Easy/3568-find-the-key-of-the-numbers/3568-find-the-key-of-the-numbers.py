fe106b7479c30ba5ae700138d937ccb9455b5cc9
hkle2 <minnie110598@gmail.com>
2024-09-03 10:05:21 +0900

    Time: 36 ms (61.97%), Space: 16.4 MB (83.08%) - LeetHub

--- LeetCode/Easy/3568-find-the-key-of-the-numbers/3568-find-the-key-of-the-numbers.py
+++ LeetCode/Easy/3568-find-the-key-of-the-numbers/3568-find-the-key-of-the-numbers.py
@@ -1,6 +1,5 @@
 class Solution:
     def generateKey(self, num1: int, num2: int, num3: int) -> int:
-        # zip
         str_1 = str(num1).zfill(4)
         str_2 = str(num2).zfill(4)
         str_3 = str(num3).zfill(4)
@@ -9,12 +8,12 @@ class Solution:
             answer += min(chr_1, chr_2, chr_3)
         return int(answer)
         
-# class Solution:
-#     def generateKey(self, num1: int, num2: int, num3: int) -> int:
-#         str1 = str(num1).zfill(4)
-#         str2 = str(num2).zfill(4)
-#         str3 = str(num3).zfill(4)
-#         answer = ""
-#         for i in range(4):
-#             answer += min(str1[i], str2[i], str3[i])
-#         return int(answer)
+class Solution:
+    def generateKey(self, num1: int, num2: int, num3: int) -> int:
+        str1 = str(num1).zfill(4)
+        str2 = str(num2).zfill(4)
+        str3 = str(num3).zfill(4)
+        answer = ""
+        for i in range(4):
+            answer += min(str1[i], str2[i], str3[i])
+        return int(answer)
