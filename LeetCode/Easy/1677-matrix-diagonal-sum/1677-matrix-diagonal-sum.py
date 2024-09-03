9f60f8214238e6c9907ab9faf3245371317a91a7
hkle2 <minnie110598@gmail.com>
2024-09-03 10:09:33 +0900

    Time: 82 ms (98.65%), Space: 16.9 MB (43.32%) - LeetHub

--- LeetCode/Easy/1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.py
+++ LeetCode/Easy/1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.py
@@ -5,17 +5,16 @@ class Solution:
         for i in range(n):
             answer += mat[i][i]
             answer += mat[i][n-1-i]
-        # 한 변의 길이가 홀수일 때는 겹치는 중간값 한번 빼주기
         if n % 2 == 1:
             answer -= mat[n//2][n//2]
         return answer
 
-# class Solution:
-#     def diagonalSum(self, mat: List[List[int]]) -> int:
-#         n = len(mat)
-#         answer = 0
-#         for i in range(n):
-#             answer += (mat[i][i] + mat[i][-i-1])
-#         if n % 2 != 0:
-#             answer -= mat[n//2][n//2]
-#         return answer
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        n = len(mat)
+        answer = 0
+        for i in range(n):
+            answer += (mat[i][i] + mat[i][-i-1])
+        if n % 2 != 0:
+            answer -= mat[n//2][n//2]
+        return answer
