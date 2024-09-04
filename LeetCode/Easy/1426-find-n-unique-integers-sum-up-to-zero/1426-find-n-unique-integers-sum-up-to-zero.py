02d18baa77b264182df602f20f81242d4025f319
hkle2 <minnie110598@gmail.com>
2024-09-04 09:28:08 +0900

    Time: 34 ms (74.27%), Space: 16.6 MB (85.65%) - LeetHub

--- LeetCode/Easy/1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.py
+++ LeetCode/Easy/1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.py
@@ -1,9 +1,13 @@
 class Solution:
     def sumZero(self, n: int) -> List[int]:
-        answer = []
-        for i in range(1, n//2+1):
-            answer.append(i)
-            answer.append(-i)
-        if n % 2 != 0:
-            answer.append(0)
-        return answer
+        return [i for i in range(-n+1, n, 2)]
+
+# class Solution:
+#     def sumZero(self, n: int) -> List[int]:
+#         answer = []
+#         for i in range(1, n//2+1):
+#             answer.append(i)
+#             answer.append(-i)
+#         if n % 2 != 0:
+#             answer.append(0)
+#         return answer
