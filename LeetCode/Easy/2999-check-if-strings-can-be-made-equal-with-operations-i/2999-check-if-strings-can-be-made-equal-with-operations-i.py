884b715c7c7e5d95bb2d530f360aa40c155fbd3e
hkle2 <minnie110598@gmail.com>
2024-09-04 09:36:12 +0900

    Time: 40 ms (38.93%), Space: 16.4 MB (95.36%) - LeetHub

--- LeetCode/Easy/2999-check-if-strings-can-be-made-equal-with-operations-i/2999-check-if-strings-can-be-made-equal-with-operations-i.py
+++ LeetCode/Easy/2999-check-if-strings-can-be-made-equal-with-operations-i/2999-check-if-strings-can-be-made-equal-with-operations-i.py
@@ -1,8 +1,6 @@
 class Solution:
     def canBeEqual(self, s1: str, s2: str) -> bool:
-        if sorted(s1[::2]) == sorted(s2[::2]) and sorted(s1[1::2]) == sorted(s2[1::2]):
-            return True
-        return False
+        return sorted(s1[::2]) == sorted(s2[::2]) and sorted(s1[1::2]) == sorted(s2[1::2])
 
 # class Solution:
 #     def canBeEqual(self, s1: str, s2: str) -> bool:
