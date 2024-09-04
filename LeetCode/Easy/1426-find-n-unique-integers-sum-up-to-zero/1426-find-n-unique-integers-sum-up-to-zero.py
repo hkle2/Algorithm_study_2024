9c592751a026e84a678fe28e0aa5c4d85f894235
hkle2 <minnie110598@gmail.com>
2024-09-04 10:07:51 +0900

    Time: 39 ms (41.13%), Space: 16.8 MB (10.65%) - LeetHub

--- LeetCode/Easy/1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.py
+++ LeetCode/Easy/1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.py
@@ -1,9 +1,6 @@
 class Solution:
     def sumZero(self, n: int) -> List[int]:
         answer = []
-        # 반복하면서 양수하나, 음수 하나 넣어주면 됨
-        # 1, -1, 2, -2
-        # 만약에 n이 홀수라면 0을 하나 넣어주면 됨
         if n % 2 == 1:
             answer.append(0)
         for i in range(n//2):
@@ -11,16 +8,16 @@ class Solution:
             answer.append(-(i+1))
         return answer
 
-# class Solution:
-#     def sumZero(self, n: int) -> List[int]:
-#         return [i for i in range(-n+1, n, 2)]
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        return [i for i in range(-n+1, n, 2)]
 
-# class Solution:
-#     def sumZero(self, n: int) -> List[int]:
-#         answer = []
-#         for i in range(1, n//2+1):
-#             answer.append(i)
-#             answer.append(-i)
-#         if n % 2 != 0:
-#             answer.append(0)
-#         return answer
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        answer = []
+        for i in range(1, n//2+1):
+            answer.append(i)
+            answer.append(-i)
+        if n % 2 != 0:
+            answer.append(0)
+        return answer
