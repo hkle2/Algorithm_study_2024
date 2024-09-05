452ea399ddf0ecab66a9434e8cdf69257db478c4
hkle2 <minnie110598@gmail.com>
2024-09-05 13:07:11 +0900

    Time: 214 ms (5.16%), Space: 28.7 MB (9.79%) - LeetHub

--- LeetCode/Easy/1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.py
+++ LeetCode/Easy/1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.py
@@ -1,17 +1,17 @@
-# import numpy as np
+import numpy as np
 
-# class Solution:
-#     def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
-#         answer = 0
-#         mat = np.zeros((m, n))
-#         for r, c in indices:
-#             mat[r] += 1
-#             mat[:, c] += 1
-#         for i in range(m):
-#             for j in range(n):
-#                 if mat[i][j] % 2 != 0:
-#                     answer += 1
-#         return answer
+class Solution:
+    def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
+        answer = 0
+        mat = np.zeros((m, n))
+        for r, c in indices:
+            mat[r] += 1
+            mat[:, c] += 1
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] % 2 != 0:
+                    answer += 1
+        return answer
 
 class Solution:
     def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
@@ -27,17 +27,17 @@ class Solution:
                     answer += 1
         return answer
 
-# class Solution:
-#     def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
-#         answer = 0
-#         mat = [[0 for _ in range(n)] for _ in range(m)]
-#         for r, c in indices:
-#             for i in range(n):
-#                 mat[r][i] += 1
-#             for i in range(m):
-#                 mat[i][c] += 1
-#         for i in range(m):
-#             for j in range(n):
-#                 if mat[i][j] % 2 != 0:
-#                     answer += 1
-#         return answer
+class Solution:
+    def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
+        answer = 0
+        mat = [[0 for _ in range(n)] for _ in range(m)]
+        for r, c in indices:
+            for i in range(n):
+                mat[r][i] += 1
+            for i in range(m):
+                mat[i][c] += 1
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] % 2 != 0:
+                    answer += 1
+        return answer
