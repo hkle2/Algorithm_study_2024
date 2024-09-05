bff51b0d202644dddb7dc593234d2f909ff65192
hkle2 <minnie110598@gmail.com>
2024-09-05 11:25:19 +0900

    Time: 48 ms (43.3%), Space: 16.6 MB (79.21%) - LeetHub

--- LeetCode/Easy/1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.py
+++ LeetCode/Easy/1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.py
@@ -13,31 +13,31 @@
 #                     answer += 1
 #         return answer
 
-# class Solution:
-#     def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
-#         answer = 0
-#         mat = [[0 for _ in range(n)] for _ in range(m)]
-#         for r, c in indices:
-#             mat[r] = [x + 1 for x in mat[r]]
-#             for col in mat:
-#                 col[c] += 1
-#         for i in range(m):
-#             for j in range(n):
-#                 if mat[i][j] % 2 != 0:
-#                     answer += 1
-#         return answer
-
 class Solution:
     def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
         answer = 0
         mat = [[0 for _ in range(n)] for _ in range(m)]
         for r, c in indices:
-            for i in range(n):
-                mat[r][i] += 1
-            for i in range(m):
-                mat[i][c] += 1
+            mat[r] = [x + 1 for x in mat[r]]
+            for col in mat:
+                col[c] += 1
         for i in range(m):
             for j in range(n):
                 if mat[i][j] % 2 != 0:
                     answer += 1
         return answer
+
+# class Solution:
+#     def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
+#         answer = 0
+#         mat = [[0 for _ in range(n)] for _ in range(m)]
+#         for r, c in indices:
+#             for i in range(n):
+#                 mat[r][i] += 1
+#             for i in range(m):
+#                 mat[i][c] += 1
+#         for i in range(m):
+#             for j in range(n):
+#                 if mat[i][j] % 2 != 0:
+#                     answer += 1
+#         return answer
