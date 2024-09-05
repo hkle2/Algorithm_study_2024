1fe61a436138b296df8b5aae40d60ea4670920df
hkle2 <minnie110598@gmail.com>
2024-09-05 10:01:50 +0900

    Time: 110 ms (15.17%), Space: 17.5 MB (99.3%) - LeetHub

--- LeetCode/Easy/1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.py
+++ LeetCode/Easy/1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.py
@@ -2,11 +2,11 @@ class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
         return sum([1 for row in grid for num in row if num < 0])
 
-# class Solution:
-#     def countNegatives(self, grid: List[List[int]]) -> int:
-#         answer = 0
-#         for i in range(len(grid)):
-#             for j in range(len(grid[0])):
-#                 if grid[i][j] < 0:
-#                     answer += 1
-#         return answer
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        answer = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] < 0:
+                    answer += 1
+        return answer
