1f33d4b5e0058788e57861ec007894922d6b9ea4
hkle2 <minnie110598@gmail.com>
2024-09-06 10:17:58 +0900

    Time: 72 ms (31.64%), Space: 16.8 MB (25.42%) - LeetHub

--- LeetCode/Easy/2654-count-the-number-of-vowel-strings-in-range/2654-count-the-number-of-vowel-strings-in-range.py
+++ LeetCode/Easy/2654-count-the-number-of-vowel-strings-in-range/2654-count-the-number-of-vowel-strings-in-range.py
@@ -1,18 +1,18 @@
-class Solution:
-    def vowelStrings(self, words: List[str], left: int, right: int) -> int:
-        vowels = ["a", 'e', 'i', 'o', 'u']
-        answer = 0
-        for i in range(left, right + 1):
-            word = words[i]
-            if word[0] in vowels and word[-1] in vowels:
-                answer += 1
-        return answer
-
 # class Solution:
 #     def vowelStrings(self, words: List[str], left: int, right: int) -> int:
+#         vowels = ["a", 'e', 'i', 'o', 'u']
 #         answer = 0
-#         vowels = "aeiou"
-#         for i in range(left, right+1):
-#             if words[i][0] in vowels and words[i][-1] in vowels:
+#         for i in range(left, right + 1):
+#             word = words[i]
+#             if word[0] in vowels and word[-1] in vowels:
 #                 answer += 1
 #         return answer
+
+class Solution:
+    def vowelStrings(self, words: List[str], left: int, right: int) -> int:
+        answer = 0
+        vowels = "aeiou"
+        for i in range(left, right+1):
+            if words[i][0] in vowels and words[i][-1] in vowels:
+                answer += 1
+        return answer
