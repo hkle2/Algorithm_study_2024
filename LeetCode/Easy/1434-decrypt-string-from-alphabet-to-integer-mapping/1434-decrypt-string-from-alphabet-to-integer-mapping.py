27f4b05fb42ae9fc08b3e128393b0c64152c9822
hkle2 <minnie110598@gmail.com>
2024-09-06 09:20:20 +0900

    Time: 37 ms (44.97%), Space: 16.3 MB (95.71%) - LeetHub

---
+++ LeetCode/Easy/1434-decrypt-string-from-alphabet-to-integer-mapping/1434-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -0,0 +1,12 @@
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        answer = ""
+        i = 0
+        while i < len(s):
+            if i + 2 < len(s) and s[i+2] == "#":
+                answer += chr(int(s[i:i+2])+96)
+                i += 3
+            else:
+                answer += chr(int(s[i])+96)
+                i += 1
+        return answer
