da088a990ae1debb9823c11b3b067ab897a86816
hkle2 <minnie110598@gmail.com>
2024-09-06 10:09:14 +0900

    Time: 27 ms (92.88%), Space: 16.5 MB (30.73%) - LeetHub

--- LeetCode/Easy/1434-decrypt-string-from-alphabet-to-integer-mapping/1434-decrypt-string-from-alphabet-to-integer-mapping.py
+++ LeetCode/Easy/1434-decrypt-string-from-alphabet-to-integer-mapping/1434-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -1,6 +1,5 @@
 class Solution:
     def freqAlphabets(self, s: str) -> str:
-        # 각 알파벳과 숫자 표기를 매핑한 딕셔너리 만들기
         d = {}
         for i in range(26):
             alphabet = chr(i + 97)
@@ -10,8 +9,6 @@ class Solution:
             else:
                 d[str(num) + "#"] = alphabet
         sorted_keys = sorted(d.keys(), reverse=True, key=lambda x: (len(x), x))
-        # 반복문 사용해서 앞에서부터 문자열 매칭해서
-        # 일치하는 숫자부터 문자열에서 제거하기
         answer = ""
         while s:
             for key in sorted_keys:
@@ -21,15 +18,15 @@ class Solution:
                     break
         return answer
 
-# class Solution:
-#     def freqAlphabets(self, s: str) -> str:
-#         answer = ""
-#         i = 0
-#         while i < len(s):
-#             if i + 2 < len(s) and s[i+2] == "#":
-#                 answer += chr(int(s[i:i+2]) + ord("a") - 1)
-#                 i += 3
-#             else:
-#                 answer += chr(int(s[i]) + ord("a") - 1)
-#                 i += 1
-#         return answer
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        answer = ""
+        i = 0
+        while i < len(s):
+            if i + 2 < len(s) and s[i+2] == "#":
+                answer += chr(int(s[i:i+2]) + ord("a") - 1)
+                i += 3
+            else:
+                answer += chr(int(s[i]) + ord("a") - 1)
+                i += 1
+        return answer
