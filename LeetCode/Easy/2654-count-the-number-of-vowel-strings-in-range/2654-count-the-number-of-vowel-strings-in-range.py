cfa8bc6672150f915e8eabe8c07d49ac0bb0f26a
hkle2 <minnie110598@gmail.com>
2024-09-06 10:03:45 +0900

    Time: 73 ms (26.66%), Space: 16.7 MB (76.45%) - LeetHub

--- LeetCode/Easy/2654-count-the-number-of-vowel-strings-in-range/2654-count-the-number-of-vowel-strings-in-range.py
+++ LeetCode/Easy/2654-count-the-number-of-vowel-strings-in-range/2654-count-the-number-of-vowel-strings-in-range.py
@@ -1,21 +1,18 @@
-class Solution:
-    def vowelStrings(self, words: List[str], left: int, right: int) -> int:
-        # for, in
-        vowels = ["a", 'e', 'i', 'o', 'u']
-        answer = 0
-        for i in range(left, right + 1):
-            word = words[i]
-            # word의 각 글자를 반복하면서 모음인지 체크하고
-            # 모음이면 answer에 1 더해주기
-            if word[0] in vowels and word[-1] in vowels:
-                answer += 1
-        return answer
-
 # class Solution:
 #     def vowelStrings(self, words: List[str], left: int, right: int) -> int:
+#         vowels = ["a", 'e', 'i', 'o', 'u']
 #         answer = 0
-#         vowels = "aeiou"
-#         for i in range(left, right+1):
-#             if words[i][0] in vowels and words[i][-1] in vowels:
+#         for i in range(left, right + 1):
+#             word = words[i]
+#             if word[0] in vowels and word[-1] in vowels:
 #                 answer += 1
 #         return answer
+
+class Solution:
+    def vowelStrings(self, words: List[str], left: int, right: int) -> int:
+        answer = 0
+        vowels = "aeiou"
+        for i in range(left, right+1):
+            if words[i][0] in vowels and words[i][-1] in vowels:
+                answer += 1
+        return answer
