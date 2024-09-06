e4b0ae813618d19186e864cada2949d8486427f3
hkle2 <minnie110598@gmail.com>
2024-09-06 10:03:04 +0900

    Time: 67 ms (66.29%), Space: 16.9 MB (25.42%) - LeetHub

--- LeetCode/Easy/2654-count-the-number-of-vowel-strings-in-range/2654-count-the-number-of-vowel-strings-in-range.py
+++ LeetCode/Easy/2654-count-the-number-of-vowel-strings-in-range/2654-count-the-number-of-vowel-strings-in-range.py
@@ -1,8 +1,21 @@
 class Solution:
     def vowelStrings(self, words: List[str], left: int, right: int) -> int:
+        # for, in
+        vowels = ["a", 'e', 'i', 'o', 'u']
         answer = 0
-        vowel = "aeiou"
-        for i in range(left, right+1):
-            if words[i][0] in vowel and words[i][-1] in vowel:
+        for i in range(left, right + 1):
+            word = words[i]
+            # word의 각 글자를 반복하면서 모음인지 체크하고
+            # 모음이면 answer에 1 더해주기
+            if word[0] in vowels and word[-1] in vowels:
                 answer += 1
         return answer
+
+# class Solution:
+#     def vowelStrings(self, words: List[str], left: int, right: int) -> int:
+#         answer = 0
+#         vowels = "aeiou"
+#         for i in range(left, right+1):
+#             if words[i][0] in vowels and words[i][-1] in vowels:
+#                 answer += 1
+#         return answer
