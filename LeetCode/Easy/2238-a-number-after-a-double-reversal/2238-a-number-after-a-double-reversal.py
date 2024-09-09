927cdf94ca26e8fd3879024381988eb269a3bcc6
hkle2 <minnie110598@gmail.com>
2024-09-09 09:33:58 +0900

    Time: 33 ms (67.73%), Space: 16.5 MB (63.1%) - LeetHub

--- LeetCode/Easy/2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.py
+++ LeetCode/Easy/2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.py
@@ -1,7 +1,16 @@
 class Solution:
     def isSameAfterReversals(self, num: int) -> bool:
-        reversed1 = int("".join(reversed(str(num))))
-        reversed2 = int("".join(reversed(str(reversed1))))
+        reversed1 = int(str(num)[::-1])
+        # reversed_num을 다시 뒤집고 num과 비교해 보기
+        reversed2 = int(str(reversed1)[::-1])
         if num == reversed2:
             return True
         return False
+
+# class Solution:
+#     def isSameAfterReversals(self, num: int) -> bool:
+#         reversed1 = int("".join(reversed(str(num))))
+#         reversed2 = int("".join(reversed(str(reversed1))))
+#         if num == reversed2:
+#             return True
+#         return False
