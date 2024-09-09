96eb361c40b51df980d8f7738c7e4cfec2c94281
hkle2 <minnie110598@gmail.com>
2024-09-09 11:43:04 +0900

    Time: 40 ms (41.54%), Space: 16.3 MB (91.85%) - LeetHub

--- LeetCode/Easy/1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.py
+++ LeetCode/Easy/1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.py
@@ -12,14 +12,14 @@ class Solution:
             prev = num
         return answer
 
-# class Solution:
-#     def maxAscendingSum(self, nums: List[int]) -> int:
-#         answer = nums[0]
-#         sum_ = nums[0]
-#         for i in range(1, len(nums)):
-#             if nums[i-1] < nums[i]:
-#                 sum_ += nums[i]
-#             else:
-#                 sum_ = nums[i]
-#             answer = max(answer, sum_)
-#         return answer
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        answer = nums[0]
+        sum_ = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i-1] < nums[i]:
+                sum_ += nums[i]
+            else:
+                sum_ = nums[i]
+            answer = max(answer, sum_)
+        return answer
