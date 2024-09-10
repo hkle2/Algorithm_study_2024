c6ea98fca183743f00777730d09ca37a671764cc
hkle2 <minnie110598@gmail.com>
2024-09-10 09:35:49 +0900

    Time: 31 ms (82.16%), Space: 16.5 MB (71.88%) - LeetHub

--- LeetCode/Easy/3567-convert-date-to-binary/3567-convert-date-to-binary.py
+++ LeetCode/Easy/3567-convert-date-to-binary/3567-convert-date-to-binary.py
@@ -1,7 +1,15 @@
 class Solution:
     def convertDateToBinary(self, date: str) -> str:
-        l_date = date.split("-")
-        bin_date = []
-        for num in l_date:
-            bin_date.append(bin(int(num))[2:])
-        return "-".join(bin_date)
+        year, month, day = date.split("-")
+        year_bin = bin(int(year))[2:]
+        month_bin = bin(int(month))[2:]
+        day_bin = bin(int(day))[2:]
+        return f"{year_bin}-{month_bin}-{day_bin}"
+
+# class Solution:
+#     def convertDateToBinary(self, date: str) -> str:
+#         l_date = date.split("-")
+#         bin_date = []
+#         for num in l_date:
+#             bin_date.append(bin(int(num))[2:])
+#         return "-".join(bin_date)
