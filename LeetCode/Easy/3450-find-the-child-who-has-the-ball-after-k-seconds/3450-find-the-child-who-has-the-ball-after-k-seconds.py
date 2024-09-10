54363dff99be98cbb976812e3eed19defcb600c9
hkle2 <minnie110598@gmail.com>
2024-09-10 09:28:51 +0900

    Time: 35 ms (65.29%), Space: 16.5 MB (44.86%) - LeetHub

--- LeetCode/Easy/3450-find-the-child-who-has-the-ball-after-k-seconds/3450-find-the-child-who-has-the-ball-after-k-seconds.py
+++ LeetCode/Easy/3450-find-the-child-who-has-the-ball-after-k-seconds/3450-find-the-child-who-has-the-ball-after-k-seconds.py
@@ -1,9 +1,7 @@
 class Solution:
     def numberOfChild(self, n: int, k: int) -> int:
         div, mod = divmod(k, n-1)
-        if div % 2 == 0:
-            return mod
-        return n - 1 - mod
+        return mod if div % 2 == 0 else n - 1 - mod
 
 # class Solution:
 #     def numberOfChild(self, n: int, k: int) -> int:
