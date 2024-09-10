22bd1d18c68d95704a65e10e6c29d965028a1484
hkle2 <minnie110598@gmail.com>
2024-09-10 10:09:51 +0900

    Time: 24 ms (98.35%), Space: 16.4 MB (80.28%) - LeetHub

--- LeetCode/Easy/3450-find-the-child-who-has-the-ball-after-k-seconds/3450-find-the-child-who-has-the-ball-after-k-seconds.py
+++ LeetCode/Easy/3450-find-the-child-who-has-the-ball-after-k-seconds/3450-find-the-child-who-has-the-ball-after-k-seconds.py
@@ -1,22 +1,13 @@
 class Solution:
     def numberOfChild(self, n: int, k: int) -> int:
-        # modulo, children list를 만든 다음, 뒤집어서 한번 붙이기
         children = [x for x in range(n)]
         queue = children + children[1:-1][::-1]
-        # k가 queue 길이보다 작을 때는 그냥 k번째 element 리턴
-        # k가 queue 길이보다 클 때는 모듈로 적용
         return queue[k % len(queue)]
 
-# class Solution:
-#     def numberOfChild(self, n: int, k: int) -> int:
-#         children = [x for x in range(n)]
-#         line = children + children[1:-1][::-1]
-#         return line[k % len(line)]
-
-# class Solution:
-#     def numberOfChild(self, n: int, k: int) -> int:
-#         div, mod = divmod(k, n-1)
-#         if div % 2 == 0:
-#             return mod
-#         else:
-#             return n - 1 - mod
+class Solution:
+    def numberOfChild(self, n: int, k: int) -> int:
+        div, mod = divmod(k, n-1)
+        if div % 2 == 0:
+            return mod
+        else:
+            return n - 1 - mod
