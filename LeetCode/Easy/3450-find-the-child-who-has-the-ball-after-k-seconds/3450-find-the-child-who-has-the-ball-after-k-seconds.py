b684864b302cac9452cfac6dcc1d6fd762cbea98
hkle2 <minnie110598@gmail.com>
2024-09-10 10:13:24 +0900

    Time: 29 ms (91.74%), Space: 16.6 MB (44.86%) - LeetHub

--- LeetCode/Easy/3450-find-the-child-who-has-the-ball-after-k-seconds/3450-find-the-child-who-has-the-ball-after-k-seconds.py
+++ LeetCode/Easy/3450-find-the-child-who-has-the-ball-after-k-seconds/3450-find-the-child-who-has-the-ball-after-k-seconds.py
@@ -1,13 +1,17 @@
 class Solution:
     def numberOfChild(self, n: int, k: int) -> int:
-        children = [x for x in range(n)]
-        queue = children + children[1:-1][::-1]
-        return queue[k % len(queue)]
+        return ([x for x in range(n)] + [x for x in range(n)][1:-1][::-1])[k % (2 * n - 2)]
 
-class Solution:
-    def numberOfChild(self, n: int, k: int) -> int:
-        div, mod = divmod(k, n-1)
-        if div % 2 == 0:
-            return mod
-        else:
-            return n - 1 - mod
+# class Solution:
+#     def numberOfChild(self, n: int, k: int) -> int:
+#         children = [x for x in range(n)]
+#         queue = children + children[1:-1][::-1]
+#         return queue[k % len(queue)]
+
+# class Solution:
+#     def numberOfChild(self, n: int, k: int) -> int:
+#         div, mod = divmod(k, n-1)
+#         if div % 2 == 0:
+#             return mod
+#         else:
+#             return n - 1 - mod
