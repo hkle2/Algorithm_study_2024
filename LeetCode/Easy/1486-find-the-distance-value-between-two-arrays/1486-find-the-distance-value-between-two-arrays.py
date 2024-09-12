2f772d2b620ca6ffd4b7dca0885b0a7da0868dae
hkle2 <minnie110598@gmail.com>
2024-09-12 13:36:31 +0900

    Time: 79 ms (53.55%), Space: 16.8 MB (14.75%) - LeetHub

--- LeetCode/Easy/1486-find-the-distance-value-between-two-arrays/1486-find-the-distance-value-between-two-arrays.py
+++ LeetCode/Easy/1486-find-the-distance-value-between-two-arrays/1486-find-the-distance-value-between-two-arrays.py
@@ -1,13 +1,13 @@
-# class Solution:
-#     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
-#         answer = 0
-#         for i in range(len(arr1)):
-#             for j in range(len(arr2)):
-#                 if abs(arr1[i] - arr2[j]) <= d:
-#                     break
-#             else:
-#                 answer += 1
-#         return answer
+class Solution:
+    def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+        answer = 0
+        for i in range(len(arr1)):
+            for j in range(len(arr2)):
+                if abs(arr1[i] - arr2[j]) <= d:
+                    break
+            else:
+                answer += 1
+        return answer
 
 from bisect import bisect_left
 
@@ -21,27 +21,27 @@ class Solution:
                 answer += 1
         return answer
 
-# class Solution:
-#     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
-#         answer = 0
-#         s = set(arr2)
-#         for num in arr1:
-#             for i in range(d + 1):
-#                 if (num + i in s) or (num - i in s):
-#                     break
-#             else:
-#                 answer += 1
-#         return answer
+class Solution:
+    def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+        answer = 0
+        s = set(arr2)
+        for num in arr1:
+            for i in range(d + 1):
+                if (num + i in s) or (num - i in s):
+                    break
+            else:
+                answer += 1
+        return answer
 
-# class Solution:
-#     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
-#         answer = 0
-#         for num in arr1:
-#             flag = True
-#             for i in range(len(arr2)):
-#                 if abs(num - arr2[i]) <= d:
-#                     flag = False
-#                     break
-#             if flag:
-#                 answer += 1
-#         return answer
+class Solution:
+    def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+        answer = 0
+        for num in arr1:
+            flag = True
+            for i in range(len(arr2)):
+                if abs(num - arr2[i]) <= d:
+                    flag = False
+                    break
+            if flag:
+                answer += 1
+        return answer
