0430274a6f867db60f9c814b9bbca8be35ed9692
hkle2 <minnie110598@gmail.com>
2024-09-12 11:24:54 +0900

    Time: 84 ms (40.64%), Space: 16.7 MB (56.96%) - LeetHub

--- LeetCode/Easy/1486-find-the-distance-value-between-two-arrays/1486-find-the-distance-value-between-two-arrays.py
+++ LeetCode/Easy/1486-find-the-distance-value-between-two-arrays/1486-find-the-distance-value-between-two-arrays.py
@@ -3,22 +3,21 @@ class Solution:
         answer = 0
         for i in range(len(arr1)):
             for j in range(len(arr2)):
-                distance = abs(arr1[i] - arr2[j])
-                if distance <= d:
+                if abs(arr1[i] - arr2[j]) <= d:
                     break
             else:
                 answer += 1
         return answer
 
-class Solution:
-    def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
-        answer = 0
-        for num in arr1:
-            flag = True
-            for i in range(len(arr2)):
-                if abs(num - arr2[i]) <= d:
-                    flag = False
-                    break
-            if flag:
-                answer += 1
-        return answer
+# class Solution:
+#     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+#         answer = 0
+#         for num in arr1:
+#             flag = True
+#             for i in range(len(arr2)):
+#                 if abs(num - arr2[i]) <= d:
+#                     flag = False
+#                     break
+#             if flag:
+#                 answer += 1
+#         return answer
