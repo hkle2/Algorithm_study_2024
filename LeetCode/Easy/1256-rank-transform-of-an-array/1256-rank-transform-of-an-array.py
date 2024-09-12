ff3c18c161a518924c2d2d1ec437083fcbf1a856
hkle2 <minnie110598@gmail.com>
2024-09-12 10:37:29 +0900

    Time: 239 ms (67.32%), Space: 35.7 MB (45%) - LeetHub

--- LeetCode/Easy/1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
+++ LeetCode/Easy/1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
@@ -3,13 +3,13 @@ class Solution:
         rank_dict = {x: (i + 1) for i, x in enumerate(sorted(set(arr)))}
         return [rank_dict[num] for num in arr]
 
-# class Solution:
-#     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-#         answer = []
-#         sorted_arr = sorted(set(arr))
-#         rank = {}
-#         for i, n in enumerate(sorted_arr):
-#             rank[n] = i+1
-#         for num in arr:
-#             answer.append(rank[num])
-#         return answer
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        answer = []
+        sorted_arr = sorted(set(arr))
+        rank = {}
+        for i, n in enumerate(sorted_arr):
+            rank[n] = i+1
+        for num in arr:
+            answer.append(rank[num])
+        return answer
