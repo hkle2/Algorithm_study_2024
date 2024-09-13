b115e8d051ef3ade910cf1e52a7f25e5958a041a
hkle2 <minnie110598@gmail.com>
2024-09-13 10:07:48 +0900

    Time: 35 ms (78.31%), Space: 16.4 MB (94.69%) - LeetHub

--- LeetCode/Easy/2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.py
+++ LeetCode/Easy/2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.py
@@ -1,6 +1,16 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        return len({num for num in nums if num})
+        # 규칙 찾기
+        # 0이 아닌 유니크한 숫자 개수
+        element = set()
+        for num in nums:
+            if num > 0:
+                element.add(num)
+        return len(element)
+        
+# class Solution:
+#     def minimumOperations(self, nums: List[int]) -> int:
+#         return len({num for num in nums if num})
 
 # class Solution:
 #     def minimumOperations(self, nums: List[int]) -> int:
