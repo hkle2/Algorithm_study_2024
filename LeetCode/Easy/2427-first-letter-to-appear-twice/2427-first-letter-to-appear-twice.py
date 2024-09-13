6d2a7c4af44bdb832eeaea4537f568fa410f1cac
hkle2 <minnie110598@gmail.com>
2024-09-13 10:05:46 +0900

    Time: 37 ms (38.41%), Space: 16.4 MB (58.51%) - LeetHub

--- LeetCode/Easy/2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.py
+++ LeetCode/Easy/2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.py
@@ -1,23 +1,21 @@
-from collections import defaultdict
-
-class Solution:
-    def repeatedCharacter(self, s: str) -> str:
-        # for, dictionary
-        d = defaultdict(int)
-        for c in s:
-            d[c] += 1
-            # 특정 글자가 2번 이상 등장하면 return 해주기!
-            if d[c] > 1:
-                return c
+# from collections import defaultdict
 
 # class Solution:
 #     def repeatedCharacter(self, s: str) -> str:
-#         visited = set()
+#         d = defaultdict(int)
 #         for c in s:
-#             if c in visited:
+#             d[c] += 1
+#             if d[c] > 1:
 #                 return c
-#             else:
-#                 visited.add(c)
+
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        visited = set()
+        for c in s:
+            if c in visited:
+                return c
+            else:
+                visited.add(c)
 
 # class Solution:
 #     def repeatedCharacter(self, s: str) -> str:
