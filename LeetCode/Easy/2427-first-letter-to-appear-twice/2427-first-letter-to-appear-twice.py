8229a0722e680d501d832e0dac5dcbf31128fc10
hkle2 <minnie110598@gmail.com>
2024-09-13 10:06:26 +0900

    Time: 28 ms (91.29%), Space: 16.4 MB (58.51%) - LeetHub

--- LeetCode/Easy/2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.py
+++ LeetCode/Easy/2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.py
@@ -8,19 +8,18 @@
 #             if d[c] > 1:
 #                 return c
 
-class Solution:
-    def repeatedCharacter(self, s: str) -> str:
-        visited = set()
-        for c in s:
-            if c in visited:
-                return c
-            visited.add(c)
-
 # class Solution:
 #     def repeatedCharacter(self, s: str) -> str:
-#         visited = []
-#         for i in range(len(s)):
-#             if s[i] in visited:
-#                 return s[i]
-#             else:
-#                 visited.append(s[i])
+#         visited = set()
+#         for c in s:
+#             if c in visited:
+#                 return c
+#             visited.add(c)
+
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        visited = []
+        for i in range(len(s)):
+            if s[i] in visited:
+                return s[i]
+            visited.append(s[i])
