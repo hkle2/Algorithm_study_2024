b6944f8d0ce40c0acdfb57059deeaaa696a95bf9
hkle2 <minnie110598@gmail.com>
2024-09-13 10:05:28 +0900

    Time: 42 ms (7.9%), Space: 16.5 MB (9.16%) - LeetHub

--- LeetCode/Easy/2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.py
+++ LeetCode/Easy/2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.py
@@ -1,14 +1,14 @@
-# from collections import defaultdict
+from collections import defaultdict
 
-# class Solution:
-#     def repeatedCharacter(self, s: str) -> str:
-#         # for, dictionary
-#         d = defaultdict(int)
-#         for c in s:
-#             d[c] += 1
-#             # 특정 글자가 2번 이상 등장하면 return 해주기!
-#             if d[c] == 2:
-#                 return c
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        # for, dictionary
+        d = defaultdict(int)
+        for c in s:
+            d[c] += 1
+            # 특정 글자가 2번 이상 등장하면 return 해주기!
+            if d[c] > 1:
+                return c
 
 # class Solution:
 #     def repeatedCharacter(self, s: str) -> str:
@@ -19,11 +19,11 @@
 #             else:
 #                 visited.add(c)
 
-class Solution:
-    def repeatedCharacter(self, s: str) -> str:
-        visited = []
-        for i in range(len(s)):
-            if s[i] in visited:
-                return s[i]
-            else:
-                visited.append(s[i])
+# class Solution:
+#     def repeatedCharacter(self, s: str) -> str:
+#         visited = []
+#         for i in range(len(s)):
+#             if s[i] in visited:
+#                 return s[i]
+#             else:
+#                 visited.append(s[i])
