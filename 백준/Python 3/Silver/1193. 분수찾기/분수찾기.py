242a562c657510e1ec9e30fb47aaeac944afee2d
hkle2 <minnie110598@gmail.com>
2024-09-15 13:47:26 +0900

    [Silver V] Title: 분수찾기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Python 3/Silver/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,13 @@
+import sys
+
+X = int(sys.stdin.readline().strip())
+line = 1
+
+while X > line:
+    X -= line
+    line += 1
+
+if line % 2 == 0:
+    print(f"{X}/{line- (X - 1)}")
+else:
+    print(f"{line - (X - 1)}/{X}")
