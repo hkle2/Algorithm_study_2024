0712c54f14e0b75cac7c675f0c259be5703ef400
hkle2 <minnie110598@gmail.com>
2024-09-17 23:20:44 +0900

    [Bronze III] Title: 대지, Time: 104 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Python 3/Bronze/9063. 대지/대지.py
+++ 백준/Python 3/Bronze/9063. 대지/대지.py
@@ -6,9 +6,7 @@ y_min, y_max = 100001, -100001
 
 for _ in range(N):
     x, y = map(int, sys.stdin.readline().split())
-    x_max = max(x_max, x)
-    x_min = min(x_min, x)
-    y_max = max(y_max, y)
-    y_min = min(y_min, y)
+    x_min, x_max = min(x_min, x), max(x_max, x)
+    y_min, y_max = min(y_min, y), max(y_max, y)
 
 print((x_max - x_min) * (y_max - y_min))
