e40092c72585db5c91c6c4118ca889f6e8582f84
hkle2 <minnie110598@gmail.com>
2024-09-19 09:18:45 +0900

    Time: 49 ms (51.46%), Space: 16.6 MB (27.18%) - LeetHub

--- LeetCode/Easy/2791-find-the-losers-of-the-circular-game/2791-find-the-losers-of-the-circular-game.py
+++ LeetCode/Easy/2791-find-the-losers-of-the-circular-game/2791-find-the-losers-of-the-circular-game.py
@@ -1,13 +1,13 @@
 class Solution:
     def circularGameLosers(self, n: int, k: int) -> List[int]:
-        cur_loc = 0
-        friends = [0]
+        ball_loc = 0
+        received = [0]
         i = 1
         while True:
-            cur_loc = (cur_loc + i * k) % n
-            if cur_loc not in friends:
-                friends.append(cur_loc)
+            ball_loc = (ball_loc + i * k) % n
+            if ball_loc not in received:
+                received.append(ball_loc)
                 i += 1
             else:
                 break
-        return [i+1 for i in range(n) if i not in friends]
+        return [i+1 for i in range(n) if i not in received]
