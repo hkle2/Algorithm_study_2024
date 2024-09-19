7a0b20c61628ecf9249b093abeed859d2d0aa0f8
hkle2 <minnie110598@gmail.com>
2024-09-19 09:23:42 +0900

    Time: 39 ms (94.5%), Space: 16.5 MB (67.64%) - LeetHub

--- LeetCode/Easy/2791-find-the-losers-of-the-circular-game/2791-find-the-losers-of-the-circular-game.py
+++ LeetCode/Easy/2791-find-the-losers-of-the-circular-game/2791-find-the-losers-of-the-circular-game.py
@@ -1,13 +1,37 @@
+# class Solution:
+#     def circularGameLosers(self, n: int, k: int) -> List[int]:
+#         # while, set
+#         cur_loc = 0
+#         visited = set([cur_loc])
+#         while True:
+#             # 한 번이라도 공을 잡았던 친구가
+#             # 다시 공을 잡으면 break
+#             pass
+#         # 공을 한 번도 못 잡은 친구들을 answer에 담아서 리턴!
+#         answer = []
+#         return answer
+
 class Solution:
     def circularGameLosers(self, n: int, k: int) -> List[int]:
+        friends = [False for _ in range(n)]
         ball_loc = 0
-        received = [0]
         i = 1
-        while True:
+        while not friends[ball_loc]:
+            friends[ball_loc] = True
             ball_loc = (ball_loc + i * k) % n
-            if ball_loc not in received:
-                received.append(ball_loc)
-                i += 1
-            else:
-                break
-        return [i+1 for i in range(n) if i not in received]
+            i += 1
+        return [i + 1 for i in range(n) if not friends[i]]
+
+# class Solution:
+#     def circularGameLosers(self, n: int, k: int) -> List[int]:
+#         ball_loc = 0
+#         received = [0]
+#         i = 1
+#         while True:
+#             ball_loc = (ball_loc + i * k) % n
+#             if ball_loc not in received:
+#                 received.append(ball_loc)
+#                 i += 1
+#             else:
+#                 break
+#         return [i + 1 for i in range(n) if i not in received]
