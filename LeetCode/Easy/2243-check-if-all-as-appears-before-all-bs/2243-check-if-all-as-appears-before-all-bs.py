10cd7afaa8f37d798abcedce061a44bccd6c4dfb
hkle2 <minnie110598@gmail.com>
2024-09-19 10:03:30 +0900

    Time: 34 ms (65.42%), Space: 16.6 MB (13.07%) - LeetHub

--- LeetCode/Easy/2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.py
+++ LeetCode/Easy/2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.py
@@ -3,16 +3,15 @@ class Solution:
         appear_b = False
         for c in s:
             if c == "a":
-                # a가 등장했는데 appear_b가 True라면 정답은 False!
                 if appear_b:
                     return False
             elif c == "b":
                 appear_b = True
         return True
 
-# class Solution:
-#     def checkString(self, s: str) -> bool:
-#         for i in range(1, len(s)):
-#             if s[i-1] == "b" and s[i] == "a":
-#                 return False
-#         return True
+class Solution:
+    def checkString(self, s: str) -> bool:
+        for i in range(1, len(s)):
+            if s[i-1] == "b" and s[i] == "a":
+                return False
+        return True
