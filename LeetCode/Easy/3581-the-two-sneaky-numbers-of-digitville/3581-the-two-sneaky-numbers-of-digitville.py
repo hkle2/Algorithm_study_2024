fc30541436d4a07b105db041666c03ecc3745a50
hkle2 <minnie110598@gmail.com>
2024-09-20 13:02:52 +0900

    Time: 52 ms (38.78%), Space: 16.6 MB (28.62%) - LeetHub

--- LeetCode/Easy/3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
+++ LeetCode/Easy/3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
@@ -1,10 +1,21 @@
+from collections import Counter
+
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-        answer1 = []
-        answer2 = []
-        for i in range(len(nums)):
-            if nums[i] not in answer1:
-                answer1.append(nums[i])
-            else:
-                answer2.append(nums[i])
-        return answer2
+        answer = []
+        c = Counter(nums)
+        for key, value in c.items():
+            if value == 2:
+                answer.append(key)
+        return answer
+
+# class Solution:
+#     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+#         answer1 = []
+#         answer2 = []
+#         for i in range(len(nums)):
+#             if nums[i] not in answer1:
+#                 answer1.append(nums[i])
+#             else:
+#                 answer2.append(nums[i])
+#         return answer2
