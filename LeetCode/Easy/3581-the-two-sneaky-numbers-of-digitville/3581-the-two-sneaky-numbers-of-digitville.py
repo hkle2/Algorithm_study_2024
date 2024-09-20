8f022ad121801dfba22bd21c388a21d94cb7eca7
hkle2 <minnie110598@gmail.com>
2024-09-20 13:23:58 +0900

    Time: 49 ms (59.09%), Space: 16.6 MB (28.62%) - LeetHub

--- LeetCode/Easy/3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
+++ LeetCode/Easy/3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
@@ -1,10 +1,6 @@
-# class Solution:
-#     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-#         seen = []
-#         answer = []
-#         for num in nums:
-#             if num in seen:
-#         return answer
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        return [key for key, value in Counter(nums).items() if value == 2]
 
 # from collections import Counter
 
@@ -17,13 +13,13 @@
 #                 answer.append(key)
 #         return answer
 
-class Solution:
-    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-        seen = set()
-        answer = []
-        for i in range(len(nums)):
-            if nums[i] not in seen:
-                seen.add(nums[i])
-            else:
-                answer.append(nums[i])
-        return answer
+# class Solution:
+#     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+#         seen = []
+#         answer = []
+#         for i in range(len(nums)):
+#             if nums[i] not in seen:
+#                 seen.append(nums[i])
+#             else:
+#                 answer.append(nums[i])
+#         return answer
