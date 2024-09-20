3e729bd84e7ae0dcf2ba5f7cf161cd6f36c4a7d6
hkle2 <minnie110598@gmail.com>
2024-09-20 13:25:17 +0900

    Time: 44 ms (86.05%), Space: 16.3 MB (96.78%) - LeetHub

--- LeetCode/Easy/3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
+++ LeetCode/Easy/3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
@@ -1,25 +1,25 @@
+from collections import Counter
+
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
         return [key for key, value in Counter(nums).items() if value == 2]
 
-# from collections import Counter
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        answer = []
+        c = Counter(nums)
+        for key, value in c.items():
+            if value == 2:
+                answer.append(key)
+        return answer
 
-# class Solution:
-#     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-#         answer = []
-#         c = Counter(nums)
-#         for key, value in c.items():
-#             if value == 2:
-#                 answer.append(key)
-#         return answer
-
-# class Solution:
-#     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-#         seen = []
-#         answer = []
-#         for i in range(len(nums)):
-#             if nums[i] not in seen:
-#                 seen.append(nums[i])
-#             else:
-#                 answer.append(nums[i])
-#         return answer
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        seen = []
+        answer = []
+        for i in range(len(nums)):
+            if nums[i] not in seen:
+                seen.append(nums[i])
+            else:
+                answer.append(nums[i])
+        return answer
