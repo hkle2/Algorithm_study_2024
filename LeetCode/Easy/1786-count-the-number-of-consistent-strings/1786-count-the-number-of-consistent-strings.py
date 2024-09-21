a062aeeb627a8652f9e87b8cb20d466a4875344b
hkle2 <minnie110598@gmail.com>
2024-09-22 02:07:10 +0900

    Time: 215 ms (55.53%), Space: 18.6 MB (34.17%) - LeetHub

--- LeetCode/Easy/1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.py
+++ LeetCode/Easy/1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.py
@@ -1,7 +1,6 @@
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
         answer = len(words)
-        # words = set(words)
         for word in words:
             for i in range(len(word)):
                 if word[i] not in allowed:
@@ -9,12 +8,12 @@ class Solution:
                     break
         return answer
 
-# class Solution:
-#     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
-#         answer = 0
-#         for word in words:
-#             for i in range(len(word)):
-#                 if word[i] not in allowed:
-#                     answer += 1
-#                     break
-#         return len(words) - answer
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        answer = 0
+        for word in words:
+            for i in range(len(word)):
+                if word[i] not in allowed:
+                    answer += 1
+                    break
+        return len(words) - answer
