1e237a5c1adafe024fa56db0638e9b38c13f0bb2
hkle2 <minnie110598@gmail.com>
2024-09-23 10:13:00 +0900

    Time: 56 ms (98.4%), Space: 16.9 MB (40.76%) - LeetHub

--- LeetCode/Easy/2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.py
+++ LeetCode/Easy/2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.py
@@ -1,6 +1,22 @@
 class Solution:
     def kthDistinct(self, arr: List[str], k: int) -> str:
         c = Counter(arr)
+        candidates = [x for x in arr if c[x] == 1]
+        if k <= len(candidates):
+            return candidates[k-1]
+        return ""
+
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        c = Counter(arr)
+        answer = [key for key, value in c.items() if value == 1]
+        if len(answer) >= k:
+            return answer[k-1]
+        return ""
+
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        c = Counter(arr)
         answer = []
         for key, value in c.items():
             if value == 1:
