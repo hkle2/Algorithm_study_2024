ec21dcae1358948fd368c9e45670c61b03260a1f
hkle2 <minnie110598@gmail.com>
2024-09-23 09:32:37 +0900

    Time: 60 ms (53.75%), Space: 16.6 MB (32.69%) - LeetHub

--- LeetCode/Easy/2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.py
+++ LeetCode/Easy/2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.py
@@ -1,8 +1,8 @@
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
         answer = 0
-        seats.sort()
-        students.sort()
+        seats = sorted(seats)
+        students = sorted(students)
         for seat, student in zip(seats, students):
             answer += abs(seat - student)
         return answer
