645ab882c1a2d1c4f0aea819d880b562d77f3612
hkle2 <minnie110598@gmail.com>
2024-09-23 09:32:09 +0900

    Time: 64 ms (26.92%), Space: 16.7 MB (32.69%) - LeetHub

--- LeetCode/Easy/2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.py
+++ LeetCode/Easy/2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.py
@@ -3,6 +3,15 @@ class Solution:
         answer = 0
         seats.sort()
         students.sort()
-        for i in range(len(seats)):
-            answer += abs(seats[i] - students[i])
+        for seat, student in zip(seats, students):
+            answer += abs(seat - student)
         return answer
+
+# class Solution:
+#     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+#         answer = 0
+#         seats.sort()
+#         students.sort()
+#         for i in range(len(seats)):
+#             answer += abs(seats[i] - students[i])
+#         return answer
