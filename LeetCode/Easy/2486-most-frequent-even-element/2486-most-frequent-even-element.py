68a6c2622139fb355f8ed51a0a58daa34123d14f
hkle2 <minnie110598@gmail.com>
2024-09-24 10:14:27 +0900

    Time: 256 ms (13.04%), Space: 17.2 MB (5.66%) - LeetHub

--- LeetCode/Easy/2486-most-frequent-even-element/2486-most-frequent-even-element.py
+++ LeetCode/Easy/2486-most-frequent-even-element/2486-most-frequent-even-element.py
@@ -1,13 +1,12 @@
-# class Solution:
-#     def mostFrequentEven(self, nums: List[int]) -> int:
-#         even_num = [x for x in nums if x % 2 == 0]
-#         if not even_num:
-#             return -1
-#         c = Counter(even_num)
-#         max_freq = max(list(c.values()))
-#         # max_freq만큼 등장한 숫자들 찾아서 가장 작은 값을 리턴
-#         candidates = [num for num, cnt in c.items() if cnt == max_freq]
-#         return sorted(candidates)[0]
+class Solution:
+    def mostFrequentEven(self, nums: List[int]) -> int:
+        even_num = [x for x in nums if x % 2 == 0]
+        if not even_num:
+            return -1
+        c = Counter(even_num)
+        max_freq = max(list(c.values()))
+        candidates = [num for num, cnt in c.items() if cnt == max_freq]
+        return sorted(candidates)[0]
 
 class Solution:
     def mostFrequentEven(self, nums: List[int]) -> int:
@@ -18,13 +17,13 @@ class Solution:
         c = sorted(c.items(), key=lambda x: (-x[1], x[0]))
         return c[0][0]
 
-# class Solution:
-#     def mostFrequentEven(self, nums: List[int]) -> int:
-#         answer = []
-#         c = Counter(nums)
-#         c = dict(filter(lambda x: x[0] % 2 == 0, c.items()))
-#         c = sorted(c.items(), key=lambda x: (-x[1], x[0]))
-#         if len(c) >= 1:
-#             return c[0][0]
-#         else:
-#             return -1
+class Solution:
+    def mostFrequentEven(self, nums: List[int]) -> int:
+        answer = []
+        c = Counter(nums)
+        c = dict(filter(lambda x: x[0] % 2 == 0, c.items()))
+        c = sorted(c.items(), key=lambda x: (-x[1], x[0]))
+        if len(c) >= 1:
+            return c[0][0]
+        else:
+            return -1
