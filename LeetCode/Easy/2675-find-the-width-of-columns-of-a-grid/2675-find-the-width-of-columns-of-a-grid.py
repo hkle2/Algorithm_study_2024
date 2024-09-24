54e6bb8e2ff10c229b23fb1a0c1f027ab26875e9
hkle2 <minnie110598@gmail.com>
2024-09-24 10:06:32 +0900

    Time: 94 ms (88.02%), Space: 18.1 MB (40.31%) - LeetHub

--- LeetCode/Easy/2675-find-the-width-of-columns-of-a-grid/2675-find-the-width-of-columns-of-a-grid.py
+++ LeetCode/Easy/2675-find-the-width-of-columns-of-a-grid/2675-find-the-width-of-columns-of-a-grid.py
@@ -1,20 +1,19 @@
 class Solution:
     def findColumnWidth(self, grid: List[List[int]]) -> List[int]:
         answer = [0] * len(grid[0])
-        # 2중 for문, 숫자 -> 문자열로 변환
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 num_len = len(str(grid[i][j]))
                 answer[j] = max(answer[j], num_len)
         return answer
 
-# class Solution:
-#     def findColumnWidth(self, grid: List[List[int]]) -> List[int]:
-#         answer = []
-#         for i in range(len(grid[0])):
-#             length = 0
-#             for j in range(len(grid)):
-#                 if len(str(grid[j][i])) > length:
-#                     length = len(str(grid[j][i]))
-#             answer.append(length)
-#         return answer
+class Solution:
+    def findColumnWidth(self, grid: List[List[int]]) -> List[int]:
+        answer = []
+        for i in range(len(grid[0])):
+            length = 0
+            for j in range(len(grid)):
+                if len(str(grid[j][i])) > length:
+                    length = len(str(grid[j][i]))
+            answer.append(length)
+        return answer
