3c2f46660272b1a01b047eaa45306c170fbed8db
hkle2 <minnie110598@gmail.com>
2024-09-24 09:36:14 +0900

    Time: 226 ms (37.48%), Space: 17.1 MB (15.01%) - LeetHub

--- LeetCode/Easy/2486-most-frequent-even-element/2486-most-frequent-even-element.py
+++ LeetCode/Easy/2486-most-frequent-even-element/2486-most-frequent-even-element.py
@@ -1,10 +1,19 @@
 class Solution:
     def mostFrequentEven(self, nums: List[int]) -> int:
-        answer = []
+        nums = [num for num in nums if num % 2 == 0]
         c = Counter(nums)
-        c = dict(filter(lambda x: x[0] % 2 == 0, c.items()))
         c = sorted(c.items(), key=lambda x: (-x[1], x[0]))
         if len(c) >= 1:
             return c[0][0]
         else:
             return -1
+# class Solution:
+#     def mostFrequentEven(self, nums: List[int]) -> int:
+#         answer = []
+#         c = Counter(nums)
+#         c = dict(filter(lambda x: x[0] % 2 == 0, c.items()))
+#         c = sorted(c.items(), key=lambda x: (-x[1], x[0]))
+#         if len(c) >= 1:
+#             return c[0][0]
+#         else:
+#             return -1
