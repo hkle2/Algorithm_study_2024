e1ca3bf5e4ad86e3f7f642305e3acbe6f03880a1
hkle2 <minnie110598@gmail.com>
2024-09-24 09:45:13 +0900

    Time: 236 ms (24.1%), Space: 17.1 MB (15.01%) - LeetHub

--- LeetCode/Easy/2486-most-frequent-even-element/2486-most-frequent-even-element.py
+++ LeetCode/Easy/2486-most-frequent-even-element/2486-most-frequent-even-element.py
@@ -1,24 +1,24 @@
-class Solution:
-    def mostFrequentEven(self, nums: List[int]) -> int:
-        answer = 100000
-        nums = [num for num in nums if num % 2 == 0]
-        if not nums:
-            return -1
-        c = Counter(nums)
-        for key, value in c.items():
-            if value == max(c.values()):
-                answer = min(key, answer)
-        return answer
-
 # class Solution:
 #     def mostFrequentEven(self, nums: List[int]) -> int:
+#         answer = 100000
 #         nums = [num for num in nums if num % 2 == 0]
-#         c = Counter(nums)
-#         c = sorted(c.items(), key=lambda x: (-x[1], x[0]))
-#         if len(c) >= 1:
-#             return c[0][0]
-#         else:
+#         if not nums:
 #             return -1
+#         c = Counter(nums)
+#         for key, value in c.items():
+#             if value == max(c.values()):
+#                 answer = min(key, answer)
+#         return answer
+
+class Solution:
+    def mostFrequentEven(self, nums: List[int]) -> int:
+        nums = [num for num in nums if num % 2 == 0]
+        c = Counter(nums)
+        c = sorted(c.items(), key=lambda x: (-x[1], x[0]))
+        if len(c) >= 1:
+            return c[0][0]
+        else:
+            return -1
 # class Solution:
 #     def mostFrequentEven(self, nums: List[int]) -> int:
 #         answer = []
