23595bb013d604322c90ca475616a0b4760d3457
hkle2 <minnie110598@gmail.com>
2024-09-25 09:40:49 +0900

    Time: 39 ms (79.57%), Space: 16.5 MB (61.3%) - LeetHub

--- LeetCode/Easy/1755-defuse-the-bomb/1755-defuse-the-bomb.py
+++ LeetCode/Easy/1755-defuse-the-bomb/1755-defuse-the-bomb.py
@@ -1,19 +1,34 @@
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
-        answer = []
         n = len(code)
+        code = code * 2
         if k == 0:
             return [0] * n
         elif k > 0:
             for i in range(n):
-                new = 0
-                for j in range(1, k+1):
-                    new += code[(i + j) % n]
-                answer.append(new)
+                code[i] = sum(code[i+1:i+1+k])
+            return code[:n]
         else:
             for i in range(n):
-                new = 0
-                for j in range(1, abs(k)+1):
-                    new += code[i - j]
-                answer.append(new)
-        return answer
+                code[i] = sum(code[i+n+k:i+n])
+            return code[:n]
+
+# class Solution:
+#     def decrypt(self, code: List[int], k: int) -> List[int]:
+#         answer = []
+#         n = len(code)
+#         if k == 0:
+#             return [0] * n
+#         elif k > 0:
+#             for i in range(n):
+#                 new = 0
+#                 for j in range(1, k+1):
+#                     new += code[(i + j) % n]
+#                 answer.append(new)
+#         else:
+#             for i in range(n):
+#                 new = 0
+#                 for j in range(1, -k+1):
+#                     new += code[i - j]
+#                 answer.append(new)
+#         return answer
