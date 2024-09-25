cffae10bfc9d3fd2cfaa37ee45573be5860ec6c8
hkle2 <minnie110598@gmail.com>
2024-09-25 10:02:47 +0900

    Time: 47 ms (63.28%), Space: 16.6 MB (28.02%) - LeetHub

--- LeetCode/Easy/3582-find-indices-of-stable-mountains/3582-find-indices-of-stable-mountains.py
+++ LeetCode/Easy/3582-find-indices-of-stable-mountains/3582-find-indices-of-stable-mountains.py
@@ -1,10 +1,10 @@
-# class Solution:
-#     def stableMountains(self, height: List[int], threshold: int) -> List[int]:
-#         answer = []
-#         for i in range(1, len(height)):
-#             if height[i-1] > threshold:
-#                 answer.append(i)
-#         return answer
+class Solution:
+    def stableMountains(self, height: List[int], threshold: int) -> List[int]:
+        answer = []
+        for i in range(1, len(height)):
+            if height[i-1] > threshold:
+                answer.append(i)
+        return answer
 
 class Solution:
     def stableMountains(self, height: List[int], threshold: int) -> List[int]:
