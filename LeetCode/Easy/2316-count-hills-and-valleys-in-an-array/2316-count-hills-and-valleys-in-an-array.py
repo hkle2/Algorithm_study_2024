db96b228a0765e4256aff9fdb0950043879e661f
hkle2 <minnie110598@gmail.com>
2024-09-26 10:06:43 +0900

    Time: 35 ms (90.53%), Space: 16.4 MB (69.31%) - LeetHub

--- LeetCode/Easy/2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.py
+++ LeetCode/Easy/2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.py
@@ -1,5 +1,21 @@
 class Solution:
     def countHillValley(self, nums: List[int]) -> int:
+        new_nums = []
+        for i in range(len(nums)):
+            if i == 0:
+                new_nums.append(nums[i])
+            elif nums[i] != nums[i-1]:
+                new_nums.append(nums[i])
+        answer = 0
+        for i in range(1, len(new_nums)-1):
+            if new_nums[i-1] < new_nums[i] >new_nums[i+1]:
+                answer += 1
+            if new_nums[i-1] > new_nums[i] < new_nums[i+1]:
+                answer += 1
+        return answer
+
+class Solution:
+    def countHillValley(self, nums: List[int]) -> int:
         answer = 0
         for i in range(1, len(nums)-1):
             if nums[i] == nums[i+1]:
