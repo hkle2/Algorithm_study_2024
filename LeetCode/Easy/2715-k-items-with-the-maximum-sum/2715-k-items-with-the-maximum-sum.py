49014c0dbcc24bb7908aa2980cf5941f755cac7b
hkle2 <minnie110598@gmail.com>
2024-09-26 10:09:04 +0900

    Time: 33 ms (84.99%), Space: 16.5 MB (35.09%) - LeetHub

--- LeetCode/Easy/2715-k-items-with-the-maximum-sum/2715-k-items-with-the-maximum-sum.py
+++ LeetCode/Easy/2715-k-items-with-the-maximum-sum/2715-k-items-with-the-maximum-sum.py
@@ -1,15 +1,12 @@
 class Solution:
     def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
-        # 1. k값이 numsOnes 보다 같거나 작을 때
-        # 2. k값이 numsOnes 보다 크고 numOnes + numZeros 보다 같거나 작을 때
-        # 3. 그보다 클 때
         return min(k, numOnes) if k <= numOnes + numZeros else numOnes - (k - (numOnes + numZeros))
 
-# class Solution:
-#     def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
-#         if k <= numOnes:
-#             return k
-#         elif k <= numOnes + numZeros:
-#             return numOnes
-#         else:
-#             return numOnes - (k - (numOnes + numZeros))
+class Solution:
+    def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
+        if k <= numOnes:
+            return k
+        elif k <= numOnes + numZeros:
+            return numOnes
+        else:
+            return numOnes - (k - (numOnes + numZeros))
