947e3ed100167a59367d3e7fdb09cbd2064c75bd
hkle2 <minnie110598@gmail.com>
2024-09-27 10:05:06 +0900

    Time: 69 ms (86.5%), Space: 16.5 MB (54.65%) - LeetHub

--- LeetCode/Easy/2181-smallest-index-with-equal-value/2181-smallest-index-with-equal-value.py
+++ LeetCode/Easy/2181-smallest-index-with-equal-value/2181-smallest-index-with-equal-value.py
@@ -1,9 +1,7 @@
 class Solution:
     def smallestEqual(self, nums: List[int]) -> int:
         answer = -1
-        # for, enumerate
         for i, num in enumerate(nums):
-            # i mod 10 == nums[i] 조건 맞으면 i를 answer 지정 후 break
             if i % 10 == nums[i]:
                 return i
         return answer
