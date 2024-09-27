88a17af23877894a02dd009471698e8f587757a6
hkle2 <minnie110598@gmail.com>
2024-09-27 10:05:35 +0900

    Time: 70 ms (82.57%), Space: 16.4 MB (99.08%) - LeetHub

--- LeetCode/Easy/2181-smallest-index-with-equal-value/2181-smallest-index-with-equal-value.py
+++ LeetCode/Easy/2181-smallest-index-with-equal-value/2181-smallest-index-with-equal-value.py
@@ -6,9 +6,9 @@ class Solution:
                 return i
         return answer
 
-# class Solution:
-#     def smallestEqual(self, nums: List[int]) -> int:
-#         for i in range(len(nums)):
-#             if i % 10 == nums[i]:
-#                 return i
-#         return -1
+class Solution:
+    def smallestEqual(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if i % 10 == nums[i]:
+                return i
+        return -1
