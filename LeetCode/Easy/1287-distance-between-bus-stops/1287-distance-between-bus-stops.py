f97b73b7522a9b8fa108ee794d06c71875bb791c
hkle2 <minnie110598@gmail.com>
2024-09-30 10:11:30 +0900

    Time: 46 ms (64.03%), Space: 17.7 MB (24.37%) - LeetHub

--- LeetCode/Easy/1287-distance-between-bus-stops/1287-distance-between-bus-stops.py
+++ LeetCode/Easy/1287-distance-between-bus-stops/1287-distance-between-bus-stops.py
@@ -1,5 +1,16 @@
 class Solution:
     def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
-        if start < destination:
-            return min(sum(distance) - sum(distance[start:destination]), sum(distance[start:destination]))
-        return min(sum(distance) - sum(distance[destination:start]), sum(distance[destination:start]))
+        # 역방향 거리 = 전체 둘레 - 정방향 거리
+        total_distance = sum(distance)
+        if start <= destination:
+            forward_distance = sum(distance[start:destination])
+        else:
+            forward_distance = sum(distance[start:]) + sum(distance[:destination])
+        backward_distance = total_distance - forward_distance
+        return min(forward_distance, backward_distance)
+
+# class Solution:
+#     def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
+#         if start < destination:
+#             return min(sum(distance) - sum(distance[start:destination]), sum(distance[start:destination]))
+#         return min(sum(distance) - sum(distance[destination:start]), sum(distance[destination:start]))
