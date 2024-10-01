dc1f7d47891b578d11f7aa540a7f3b5e269ba4a0
hkle2 <minnie110598@gmail.com>
2024-10-01 09:23:56 +0900

    Time: 75 ms (98.06%), Space: 20 MB (59.51%) - LeetHub

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.py
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.py
@@ -1,7 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         str_dict = defaultdict(list)
-        for string in strs:
-            sorted_string = "".join(sorted(string))
-            str_dict[sorted_string].append(string)
+        for s in strs:
+            sorted_s = "".join(sorted(s))
+            str_dict[sorted_s].append(s)
         return str_dict.values()
