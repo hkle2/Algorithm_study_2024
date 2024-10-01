36229764df4d971143489e725238088e4312133e
hkle2 <minnie110598@gmail.com>
2024-10-01 10:03:26 +0900

    Time: 88 ms (65.3%), Space: 19.9 MB (64.29%) - LeetHub

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.py
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.py
@@ -1,5 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = defaultdict(list)
+        for word in strs:
+            key = "".join(sorted(word))
+            d[key].append(word)
+        return list(d.values())
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         str_dict = defaultdict(list)
         for s in strs:
             sorted_s = "".join(sorted(s))
