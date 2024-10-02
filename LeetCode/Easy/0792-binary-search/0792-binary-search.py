7cf40936dbd8088c884bfd739c374df0f8d3d5a8
hkle2 <minnie110598@gmail.com>
2024-10-02 09:08:04 +0900

    Time: 190 ms (62.56%), Space: 18.1 MB (72.69%) - LeetHub

--- LeetCode/Easy/0792-binary-search/0792-binary-search.py
+++ LeetCode/Easy/0792-binary-search/0792-binary-search.py
@@ -1,5 +1,18 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if target in nums:
-            return nums.index(target)
+        l, r = 0, len(nums)-1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
         return -1
+
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         if target in nums:
+#             return nums.index(target)
+#         return -1
