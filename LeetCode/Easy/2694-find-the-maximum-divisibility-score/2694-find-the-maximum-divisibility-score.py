0a5c4e7b36c0b74abf80f5d25c430ffa60074c55
hkle2 <minnie110598@gmail.com>
2024-10-07 09:42:08 +0900

    Time: 4113 ms (14.39%), Space: 17.1 MB (5.12%) - LeetHub

--- LeetCode/Easy/2694-find-the-maximum-divisibility-score/2694-find-the-maximum-divisibility-score.py
+++ LeetCode/Easy/2694-find-the-maximum-divisibility-score/2694-find-the-maximum-divisibility-score.py
@@ -1,58 +1,12 @@
 class Solution:
     def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
-        d = {}
-        for divisor in divisors:
-            score = 0
-            for num in nums:
-                if num % divisor == 0:
-                    score += 1
-            d[divisor] = score
-        max_score = max(d.values())
-        candidates = []
-        for divisor, score in d.items():
-            if score == max_score:
-                candidates.append(divisor)
-        answer = sorted(candidates)[0]
-        return answer
-
-class Solution:
-    def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
-        d = {}
-        for divisor in divisors:
-            score = 0
-            for num in nums:
-                if num % divisor == 0:
-                    score += 1
-            d[divisor] = score
-        answer = sorted(d.items(), key=lambda x: (-x[1], x[0]))[0][0]
-        return answer
-
-class Solution:
-    def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
-        answer = 0
-        d = {}
-        for divisor in divisors:
-            score = 0
-            for num in nums:
-                if num % divisor == 0:
-                    score += 1
-            d[divisor] = score
-        answer = float("inf")
-        for key, value in d.items():
-            if value == max(d.values()):
-                if key < answer:
-                    answer = key
-        return answer
-
-class Solution:
-    def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
         divisors.sort()
+        answer = 0
         div = []
-        for d in range(len(divisors)):
+        for i in range(len(divisors)):
             cnt = 0
             for j in range(len(nums)):
-                if nums[j] % divisors[d] == 0:
+                if nums[j] % divisors[i] == 0:
                     cnt += 1
             div.append(cnt)
-        i = div.index(max(div))
-        return divisors[i]
+        return divisors[div.index(max(div))]
