f52313576ab63d3c647fca6c65cee34299c8ec33
hkle2 <minnie110598@gmail.com>
2024-10-07 09:34:08 +0900

    Time: 41 ms (82.82%), Space: 16.7 MB (12.35%) - LeetHub

--- LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.py
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.py
@@ -3,11 +3,8 @@ class Solution:
         roman_num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         answer = roman_num[s[0]]
         for i in range(1, len(s)):
-            tmp = roman_num[s[i]] - roman_num[s[i-1]]
-            print(tmp)
-            if tmp > 0:
-                answer -= roman_num[s[i-1]]
-                answer += (roman_num[s[i]] - roman_num[s[i-1]])
+            if roman_num[s[i]] - roman_num[s[i-1]] > 0:
+                answer += (roman_num[s[i]] - 2 * roman_num[s[i-1]])
             else:
                 answer += roman_num[s[i]]
         return answer
