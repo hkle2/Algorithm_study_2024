0a289d5796d25c1f260af12698bfe231c7305779
hkle2 <minnie110598@gmail.com>
2024-10-07 10:12:11 +0900

    Time: 34 ms (97.46%), Space: 16.5 MB (53.19%) - LeetHub

--- LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.py
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,17 +1,30 @@
+# class Solution:
+#     def romanToInt(self, s: str) -> int:
+#         d = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50,
+#         'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
+#         keys = sorted(d.keys(), key=lambda x: d[x], reverse=True)
+#         answer = 0
+#         while s:
+#             for key in keys:
+#                 if s.startswith(key):
+#                     answer += d[key]
+#                     s = s[len(key):]
+#                     break
+#         return answer
+
 class Solution:
     def romanToInt(self, s: str) -> int:
         d = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50,
         'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
-        keys = sorted(d.keys(), key=lambda x: d[x], reverse=True)
+        i = 0
         answer = 0
-        while s:
-            for key in keys:
-                if s.startswith(key):
-                    # answer에 해당 숫자만큼 더해주고
-                    # s에서 해당 문자열을 잘라내 주기
-                    answer += d[key]
-                    s = s[len(key):]
-                    break
+        while i < len(s):
+            if s[i:i+2] in d:
+                answer += d[s[i:i+2]]
+                i += 2
+            else:
+                answer += d[s[i]]
+                i += 1
         return answer
 
 # class Solution:
