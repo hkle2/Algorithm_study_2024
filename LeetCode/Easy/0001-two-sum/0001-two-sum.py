38bf5835d6ee4f29d2db980f1d3d5bad5566c710
hkle2 <minnie110598@gmail.com>
2024-10-07 10:36:11 +0900

    Time: 1722 ms (16.44%), Space: 17.5 MB (61.4%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.py
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.py
@@ -1,27 +1,10 @@
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         answer = []
-#         for i in range(len(nums)):
-#             for j in range(len(nums)):
-#                 if i == j:
-#                     continue
-#                 if nums[i] + nums[j] == target:
-#                     return [i, j]
-
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         length = len(nums)
-#         for i, num1 in enumerate(nums):
-#             for j in range(i+1, length):
-#                 if num1 + nums[j] == target:
-#                     return [i, j]
-
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         for i in range(len(nums)):
-#             for j in range(i+1, len(nums)):
-#                 if nums[i] + nums[j] == target:
-#                     return [i, j]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        length = len(nums)
+        for i, num in enumerate(nums):
+            for j in range(i+1, length):
+                if num + nums[j] == target:
+                    return [i, j]
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -31,3 +14,10 @@ class Solution:
             if diff in d:
                 return [d[diff], i]
             d[num] = i
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
