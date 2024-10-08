537e85df5a0d3ce88cfa75f96164fc35929b71a3
hkle2 <minnie110598@gmail.com>
2024-10-08 10:05:52 +0900

    Time: 123 ms (48.47%), Space: 16.8 MB (8.35%) - LeetHub

--- LeetCode/Easy/2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.py
+++ LeetCode/Easy/2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.py
@@ -9,28 +9,28 @@ class Solution:
                 answer += len(d[num+k])
         return answer
 
-# class Solution:
-#     def countKDifference(self, nums: List[int], k: int) -> int:
-#         c = Counter(nums)
-#         answer = 0
-#         for num in nums:
-#             if num + k in c:
-#                 answer += c[num+k]
-#         return answer
+class Solution:
+    def countKDifference(self, nums: List[int], k: int) -> int:
+        c = Counter(nums)
+        answer = 0
+        for num in nums:
+            if num + k in c:
+                answer += c[num+k]
+        return answer
 
-# class Solution:
-#     def countKDifference(self, nums: List[int], k: int) -> int:
-#         answer = 0
-#         for a, b in combinations(nums, 2):
-#             if abs(a - b) == k:
-#                 answer += 1
-#         return answer
+class Solution:
+    def countKDifference(self, nums: List[int], k: int) -> int:
+        answer = 0
+        for a, b in combinations(nums, 2):
+            if abs(a - b) == k:
+                answer += 1
+        return answer
 
-# class Solution:
-#     def countKDifference(self, nums: List[int], k: int) -> int:
-#         answer = 0
-#         for i in range(len(nums)):
-#             for j in range(i+1, len(nums)):
-#                 if abs(nums[i] - nums[j]) == k:
-#                     answer += 1
-#         return answer
+class Solution:
+    def countKDifference(self, nums: List[int], k: int) -> int:
+        answer = 0
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if abs(nums[i] - nums[j]) == k:
+                    answer += 1
+        return answer
