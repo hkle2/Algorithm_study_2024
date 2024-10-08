eca7e5814c66c17e4f6a381f3dcb07b72ad043cd
hkle2 <minnie110598@gmail.com>
2024-10-08 09:36:00 +0900

    Time: 124 ms (47.16%), Space: 16.5 MB (42.58%) - LeetHub

--- LeetCode/Easy/2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.py
+++ LeetCode/Easy/2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.py
@@ -1,8 +1,16 @@
+# class Solution:
+#     def countKDifference(self, nums: List[int], k: int) -> int:
+#         answer = 0
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 if abs(nums[i] - nums[j]) == k:
+#                     answer += 1
+#         return answer
+
 class Solution:
     def countKDifference(self, nums: List[int], k: int) -> int:
         answer = 0
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if abs(nums[i] - nums[j]) == k:
-                    answer += 1
+        for a, b in combinations(nums, 2):
+            if abs(a - b) == k:
+                answer += 1
         return answer
