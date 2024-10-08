f40eb8053e8f212b416cf5d8e8dfb71c64833d14
hkle2 <minnie110598@gmail.com>
2024-10-08 09:12:31 +0900

    Time: 29 ms (89.57%), Space: 16.6 MB (5.96%) - LeetHub

--- LeetCode/Easy/1434-decrypt-string-from-alphabet-to-integer-mapping/1434-decrypt-string-from-alphabet-to-integer-mapping.py
+++ LeetCode/Easy/1434-decrypt-string-from-alphabet-to-integer-mapping/1434-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -4,9 +4,9 @@ class Solution:
         i = 0
         while i < len(s):
             if i + 2 < len(s) and s[i+2] == "#":
-                answer += chr(int(s[i:i+2]) + 96)
+                answer += chr(int(s[i:i+2]) + ord("a") - 1)
                 i += 3
             else:
-                answer += chr(int(s[i]) + 96)
+                answer += chr(int(s[i]) + ord("a") - 1)
                 i += 1
         return answer
