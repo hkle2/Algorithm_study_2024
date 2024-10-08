d9aa9e548c20f05bf7a05df3573c9afc95d12e60
hkle2 <minnie110598@gmail.com>
2024-10-08 09:10:53 +0900

    Time: 33 ms (71.95%), Space: 16.4 MB (72.76%) - LeetHub

--- LeetCode/Easy/1434-decrypt-string-from-alphabet-to-integer-mapping/1434-decrypt-string-from-alphabet-to-integer-mapping.py
+++ LeetCode/Easy/1434-decrypt-string-from-alphabet-to-integer-mapping/1434-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -1,32 +1,12 @@
 class Solution:
     def freqAlphabets(self, s: str) -> str:
-        d = {}
-        for i in range(26):
-            alphabet = chr(i + 97)
-            num = i + 1
-            if num < 10:
-                d[str(num)] = alphabet
-            else:
-                d[str(num) + "#"] = alphabet
-        sorted_keys = sorted(d.keys(), reverse=True, key=lambda x: (len(x), x))
-        answer = ""
-        while s:
-            for key in sorted_keys:
-                if s.startswith(key):
-                    answer += d[key]
-                    s = s[len(key):]
-                    break
-        return answer
-
-class Solution:
-    def freqAlphabets(self, s: str) -> str:
         answer = ""
         i = 0
         while i < len(s):
             if i + 2 < len(s) and s[i+2] == "#":
-                answer += chr(int(s[i:i+2]) + ord("a") - 1)
+                answer += chr(int(s[i:i+2]) + 96)
                 i += 3
             else:
-                answer += chr(int(s[i]) + ord("a") - 1)
+                answer += chr(int(s[i]) + 96)
                 i += 1
         return answer
