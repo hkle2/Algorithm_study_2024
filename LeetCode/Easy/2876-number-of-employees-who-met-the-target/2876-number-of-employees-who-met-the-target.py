9190688142d8b8578c7ed646962e7f5abac2af20
hkle2 <minnie110598@gmail.com>
2024-10-09 09:52:57 +0900

    Time: 47 ms (35.37%), Space: 16.6 MB (29.15%) - LeetHub

--- LeetCode/Easy/2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.py
+++ LeetCode/Easy/2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.py
@@ -1,7 +1,11 @@
+# class Solution:
+#     def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+#         answer = 0
+#         for hour in hours:
+#             if hour >= target:
+#                 answer += 1
+#         return answer
+
 class Solution:
     def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
-        answer = 0
-        for hour in hours:
-            if hour >= target:
-                answer += 1
-        return answer
+        return sum([1 for hour in hours if hour >= target])
