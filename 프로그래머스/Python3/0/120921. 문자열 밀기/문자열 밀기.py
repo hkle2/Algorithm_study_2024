5d8bdd02a4c1e1463aa479fc7643d3de127db2f2
hkle2 <minnie110598@gmail.com>
2024-10-20 16:07:58 +0900

    [level 0] Title: 문자열 밀기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/Python3/0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/Python3/0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,12 +1,9 @@
-from collections import deque
-
 def solution(A, B):
     answer = 0
     if A == B:
         return 0
     for i in range(len(A)-1, -1, -1):
-        if A[i:] + A[:i] == B:
-            answer += 1
-            return answer
         answer += 1
+        if A[i:] + A[:i] == B:
+            return answer
     return -1
